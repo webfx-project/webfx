60c69d84ba97d56234071aa31c6c25df3c2956d3
Bruno Salmon <dev.salmonb@gmail.com>
2015-11-23 19:37:54 +0100

    Moved id generators into idgen package

--- naga-core/src/main/java/naga/core/util/idgen/FuzzingBackOffGenerator.java
+++ naga-core/src/main/java/naga/core/util/idgen/FuzzingBackOffGenerator.java
@@ -15,7 +15,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package naga.core.util;
+package naga.core.util.idgen;
 
 /**
  * Simple fibonacci back off with fuzzing. This is important for reconnection so that everyone
