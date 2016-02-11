7de7dd7822ad504a105aa92e8c99b0da941078af
Bruno Salmon <dev.salmonb@gmail.com>
2016-02-12 00:26:43 +0100

    Temporary changed HelloNaga TeaVM application to test the expression parser

--- naga-examples/hellonaga/hellonaga-teavm/src/main/java/hellonaga/teavm/HelloNagaTeaVmApplication.java
+++ naga-examples/hellonaga/hellonaga-teavm/src/main/java/hellonaga/teavm/HelloNagaTeaVmApplication.java
@@ -1,6 +1,9 @@
 package hellonaga.teavm;
 
 import hellonaga.logic.HelloNagaLogic;
+import naga.core.orm.expression.term.Select;
+import naga.core.orm.expressionparser.ExpressionParser;
+import naga.core.orm.expressionparser.lci.ParserModelReaderMock;
 import naga.core.spi.platform.Platform;
 import org.teavm.jso.dom.html.HTMLDocument;
 import org.teavm.jso.dom.html.HTMLElement;
@@ -13,8 +16,13 @@ public class HelloNagaTeaVmApplication {
     /* No need for TeaVmPlatform.register(); as the platform will be found by the ServiceLoader */
 
     public static void main(String[] args) {
+        // Testing the expression parser with TeaVM (produces a compilation error for now)
+        Select select = ExpressionParser.parseSelect("select event,<ident> from Document order by id desc limit 100",
+                new ParserModelReaderMock());
+        Platform.log("expression: " + select);
+        /*
         Platform.setWebLogger(HelloNagaTeaVmApplication::displayMessageInDom);
-        HelloNagaLogic.runClient();
+        HelloNagaLogic.runClient();*/
     }
 
     private static void displayMessageInDom(String helloMessage) {
