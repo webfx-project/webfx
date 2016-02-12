9f8fbeac2a5c4b69a8b9398a4559b4990b380691
Bruno Salmon <dev.salmonb@gmail.com>
2016-02-12 22:59:04 +0100

    Commented query mapping and renamed some fields

--- naga-core/src/main/java/naga/core/orm/mapping/query/SqlColumnToEntityFieldMapping.java
+++ naga-core/src/main/java/naga/core/orm/mapping/query/SqlColumnToEntityFieldMapping.java
@@ -4,46 +4,85 @@ package naga.core.orm.mapping.query;
 /**
  * Mapping between the sql column and the entity field. There are 2 cases.
  * (ex: select p.id, p.first_name, p.last_name, c.id, c.name from person p join country c on c.id=p.country_id)
- * 1) either the column refers to a value of the record itself (ex: column 3 = p.last_name)
- * 2) or it refers to a value of a foreign record because of a join (ex: column 5 = c.name)
+ * 1) either the column refers to a value of the primary entity (ex: column p.last_name => p = primary Person entity)
+ * 2) or it refers to a value of a foreign entity because of a join (ex: column c.name => c = foreign Country entity)
  *
  *
  * @author Bruno Salmon
  */
 public class SqlColumnToEntityFieldMapping {
 
-    private final int queryColumnIndex; // The column index of the query result that we need to process (we focused on 3 for case 1 and 5 for case 2 in our example)
-    private final Object modelFieldId; // The field id of the target entity - which depends on the case (ex: last_name in case 1, name in case 2)
+    /**
+     * The query result column index (first = 0) that we are talking about. This column contains a sql value that needs
+     * to be mapped into an entity field.
+     */
+    private final int columnIndex;
+    /**
+     * The domain field (id) that needs to receive the column value in the target entity (which can be a primary or a foreign entity).
+     * Ex: "lastName" for last_name column (entity field names conventionally have logical names with no underscore as
+     * opposed to sql names).
+     */
+    private final Object domainFieldId;
+
     // The following fields are filled only in case 2
-    private final Object foreignModelClassId; // The class id of the foreign entity (ex: Country)
-    private final SqlColumnToEntityFieldMapping joinMapping; // The join mapping object - so containing the foreign entity column index (ex: 4)
 
-    // Constructor for case 1
-    public SqlColumnToEntityFieldMapping(int queryColumnIndex, Object modelFieldId) {
-        this(queryColumnIndex, modelFieldId, null, null);
+    /**
+     * Contains the domain class (id) of the foreign entity when the column contains a foreign key (ie a primary key to
+     * a foreign entity) or null if the column value is not a foreign key.
+     * Ex: it contains "Country" for column c.id (because c.id is a primary key to country table = Country entity).
+     * Note: in this case, domainFieldId can be "country" and it will point to that foreign Country entity.
+     */
+    private final Object foreignClassId;
+    /**
+     * Contains the id column mapping of the foreign entity when the column value needs to be mapped into a field of a
+     * foreign entity (because of a join), or null if the target entity is simply the primary entity (no join).
+     * Ex: for column c.name it contains the c.id column mapping
+     * Note: if foreignIdColumnMapping is not null, foreignIdColumnMapping.getForeignClassId() returns the foreign
+     * entity class (ex: "Country").
+     */
+    private final SqlColumnToEntityFieldMapping foreignIdColumnMapping;
+
+    // Constructor for when the column is a simple value that needs to be mapped into a simple field of the primary entity
+    public SqlColumnToEntityFieldMapping(int columnIndex, Object domainFieldId) {
+        this(columnIndex, domainFieldId, null, null);
     }
 
-    // Constructor for case 2
-    public SqlColumnToEntityFieldMapping(int queryColumnIndex, Object modelFieldId, Object foreignModelClassId, SqlColumnToEntityFieldMapping joinMapping) {
-        this.modelFieldId = modelFieldId;
-        this.queryColumnIndex = queryColumnIndex;
-        this.foreignModelClassId = foreignModelClassId;
-        this.joinMapping = joinMapping;
+    // Constructor for when the column is a foreign key that needs to me mapped into a foreign field of the primary entity (it will point to the foreign entity).
+    public SqlColumnToEntityFieldMapping(int columnIndex, Object foreignFieldId, Object foreignClassId) {
+        this(columnIndex, foreignFieldId, foreignClassId, null);
     }
 
-    public int getQueryColumnIndex() {
-        return queryColumnIndex;
+    // Constructor for when the column is a simple value that needs to me mapped into a simple field of a foreign entity.
+    public SqlColumnToEntityFieldMapping(int columnIndex, SqlColumnToEntityFieldMapping foreignIdColumnMapping, Object foreignEntityFieldId) {
+        this(columnIndex, foreignEntityFieldId, null, foreignIdColumnMapping);
     }
 
-    public Object getForeignModelClassId() {
-        return foreignModelClassId;
+    // Constructor for when the column is a foreign key that needs to me mapped into a foreign field of a foreign entity (it will point to that "second" foreign entity).
+    public SqlColumnToEntityFieldMapping(int columnIndex, SqlColumnToEntityFieldMapping foreignIdColumnMapping, Object foreignFieldId, Object foreignClassId) {
+        this(columnIndex, foreignFieldId, foreignClassId, foreignIdColumnMapping);
     }
 
-    public SqlColumnToEntityFieldMapping getJoinMapping() {
-        return joinMapping;
+    // Constructor that can cover all cases
+    public SqlColumnToEntityFieldMapping(int columnIndex, Object domainFieldId, Object foreignClassId, SqlColumnToEntityFieldMapping foreignIdColumnMapping) {
+        this.columnIndex = columnIndex;
+        this.domainFieldId = domainFieldId;
+        this.foreignClassId = foreignClassId;
+        this.foreignIdColumnMapping = foreignIdColumnMapping;
     }
 
-    public Object getModelFieldId() {
-        return modelFieldId;
+    public int getColumnIndex() {
+        return columnIndex;
+    }
+
+    public Object getDomainFieldId() {
+        return domainFieldId;
+    }
+
+    public Object getForeignClassId() {
+        return foreignClassId;
+    }
+
+    public SqlColumnToEntityFieldMapping getForeignIdColumnMapping() {
+        return foreignIdColumnMapping;
     }
 }
--- naga-core/src/main/java/naga/core/orm/mapping/query/SqlResultToEntityListGenerator.java
+++ naga-core/src/main/java/naga/core/orm/mapping/query/SqlResultToEntityListGenerator.java
@@ -24,24 +24,24 @@ public class SqlResultToEntityListGenerator {
                 // The target entity (to affect the column value to) is normally the current entity
                 Entity targetEntity = entity;
                 // However if this column index is associated with a join, it actually refers to a foreign entity, so let's check this
-                SqlColumnToEntityFieldMapping joinMapping = columnMapping.getJoinMapping();
+                SqlColumnToEntityFieldMapping joinMapping = columnMapping.getForeignIdColumnMapping();
                 if (joinMapping != null) { // Yes it is a join
                     // So let's first get the row id of the database foreign record
-                    Object foreignKey = entityQueryResult.getValue(rowIndex, joinMapping.getQueryColumnIndex());
+                    Object foreignKey = entityQueryResult.getValue(rowIndex, joinMapping.getColumnIndex());
                     // If it is null, there is nothing to do (finally no target entity)
                     if (foreignKey == null)
                         continue;
                     // And creating the foreign entity (or getting the same instance if already created)
-                    targetEntity = store.getOrCreateEntity(joinMapping.getForeignModelClassId(), foreignKey); // And finally using is as the target entity
+                    targetEntity = store.getOrCreateEntity(joinMapping.getForeignClassId(), foreignKey); // And finally using is as the target entity
                 }
                 // Now that we have the target entity, getting the value for the column index
-                Object value = entityQueryResult.getValue(rowIndex, columnMapping.getQueryColumnIndex());
+                Object value = entityQueryResult.getValue(rowIndex, columnMapping.getColumnIndex());
                 // If this is a foreign key (when foreignClassId is filled), we transform the value into a link to the foreign entity
-                if (columnMapping.getForeignModelClassId() != null)
-                    value = store.getOrCreateEntity(columnMapping.getForeignModelClassId(), value);
+                if (columnMapping.getForeignClassId() != null)
+                    value = store.getOrCreateEntity(columnMapping.getForeignClassId(), value);
                 // Now everything is ready to set the field on the target entity
-                targetEntity.setFieldValue(columnMapping.getModelFieldId(), value);
-                //System.out.println(targetEntity.getId().toString() + '.' + columnMapping.getModelFieldId() + " = " + value);
+                targetEntity.setFieldValue(columnMapping.getDomainFieldId(), value);
+                //System.out.println(targetEntity.getId().toString() + '.' + columnMapping.getDomainFieldId() + " = " + value);
             }
             // And finally adding this entity to the list
             entityList.add(entity);
