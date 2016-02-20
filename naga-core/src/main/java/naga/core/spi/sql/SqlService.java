f954f20df69be6b77e490f0521b48c10db3bcb5e
Bruno Salmon <dev.salmonb@gmail.com>
2016-02-20 15:53:27 +0100

    Added Batch support in SqlService interface

--- naga-core/src/main/java/naga/core/spi/sql/SqlService.java
+++ naga-core/src/main/java/naga/core/spi/sql/SqlService.java
@@ -1,5 +1,6 @@
 package naga.core.spi.sql;
 
+import naga.core.util.async.Batch;
 import naga.core.util.async.Future;
 
 /**
@@ -10,5 +11,15 @@ public interface SqlService {
     Future<SqlReadResult> read(SqlArgument argument);
 
     Future<SqlWriteResult> write(SqlArgument argument);
+
+    // Batch support
+
+    default Future<Batch<SqlReadResult>> readBatch(Batch<SqlArgument> batch) {
+        return batch.execute(this::read, SqlReadResult.class);
+    }
+
+    default Future<Batch<SqlWriteResult>> writeBatch(Batch<SqlArgument> batch) {
+        return batch.execute(this::write, SqlWriteResult.class);
+    }
 
 }
