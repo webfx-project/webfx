c78bac6e02eff72fee40dbb968f8e84de85860cb
Bruno Salmon <dev.salmonb@gmail.com>
2016-02-20 16:24:12 +0100

    Added Collections class with toString() static method helper

---
+++ naga-core/src/main/java/naga/core/util/collection/Collections.java
@@ -0,0 +1,25 @@
+package naga.core.util.collection;
+
+import java.util.Iterator;
+
+/**
+ * @author Bruno Salmon
+ */
+public class Collections {
+
+    public static String toString(Iterator it) {
+        if (!it.hasNext())
+            return "[]";
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        for (;;) {
+            sb.append('\n');
+            Object e = it.next();
+            sb.append(e);
+            if (!it.hasNext())
+                return sb.append("\n]").toString();
+            sb.append(',').append(' ');
+        }
+    }
+
+}
