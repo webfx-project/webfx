a576c5bee8b20c6203c387ff3985bc2ab2c77e95
Bruno Salmon <dev.salmonb@gmail.com>
2016-02-20 16:13:18 +0100

    Registered read/write batch sql services in Naga

--- naga-core/src/main/java/naga/core/Naga.java
+++ naga-core/src/main/java/naga/core/Naga.java
@@ -12,6 +12,8 @@ public class Naga {
     public static final String VERSION_ADDRESS = "version";
     public static final String SQL_READ_ADDRESS = "sql.read";
     public static final String SQL_WRITE_ADDRESS = "sql.write";
+    public static final String SQL_READ_BATCH_ADDRESS = "sql.read.batch";
+    public static final String SQL_WRITE_BATCH_ADDRESS = "sql.write.batch";
 
     private static final Naga SINGLETON = new Naga();
 
@@ -31,6 +33,8 @@ public class Naga {
         BusCallService.registerCallableJavaService(VERSION_ADDRESS, this::getVersion);
         BusCallService.registerAsyncFunctionJavaService(SQL_READ_ADDRESS, Platform.sql()::read);
         BusCallService.registerAsyncFunctionJavaService(SQL_WRITE_ADDRESS, Platform.sql()::write);
+        BusCallService.registerAsyncFunctionJavaService(SQL_READ_BATCH_ADDRESS, Platform.sql()::readBatch);
+        BusCallService.registerAsyncFunctionJavaService(SQL_WRITE_BATCH_ADDRESS, Platform.sql()::writeBatch);
 
         // Starting the BusCallService by listening entry calls
         BusCallService.listenEntryCalls();
