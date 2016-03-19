2c5ebd7bd2aa23fb4e92de75f92d6ac73f056f55
Bruno Salmon <dev.salmonb@gmail.com>
2016-03-19 23:00:51 +0100

    Updated Mongoose logic with UI routing and presentation model

--- mongoose/mongoose-logic/src/main/java/mongoose/logic/MongooseLogic.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/logic/MongooseLogic.java
@@ -1,6 +1,9 @@
 package mongoose.logic;
 
 import mongoose.domainmodel.DomainModelSnapshotLoader;
+import naga.core.ngui.routing.UiRouteHandler;
+import naga.core.ngui.routing.UiRouter;
+import naga.core.ngui.routing.UiState;
 import naga.core.orm.filter.StringFilterBuilder;
 import naga.core.rx.RxFilter;
 import naga.core.spi.gui.GuiToolkit;
@@ -8,14 +11,24 @@ import naga.core.spi.gui.node.BorderPane;
 import naga.core.spi.gui.node.SearchBox;
 import naga.core.spi.gui.node.Table;
 import naga.core.spi.gui.node.ToggleButton;
-import naga.core.spi.platform.Platform;
 
 /**
  * @author Bruno Salmon
  */
 public class MongooseLogic {
 
+    public static UiRouteHandler organizationsUiRouterHandler = new UiRouteHandler()
+            .setPresentationModelFactory(OrganizationsPresentationModel::new)
+            .setUiBuilder(MongooseLogic::buildOrganizationsUi)
+            .setPresentationModelLogicBinder(MongooseLogic::doOrganizationsPresentationModelLogicBinding);
+
     public static void runBackendApplication() {
+        UiRouter uiRouter = UiRouter.get();
+        uiRouter.route("/organizations").handler(organizationsUiRouterHandler);
+        uiRouter.start();
+    }
+
+    private static void buildOrganizationsUi(UiState uiState) {
         // Building the UI components (involved in the reactive filter)
         GuiToolkit toolkit = GuiToolkit.get();
         SearchBox<?> searchBox = toolkit.createNode(SearchBox.class);
@@ -24,36 +37,41 @@ public class MongooseLogic {
         limitCheckBox.setText("Limit to 100");
         limitCheckBox.setSelected(true);
 
-        // Loading the domain model (in the background) and setting up the reactive filter
-        Platform.runInBackground(() -> {
-            new RxFilter()
-                    .setDomainModel(DomainModelSnapshotLoader.getOrLoadDomainModel())
-                    .setDataSourceId(3)
-                    // Base filter
-                    .combine(new StringFilterBuilder("Organization")
-                            .setLogicFields("country.(code,continent.code)")
-                            .setOrderBy("name"))
-                    // Condition
-                    .combine(new StringFilterBuilder()
-                            .setCondition("!closed"))
-                    // Fields to display
-                    .combine(new StringFilterBuilder()
-                            .setDisplayFields("name,type.name, country.(name + ' (' + continent.name + ')')"))
-                    // Search box condition
-                    .combine(searchBox, s -> "lower(name) like '%" + s.toLowerCase() + "%'")
-                    // Limit condition
-                    .combine(limitCheckBox, new StringFilterBuilder()
-                            .setLimit("100"))
-                    .displayResultInto(table);
-        });
-
-        // Displaying the UI
+        // Displaying the UI (Showing the first window can take a few seconds)
         toolkit.displayRootNode(toolkit.createNode(BorderPane.class)
                 .setTop(searchBox)
                 .setCenter(table)
-                .setBottom(limitCheckBox)); // Showing the first windonw can take a few seconds
+                .setBottom(limitCheckBox));
         // Requesting the focus on the search box
         searchBox.requestFocus();
+
+        OrganizationsPresentationModel pm = (OrganizationsPresentationModel) uiState.presentationModel();
+        pm.searchTextProperty().bind(searchBox.textProperty());
+        pm.limitProperty().bind(limitCheckBox.selectedProperty());
+        table.displayResultProperty().bind(pm.organizationDisplayResultProperty());
+    }
+
+    private static void doOrganizationsPresentationModelLogicBinding(OrganizationsPresentationModel pm) {
+        // Loading the domain model and setting up the reactive filter
+        new RxFilter()
+                .setDomainModel(DomainModelSnapshotLoader.getOrLoadDomainModel())
+                .setDataSourceId(3)
+                // Base filter
+                .combine(new StringFilterBuilder("Organization")
+                        .setLogicFields("country.(code,continent.code)")
+                        .setOrderBy("name"))
+                // Condition
+                .combine(new StringFilterBuilder()
+                        .setCondition("!closed"))
+                // Fields to display
+                .combine(new StringFilterBuilder()
+                        .setDisplayFields("name,type.name, country.(name + ' (' + continent.name + ')')"))
+                // Search box condition
+                .combine(pm.searchTextProperty(), s -> "lower(name) like '%" + s.toLowerCase() + "%'")
+                // Limit condition
+                .combine(pm.limitProperty(), new StringFilterBuilder()
+                        .setLimit("100"))
+                .displayResultInto(pm.organizationDisplayResultProperty());
     }
 
 }
---
+++ mongoose/mongoose-logic/src/main/java/mongoose/logic/OrganizationsPresentationModel.java
@@ -0,0 +1,25 @@
+package mongoose.logic;
+
+import javafx.beans.property.*;
+import naga.core.ngui.displayresult.DisplayResult;
+import naga.core.ngui.presentationmodel.PresentationModel;
+
+/**
+ * @author Bruno Salmon
+ */
+public class OrganizationsPresentationModel implements PresentationModel {
+
+    // User inputs
+
+    private final Property<String> searchTextProperty = new SimpleObjectProperty<>();
+    public Property<String> searchTextProperty() { return searchTextProperty; }
+
+    private final Property<Boolean> limitProperty = new SimpleObjectProperty<>();
+    public Property<Boolean> limitProperty() { return limitProperty; }
+
+    // Display output
+
+    private final Property<DisplayResult> organizationDisplayResultProperty = new SimpleObjectProperty<>();
+    public Property<DisplayResult> organizationDisplayResultProperty() { return organizationDisplayResultProperty; }
+
+}
