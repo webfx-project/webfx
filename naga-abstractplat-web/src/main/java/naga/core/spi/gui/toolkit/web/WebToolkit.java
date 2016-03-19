ee3da7c0aaafc0d2eb9a359c03b8f5bc5ecb6779
Bruno Salmon <dev.salmonb@gmail.com>
2016-03-19 22:53:40 +0100

    Added WebToolkit class

---
+++ naga-abstractplat-web/src/main/java/naga/core/spi/gui/toolkit/web/WebToolkit.java
@@ -0,0 +1,14 @@
+package naga.core.spi.gui.toolkit.web;
+
+import naga.core.spi.gui.GuiToolkit;
+import naga.core.spi.gui.node.Node;
+
+/**
+ * @author Bruno Salmon
+ */
+public class WebToolkit extends GuiToolkit {
+
+    @Override
+    public void displayRootNode(Node rootNode) {
+    }
+}
