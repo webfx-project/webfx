d1733266fb12c4e2fec8253e0075045000f13285
Bruno Salmon <dev.salmonb@gmail.com>
2016-04-20 10:30:34 +0200

    Added mongoose-pivot

---
+++ mongoose/mongoose-backend/mongoose-backend-pivot/src/main/java/mongoose/backend/pivot/MongooseBackendPivotApplication.java
@@ -0,0 +1,13 @@
+package mongoose.backend.pivot;
+
+import mongoose.logic.MongooseLogic;
+
+/**
+ * @author Bruno Salmon
+ */
+public class MongooseBackendPivotApplication {
+
+    public static void main(String[] args) {
+        MongooseLogic.runBackendApplication();
+    }
+}
