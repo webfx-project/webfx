1f6856d40fb55f7502736eb86d71a27bf0fa1e6c
Bruno Salmon <dev.salmonb@gmail.com>
2016-05-18 22:45:00 +0200

    Added mongoose-frontend-gwt

---
+++ mongoose/mongoose-frontend/mongoose-frontend-gwt/src/main/java/mongoose/frontend/gwt/MongooseFrontendGwtApplication.java
@@ -0,0 +1,30 @@
+package mongoose.frontend.gwt;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
+import mongoose.logic.MongooseFrontendApplication;
+import naga.core.spi.platform.client.gwt.GwtPlatform;
+
+/**
+ * @author Bruno Salmon
+ */
+public class MongooseFrontendGwtApplication implements EntryPoint {
+
+    /* No need for GwtPlatform.register(); as the platform will be found by the customized ServiceLoader provided in the super-source */
+
+    @Override
+    public void onModuleLoad() {
+        replaceSplashScreen(Document.get().createBRElement());
+        registerResourceBundles();
+        MongooseFrontendApplication.main(null);
+    }
+
+    private static void registerResourceBundles() {
+        GwtPlatform.registerBundle(MongooseFrontendGwtBundle.B);
+    }
+
+    private static native void replaceSplashScreen(Element e) /*-{ var preloader = $wnd.document.getElementById("preloader"); preloader.innerHTML = ""; preloader.appendChild(e); }-*/;
+
+
+}
---
+++ mongoose/mongoose-frontend/mongoose-frontend-gwt/src/main/java/mongoose/frontend/gwt/MongooseFrontendGwtBundle.java
@@ -0,0 +1,24 @@
+package mongoose.frontend.gwt;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.TextResource;
+import naga.core.spi.platform.client.gwt.GwtBundle;
+
+/**
+ * @author Bruno Salmon
+ */
+public interface MongooseFrontendGwtBundle extends ClientBundle {
+
+    MongooseFrontendGwtBundle R = GWT.create(MongooseFrontendGwtBundle.class);
+
+    @Source("mongoose/domainmodel/DomainModelSnapshot.lzb64json")
+    TextResource lzBase64DomainModelSnapshot();
+
+    GwtBundle B = resourcePath -> {
+        switch (resourcePath) {
+            case "mongoose/domainmodel/DomainModelSnapshot.lzb64json": return R.lzBase64DomainModelSnapshot();
+            default: return null;
+        }
+    };
+}
