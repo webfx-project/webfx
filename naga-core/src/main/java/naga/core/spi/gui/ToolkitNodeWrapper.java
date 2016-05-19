0a25b4f2564f5406e974622f61ba2c20a03cea4b
Bruno Salmon <dev.salmonb@gmail.com>
2016-05-19 09:53:08 +0200

    Deleted unused ToolkitNodeWrapper class

--- naga-core/src/main/java/naga/core/spi/gui/ToolkitNodeWrapper.java
+++
@@ -1,9 +0,0 @@
-package naga.core.spi.gui;
-
-import naga.core.util.function.Converter;
-
-/**
- * @author Bruno Salmon
- */
-public interface ToolkitNodeWrapper extends Converter<Object, GuiNode> {
-}
