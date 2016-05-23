d542150df170d470adfa8a809b2db08042fc3051
Bruno Salmon <dev.salmonb@gmail.com>
2016-05-23 14:18:59 +0200

    Added head style dynamic addition to have Roboto font for VaadinGrid and PaperCheckbox

--- naga-gui-toolkits/naga-gui-gwtpolymer/src/main/java/naga/core/spi/toolkit/gwtpolymer/GwtPolymerToolkit.java
+++ naga-gui-toolkits/naga-gui-gwtpolymer/src/main/java/naga/core/spi/toolkit/gwtpolymer/GwtPolymerToolkit.java
@@ -1,5 +1,7 @@
 package naga.core.spi.toolkit.gwtpolymer;
 
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.StyleElement;
 import com.vaadin.polymer.paper.widget.PaperCheckbox;
 import com.vaadin.polymer.vaadin.widget.VaadinGrid;
 import naga.core.spi.toolkit.gwt.GwtToolkit;
@@ -14,6 +16,24 @@ import naga.core.spi.toolkit.nodes.Table;
  * @author Bruno Salmon
  */
 public class GwtPolymerToolkit extends GwtToolkit {
+
+    static {
+        String sharedCss =
+                "    font-family: 'Roboto', 'Noto', sans-serif;\n" +
+                "    -webkit-font-smoothing: antialiased;\n" +
+                "    font-size: 16px;\n" +
+                "    font-weight: 400;\n" +
+                "    line-height: 24px;\n";
+        addStyleElement("vaadin-grid, .vaadin-grid-header.vaadin-grid th.vaadin-grid {\n" + sharedCss + "}\n");
+        addStyleElement("paper-checkbox {\n" + sharedCss + "}\n");
+    }
+
+    private static void addStyleElement(String content) {
+        Document doc = Document.get();
+        StyleElement styleElement = doc.createStyleElement();
+        styleElement.setInnerHTML(content);
+        doc.getHead().insertFirst(styleElement);
+    }
 
     public GwtPolymerToolkit() {
         registerNodeFactoryAndWrapper(Table.class, GwtPolymerTable::new, VaadinGrid.class, GwtPolymerTable::new);
