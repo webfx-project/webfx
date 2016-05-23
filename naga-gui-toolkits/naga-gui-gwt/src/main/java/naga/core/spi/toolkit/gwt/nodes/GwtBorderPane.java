89783f37306ee4577fdd8159655428fe6f22651f
Bruno Salmon <dev.salmonb@gmail.com>
2016-05-23 22:07:53 +0200

    Fixed GwtBorderPane regarding North/South components heights

--- naga-gui-toolkits/naga-gui-gwt/src/main/java/naga/core/spi/toolkit/gwt/nodes/GwtBorderPane.java
+++ naga-gui-toolkits/naga-gui-gwt/src/main/java/naga/core/spi/toolkit/gwt/nodes/GwtBorderPane.java
@@ -1,14 +1,18 @@
 package naga.core.spi.toolkit.gwt.nodes;
 
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.Widget;
 import javafx.beans.property.Property;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.value.ObservableValue;
+import javafx.beans.value.ChangeListener;
+import naga.core.spi.platform.Platform;
 import naga.core.spi.toolkit.GuiNode;
 import naga.core.spi.toolkit.gwt.GwtNode;
 import naga.core.spi.toolkit.nodes.BorderPane;
+import naga.core.util.async.Future;
 
 /**
  * @author Bruno Salmon
@@ -16,28 +20,57 @@ import naga.core.spi.toolkit.nodes.BorderPane;
 public class GwtBorderPane extends GwtNode<DockLayoutPanel> implements BorderPane<DockLayoutPanel, Widget> {
 
     public GwtBorderPane() {
-        this(new DockLayoutPanel(Style.Unit.EM));
+        this(new DockLayoutPanel(Style.Unit.PX));
     }
 
     public GwtBorderPane(DockLayoutPanel node) {
         super(node);
-        topProperty.addListener(this::onNodePropertyChange);
-        bottomProperty.addListener(this::onNodePropertyChange);
-        centerProperty.addListener(this::onNodePropertyChange);
+        ChangeListener<GuiNode<Widget>> onAnyNodePropertyChange = (observable, oldValue, newValue) -> populate();
+        topProperty.addListener(onAnyNodePropertyChange);
+        bottomProperty.addListener(onAnyNodePropertyChange);
+        centerProperty.addListener(onAnyNodePropertyChange);
     }
 
-    private void onNodePropertyChange(ObservableValue<? extends GuiNode<Widget>> observable, GuiNode<Widget> oldValue, GuiNode<Widget> newValue) {
-        node.clear();
+    private boolean populating;
+
+    private void populate() {
+        if (!populating) {
+            populating = true;
+            if (!node.isAttached())
+                node.addAttachHandler(event -> addNorth());
+            else {
+                node.clear();
+                addNorth();
+            }
+        }
+    }
+
+    private void addNorth() {
+        populating = false;
         GuiNode<Widget> topPropertyValue = topProperty.getValue();
         if (topPropertyValue != null) {
             Widget widget = topPropertyValue.unwrapToNativeNode();
-            node.addNorth(widget, widget.getOffsetHeight());
-        }
+            measureHeight(widget).setHandler(asyncResult -> {
+                node.addNorth(widget, asyncResult.result());
+                addSouth();
+            });
+        } else
+            addSouth();
+    }
+
+    private void addSouth() {
         GuiNode<Widget> bottomPropertyValue = bottomProperty.getValue();
         if (bottomPropertyValue != null) {
             Widget widget = bottomPropertyValue.unwrapToNativeNode();
-            node.addSouth(widget, widget.getOffsetHeight());
-        }
+            measureHeight(widget).setHandler(asyncResult -> {
+                node.addSouth(widget, asyncResult.result());
+                addCenter();
+            });
+        } else
+            addCenter();
+    }
+
+    private void addCenter() {
         GuiNode<Widget> centerPropertyValue = centerProperty.getValue();
         if (centerPropertyValue != null) {
             Widget widget = centerPropertyValue.unwrapToNativeNode();
@@ -45,6 +78,28 @@ public class GwtBorderPane extends GwtNode<DockLayoutPanel> implements BorderPan
             widget.setHeight("100%");
             node.add(widget);
         }
+    }
+
+    private static Future<Double> measureHeight(Widget widget) {
+        Future<Double> future = Future.future();
+        startMeasureHeight(widget, future);
+        return future;
+    }
+
+    private static void startMeasureHeight(Widget widget, Future<Double> future) {
+        Element clone = widget.getElement().cloneNode(true).cast();
+        clone.setAttribute("style", "position: absolute; visibility: hidden;");
+        Document.get().getBody().appendChild(clone);
+        finishMeasureHeight(clone, future);
+    }
+
+    private static void finishMeasureHeight(Element clone, Future<Double> future) {
+        int height = clone.getOffsetHeight();
+        if (height > 0) {
+            future.complete((double) height);
+            clone.removeFromParent();
+        } else
+            Platform.scheduleDelay(100, () ->  finishMeasureHeight(clone, future));
     }
 
     private final Property<GuiNode<Widget>> topProperty = new SimpleObjectProperty<>();
