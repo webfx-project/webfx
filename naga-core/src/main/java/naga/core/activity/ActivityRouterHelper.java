9bd21b67177a9286089091245892f64e0fff0b2c
Bruno Salmon <dev.salmonb@gmail.com>
2016-05-27 15:34:18 +0200

    Improved the routing activity lifecycle with pause & resume

--- naga-core/src/main/java/naga/core/activity/ActivityRouterHelper.java
+++ naga-core/src/main/java/naga/core/activity/ActivityRouterHelper.java
@@ -2,6 +2,7 @@ package naga.core.activity;
 
 import naga.core.routing.Router;
 import naga.core.routing.RoutingContext;
+import naga.core.spi.toolkit.Toolkit;
 import naga.core.util.async.Handler;
 import naga.core.util.function.Converter;
 import naga.core.util.function.Factory;
@@ -46,7 +47,7 @@ public class ActivityRouterHelper {
         return this;
     }
 
-    private static class ActivityRoutingHandler implements Handler<RoutingContext> {
+    private class ActivityRoutingHandler implements Handler<RoutingContext> {
 
         private final Converter<RoutingContext, ActivityContext> contextConverter;
         private final Factory<ActivityManager> activityManagerFactory;
@@ -59,10 +60,23 @@ public class ActivityRouterHelper {
 
         @Override
         public void handle(RoutingContext context) {
+            // Creating or retrieving the activity context associated with the requested routing context
             ActivityContext activityContext = contextConverter.convert(context);
-            if (activityManager == null)
-                activityManager = activityManagerFactory.create();
-            activityManager.run(activityContext);
+            // Since we will switch the activity, the current activity and its manager will now become the previous ones
+            ActivityManager previousActivityManager = activityManager; // let's memorize the reference to it
+            // Now we switch the current activity and its manager to the current one
+            activityManager = activityContext.getActivityManager();
+            // The returned value is not null only if we switched back to an already existing activity that has been paused before
+            if (activityManager == null) { // otherwise, this is the first time we switch to this activity which is therefore not yet created
+                activityManager = activityManagerFactory.create(); // So we create the activity manager (and its associated activity)
+                activityManager.create(activityContext); // and we transit the activity into the create state and pass the context
+            }
+            // Now we transit the current activity (which was either paused or newly created) into the resume state and
+            // once done we display the activity node by binding it with the parent context (done in the UI tread)
+            activityManager.resume().setHandler(event -> Toolkit.get().scheduler().runInUiThread(() -> parentContext.nodeProperty().bind(activityContext.nodeProperty())));
+            // Now that the new requested activity is displayed, we pause the previous activity
+            if (previousActivityManager != null) // if there was a previous activity
+                previousActivityManager.pause();
         }
     }
 
@@ -75,7 +89,6 @@ public class ActivityRouterHelper {
             history.put(routePath, activityContext = new ActivityContext(parentContext));
         activityContext.setRouter(router);
         activityContext.setParams(routingContext.getParams());
-        parentContext.nodeProperty().bind(activityContext.nodeProperty());
         return activityContext;
     }
 
