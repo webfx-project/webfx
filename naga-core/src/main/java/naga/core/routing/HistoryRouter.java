c9a3f71ada7a8bfa7036c2c651d36b7fd98c839d
Bruno Salmon <dev.salmonb@gmail.com>
2016-06-16 18:19:48 +0200

    Added defaultInitialHistoryPath property in HistoryRouter to be used if the history is initially empty or the path is not found

--- naga-core/src/main/java/naga/core/routing/HistoryRouter.java
+++ naga-core/src/main/java/naga/core/routing/HistoryRouter.java
@@ -12,6 +12,8 @@ public class HistoryRouter {
 
     protected final Router router;
     protected final History history;
+    // The default path to be used if the history is initially empty or the path is not found
+    private String defaultInitialHistoryPath;
 
     public HistoryRouter() {
         this(null);
@@ -24,6 +26,10 @@ public class HistoryRouter {
     public HistoryRouter(Router router, History history) {
         this.router = router != null ? router : Router.create();
         this.history = history != null ? history : Platform.get().getBrowserHistory();
+        this.router.exceptionHandler(throwable -> {
+            Platform.log("Path not found", throwable);
+            replaceCurrentHistoryWithInitialDefaultPath();
+        });
     }
 
     public Router getRouter() {
@@ -34,15 +40,30 @@ public class HistoryRouter {
         return history;
     }
 
+    public String getDefaultInitialHistoryPath() {
+        return defaultInitialHistoryPath;
+    }
+
+    public void setDefaultInitialHistoryPath(String defaultInitialHistoryPath) {
+        this.defaultInitialHistoryPath = defaultInitialHistoryPath;
+    }
+
     public void start() {
-        history.listen(this::onNewHistoryLocation);
         HistoryLocation currentLocation = history.getCurrentLocation();
+        history.listen(this::onNewHistoryLocation);
         if (currentLocation != null)
             onNewHistoryLocation(currentLocation);
+        else
+            replaceCurrentHistoryWithInitialDefaultPath();
+    }
+
+    private void replaceCurrentHistoryWithInitialDefaultPath() {
+        if (defaultInitialHistoryPath != null)
+            history.replace(defaultInitialHistoryPath);
     }
 
     protected void onNewHistoryLocation(HistoryLocation historyLocation) {
-        router.accept(history.createPath(historyLocation), historyLocation.getState());
+        router.accept(history.getPath(historyLocation), historyLocation.getState());
     }
 
 }
