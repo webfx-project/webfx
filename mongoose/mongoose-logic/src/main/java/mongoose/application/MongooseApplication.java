c39fc1895f0b03a5ef4dff77d52cd6f479b4385e
Bruno Salmon <dev.salmonb@gmail.com>
2016-06-22 16:54:36 +0200

    Registered price and date formats in Mongoose application

--- mongoose/mongoose-logic/src/main/java/mongoose/application/MongooseApplication.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/application/MongooseApplication.java
@@ -5,10 +5,15 @@ import mongoose.activities.container.ContainerActivity;
 import mongoose.activities.event.bookings.BookingsActivity;
 import mongoose.activities.organizations.OrganizationsActivity;
 import mongoose.domainmodel.DomainModelSnapshotLoader;
+import mongoose.format.DateFormatter;
+import mongoose.format.PriceFormatter;
 import naga.core.activity.Activity;
 import naga.core.activity.ActivityContext;
 import naga.core.activity.ActivityManager;
 import naga.core.activity.ActivityRouter;
+import naga.core.bus.client.WebSocketBusOptions;
+import naga.core.format.FormatterRegistry;
+import naga.core.spi.platform.Platform;
 
 /**
  * @author Bruno Salmon
@@ -20,7 +25,7 @@ abstract class MongooseApplication implements Activity {
     @Override
     public void onCreate(ActivityContext context) {
         activityRouter = ActivityRouter.create(context)
-                .routeAndMountSubRouter("/", ContainerActivity::new, ActivityRouter.createSubRouter(context)
+                .routeAndMountSubRouter("/backend/", ContainerActivity::new, ActivityRouter.createSubRouter(context)
                         .route("/organizations", OrganizationsActivity::new)
                         .route("/event/:eventId/bookings", BookingsActivity::new)
                         .route("/cart/:cartUuid", CartActivity::new)
@@ -33,6 +38,8 @@ abstract class MongooseApplication implements Activity {
     }
 
     protected static void launchApplication(MongooseApplication mongooseApplication, String[] args) {
+        FormatterRegistry.registerFormatter("price", PriceFormatter.SINGLETON);
+        FormatterRegistry.registerFormatter("date", DateFormatter.SINGLETON);
         ActivityManager.launchApplication(mongooseApplication, args, DomainModelSnapshotLoader.getDataSourceModel());
     }
 }
