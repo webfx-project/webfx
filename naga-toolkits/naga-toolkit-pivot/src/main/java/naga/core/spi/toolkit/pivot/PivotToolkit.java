76b02bf077300a02df131569e76e93f44032cb23
Bruno Salmon <dev.salmonb@gmail.com>
2016-06-24 16:21:54 +0200

    Added Pivot implementation of Slider control (PivotSlider)

--- naga-toolkits/naga-toolkit-pivot/src/main/java/naga/core/spi/toolkit/pivot/PivotToolkit.java
+++ naga-toolkits/naga-toolkit-pivot/src/main/java/naga/core/spi/toolkit/pivot/PivotToolkit.java
@@ -2,18 +2,17 @@ package naga.core.spi.toolkit.pivot;
 
 import naga.core.spi.toolkit.Toolkit;
 import naga.core.spi.toolkit.controls.*;
+import naga.core.spi.toolkit.controls.Button;
+import naga.core.spi.toolkit.controls.Slider;
 import naga.core.spi.toolkit.layouts.HBox;
 import naga.core.spi.toolkit.layouts.VBox;
 import naga.core.spi.toolkit.layouts.VPage;
-import naga.core.spi.toolkit.pivot.controls.PivotButton;
-import naga.core.spi.toolkit.pivot.controls.PivotCheckBox;
-import naga.core.spi.toolkit.pivot.controls.PivotSearchBox;
-import naga.core.spi.toolkit.pivot.controls.PivotTable;
+import naga.core.spi.toolkit.pivot.controls.*;
 import naga.core.spi.toolkit.pivot.layouts.PivotHBox;
 import naga.core.spi.toolkit.pivot.layouts.PivotVBox;
 import naga.core.spi.toolkit.pivot.layouts.PivotVPage;
 import naga.core.spi.toolkit.pivot.layouts.PivotWindow;
-import org.apache.pivot.wtk.PushButton;
+import org.apache.pivot.wtk.*;
 
 
 /**
@@ -29,6 +28,7 @@ public class PivotToolkit extends Toolkit {
         registerNodeFactory(Table.class, PivotTable::new);
         registerNodeFactoryAndWrapper(CheckBox.class, PivotCheckBox::new, org.apache.pivot.wtk.Checkbox.class, PivotCheckBox::new);
         registerNodeFactoryAndWrapper(Button.class, PivotButton::new, PushButton.class, PivotButton::new);
+        registerNodeFactoryAndWrapper(Slider.class, PivotSlider::new, org.apache.pivot.wtk.Slider.class, PivotSlider::new);
         registerNodeFactory(ToggleSwitch.class, PivotCheckBox::new);
         registerNodeFactory(SearchBox.class, PivotSearchBox::new);
     }
