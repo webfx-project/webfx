c42db9ef1a23c1a5b08efecc0989b427cb282332
Bruno Salmon <dev.salmonb@gmail.com>
2016-06-28 17:37:56 +0200

    Fixed: GwtBarChart was not implementing the right interface

--- naga-toolkits/naga-toolkit-gwt/src/main/java/naga/core/spi/toolkit/gwt/charts/GwtBarChart.java
+++ naga-toolkits/naga-toolkit-gwt/src/main/java/naga/core/spi/toolkit/gwt/charts/GwtBarChart.java
@@ -2,12 +2,12 @@ package naga.core.spi.toolkit.gwt.charts;
 
 import com.google.gwt.user.client.ui.SimpleLayoutPanel;
 import com.googlecode.gwt.charts.client.ChartWidget;
-import naga.core.spi.toolkit.charts.LineChart;
+import naga.core.spi.toolkit.charts.BarChart;
 
 /**
  * @author Bruno Salmon
  */
-public class GwtBarChart extends GwtChart implements LineChart<SimpleLayoutPanel> {
+public class GwtBarChart extends GwtChart implements BarChart<SimpleLayoutPanel> {
 
     @Override
     protected ChartWidget createChartWidget() {
