0145c934a3617a4df83bce708901b803ed6f8f03
Bruno Salmon <dev.salmonb@gmail.com>
2016-06-28 21:20:45 +0200

    Fixed: GwtPieChart was not implementing the right interface

--- naga-toolkits/naga-toolkit-gwt/src/main/java/naga/core/spi/toolkit/gwt/charts/GwtPieChart.java
+++ naga-toolkits/naga-toolkit-gwt/src/main/java/naga/core/spi/toolkit/gwt/charts/GwtPieChart.java
@@ -2,12 +2,12 @@ package naga.core.spi.toolkit.gwt.charts;
 
 import com.google.gwt.user.client.ui.SimpleLayoutPanel;
 import com.googlecode.gwt.charts.client.ChartWidget;
-import naga.core.spi.toolkit.charts.LineChart;
+import naga.core.spi.toolkit.charts.PieChart;
 
 /**
  * @author Bruno Salmon
  */
-public class GwtPieChart extends GwtChart implements LineChart<SimpleLayoutPanel> {
+public class GwtPieChart extends GwtChart implements PieChart<SimpleLayoutPanel> {
 
     @Override
     protected ChartWidget createChartWidget() {
