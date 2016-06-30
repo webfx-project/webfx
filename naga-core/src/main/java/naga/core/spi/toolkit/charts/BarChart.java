0da7970efc0474a7397d8dd14d7939c6e2807ca9
Bruno Salmon <dev.salmonb@gmail.com>
2016-06-30 23:17:33 +0200

    Commented Chart interface and sub-interfaces regarding the expected format in DisplayResultSet

--- naga-core/src/main/java/naga/core/spi/toolkit/charts/BarChart.java
+++ naga-core/src/main/java/naga/core/spi/toolkit/charts/BarChart.java
@@ -1,6 +1,10 @@
 package naga.core.spi.toolkit.charts;
 
 /**
+ * Describes the bar chart, a two-axis chart that presents discrete data with rectangular bars.
+ *
+ * See {@link Chart} for an explanation of the data format.
+ *
  * @author Bruno Salmon
  */
 public interface BarChart<N> extends Chart<N> {
--- naga-core/src/main/java/naga/core/spi/toolkit/charts/PieChart.java
+++ naga-core/src/main/java/naga/core/spi/toolkit/charts/PieChart.java
@@ -1,6 +1,10 @@
 package naga.core.spi.toolkit.charts;
 
 /**
+ * Describes a chart that represents data in a form of a circle divided into triangular wedges called slices.
+ *
+ * See {@link Chart} for an explanation of the data format.
+ *
  * @author Bruno Salmon
  */
 public interface PieChart<N> extends Chart<N> {
