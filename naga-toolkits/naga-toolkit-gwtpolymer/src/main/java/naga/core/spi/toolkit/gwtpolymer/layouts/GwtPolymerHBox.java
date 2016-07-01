f2d92e563aa06391f54b9b2dc8ca0b1e384dd7c7
Bruno Salmon <dev.salmonb@gmail.com>
2016-07-01 22:32:37 +0200

    Made the GwtPolymer HBox and VBox use the LayoutFlowPane (originally defined in VPage)

--- naga-toolkits/naga-toolkit-gwtpolymer/src/main/java/naga/core/spi/toolkit/gwtpolymer/layouts/GwtPolymerHBox.java
+++ naga-toolkits/naga-toolkit-gwtpolymer/src/main/java/naga/core/spi/toolkit/gwtpolymer/layouts/GwtPolymerHBox.java
@@ -2,10 +2,10 @@ package naga.core.spi.toolkit.gwtpolymer.layouts;
 
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.StyleElement;
-import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.Widget;
 import com.vaadin.polymer.Polymer;
+import com.vaadin.polymer.PolymerWidget;
 import naga.core.spi.toolkit.gwt.node.GwtParent;
 import naga.core.spi.toolkit.layouts.HBox;
 
@@ -15,13 +15,13 @@ import naga.core.spi.toolkit.layouts.HBox;
 public class GwtPolymerHBox extends GwtParent<Panel> implements HBox<Panel, Widget> {
 
     public GwtPolymerHBox() {
-        this(new HTMLPanel("div", ""));
+        this(new LayoutFlowPanel());
     }
 
     public GwtPolymerHBox(Panel node) {
         super(node);
         node.getElement().addClassName("layout horizontal");
-        node.getElement().setAttribute("style", "100%");
+        //node.getElement().setAttribute("style", "height: 100%;");
         Polymer.importHref("iron-flex-layout/iron-flex-layout.html", o -> {
             Document doc = Document.get();
             StyleElement styleElement = doc.createStyleElement();
@@ -31,4 +31,13 @@ public class GwtPolymerHBox extends GwtParent<Panel> implements HBox<Panel, Widg
             return null;
         });
     }
+
+    @Override
+    protected Widget prepareChildWidget(Widget child) {
+        // This code should'nt be here but part of the UI building
+        if (!(child instanceof PolymerWidget)) // ex: not button but gauge and chart
+            child.addStyleName("flex");
+        return child;
+    }
+
 }
