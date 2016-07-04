9d3bce8d01b1bdf25457a701dc83d90487636a73
Bruno Salmon <dev.salmonb@gmail.com>
2016-07-04 11:10:23 +0200

    Added mongoose.css and OpenSans font in mongoose javafx resources with hooks to load them

--- mongoose/mongoose-backend/mongoose-backend-javafx/src/main/java/mongoose/backend/javafx/MongooseBackendJavaFxApplication.java
+++ mongoose/mongoose-backend/mongoose-backend-javafx/src/main/java/mongoose/backend/javafx/MongooseBackendJavaFxApplication.java
@@ -1,6 +1,8 @@
 package mongoose.backend.javafx;
 
+import javafx.scene.text.Font;
 import mongoose.application.MongooseBackendApplication;
+import naga.core.spi.toolkit.javafx.JavaFxToolkit;
 
 /**
  * @author Bruno Salmon
@@ -8,6 +10,19 @@ import mongoose.application.MongooseBackendApplication;
 public class MongooseBackendJavaFxApplication {
 
     public static void main(String[] args) {
+        JavaFxToolkit.setStartHook(() -> {
+            try {
+                double fontSize = 11;
+                Class applicationClass = MongooseBackendJavaFxApplication.class;
+                Font.loadFont(applicationClass.getResource("/fonts/OpenSans-Regular.ttf").toExternalForm(), fontSize);
+                Font.loadFont(applicationClass.getResource("/fonts/OpenSans-Bold.ttf").toExternalForm(), fontSize);
+                Font.loadFont(applicationClass.getResource("/fonts/OpenSans-Italic.ttf").toExternalForm(), fontSize);
+                Font.loadFont(applicationClass.getResource("/fonts/OpenSans-BoldItalic.ttf").toExternalForm(), fontSize);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        JavaFxToolkit.setSceneHook(scene -> scene.getStylesheets().addAll("css/mongoose.css"));
         MongooseBackendApplication.main(args);
     }
 }
