a2d9d9eb35618a2d972cb8985c7ea58bad7216df
Jean-Pierre <dev.alonsojp@gmail.com>
2016-07-05 16:40:41 +0200

    Renamed from prototype 'SystemSnapshot'

--- mongoose/mongoose-backend/mongoose-backend-javafx/src/main/java/mongoose/backend/javafx/SystemLookupFX.java
+++ mongoose/mongoose-backend/mongoose-backend-javafx/src/main/java/mongoose/backend/javafx/SystemLookupFX.java
@@ -1,6 +1,7 @@
-package mongoose.activities.tester.monitor.controller;
+package mongoose.backend.javafx;
 
 import com.sun.management.OperatingSystemMXBean;
+import mongoose.activities.tester.monitor.controller.SystemLookup;
 import mongoose.activities.tester.monitor.model.SysBean;
 
 import java.lang.management.ManagementFactory;
@@ -8,15 +9,13 @@ import java.lang.management.ManagementFactory;
 /**
  * @author Jean-Pierre Alonso.
  */
-public class SystemSnapshot {
-
+public class SystemLookupFX extends SystemLookup {
     private Runtime rt = Runtime.getRuntime();
     private OperatingSystemMXBean osMXBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
-    private SysBean sb;
 
-    public SysBean systemSnapshot () {
+    @Override
+    public SysBean snapshot() {
         sb = new SysBean();
-
         sb.setCommittedMem(osMXBean.getCommittedVirtualMemorySize());
         sb.setTotalMem(rt.totalMemory());
         sb.setFreeMem(rt.freeMemory());
