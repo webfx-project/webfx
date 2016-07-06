0bb5bded94da69754565ea5a9bb49212831bc7c6
Bruno Salmon <dev.salmonb@gmail.com>
2016-07-06 20:17:08 +0200

    Added missing MicroserviceContext class in recent commit

---
+++ naga-core/src/main/java/naga/core/activity/MicroserviceContext.java
@@ -0,0 +1,11 @@
+package naga.core.activity;
+
+/**
+ * @author Bruno Salmon
+ */
+class MicroserviceContext extends ActivityContextImpl {
+
+    MicroserviceContext() {
+        super(null);
+    }
+}
