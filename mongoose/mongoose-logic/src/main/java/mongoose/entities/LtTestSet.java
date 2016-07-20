48fbc642a8bb9a777b3c86eea438c9c408a10178
Jean-Pierre <dev.alonsojp@gmail.com>
2016-07-20 18:24:07 +0200

    Initial commit

---
+++ mongoose/mongoose-logic/src/main/java/mongoose/entities/LtTestSet.java
@@ -0,0 +1,15 @@
+package mongoose.entities;
+
+/**
+ * @author Jean-Pierre Alonso.
+ */
+public interface LtTestSet {
+
+    String getName();
+
+    void setName(String name);
+
+    String getComment();
+
+    void setComment(String comment);
+}
