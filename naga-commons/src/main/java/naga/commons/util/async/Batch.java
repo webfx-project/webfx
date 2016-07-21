14384f8bf884c289c60ee98c9d75a2251f903785
Bruno Salmon <dev.salmonb@gmail.com>
2016-07-21 14:52:34 +0200

    Added executeIfSingularBatch() method in Batch class

--- naga-commons/src/main/java/naga/commons/util/async/Batch.java
+++ naga-commons/src/main/java/naga/commons/util/async/Batch.java
@@ -50,7 +50,7 @@ public class Batch<A> {
     }
 
     public <R> Future<Batch<R>> executeSerial(AsyncFunction<A, R> asyncFunction, Class<R> expectedResultClass, Future<Batch<R>> future) {
-        int n = array.length, i = 0;
+        int n = array.length;
         R[] results = (R[]) Array.newInstance(expectedResultClass, n);
         Unit<Integer> responseCounter = new Unit<>(0);
         Unit<Handler<AsyncResult<R>>> handlerUnit = new Unit<>();
@@ -72,4 +72,32 @@ public class Batch<A> {
         return future;
     }
 
+    public <R> Future<Batch<R>> executeIfSingularBatch(AsyncFunction<A, R> asyncFunction, Class<R> expectedResultClass) {
+        if (array.length > 1)
+            return null;
+        Future<Batch<R>> future = Future.future();
+        executeIfSingularBatch(asyncFunction, expectedResultClass, future);
+        return future;
+    }
+
+    public <R> boolean executeIfSingularBatch(AsyncFunction<A, R> asyncFunction, Class<R> expectedResultClass, Future<Batch<R>> future) {
+        int n = array.length;
+        if (n > 1)
+            return false;
+        R[] results = (R[]) Array.newInstance(expectedResultClass, n);
+        if (n == 0)
+            future.complete(new Batch<>(results));
+        else
+            asyncFunction.apply(array[0]).setHandler(asyncResult -> {
+                if (!future.isComplete()) {
+                    if (asyncResult.failed())
+                        future.fail(asyncResult.cause());
+                    else {
+                        results[0] = asyncResult.result();
+                        future.complete(new Batch<>(results));
+                    }
+                }
+            });
+        return true;
+    }
 }
