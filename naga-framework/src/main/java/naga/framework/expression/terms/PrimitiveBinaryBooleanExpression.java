5b5de3b991e8ec1084e786d81974f5355bae0d26
Bruno Salmon <dev.salmonb@gmail.com>
2016-08-12 18:41:37 +0200

    Fixed binary expression evaluation: returning evaluateObject() instead of null when both arguments are null (to correctly evaluate expressions such as "x == null" or "x != null")

--- naga-framework/src/main/java/naga/framework/expression/terms/PrimitiveBinaryBooleanExpression.java
+++ naga-framework/src/main/java/naga/framework/expression/terms/PrimitiveBinaryBooleanExpression.java
@@ -29,44 +29,45 @@ public abstract class PrimitiveBinaryBooleanExpression<T> extends BinaryBooleanE
         if (leftPrimType != null) {
             leftValue = dataReader.prepareValueBeforeTypeConversion(leftValue, leftPrimType);
             rightValue = dataReader.prepareValueBeforeTypeConversion(rightValue, leftPrimType);
-            switch (leftPrimType) {
-                case BOOLEAN:
-                    return evaluateBoolean(Booleans.booleanValue(leftValue), Booleans.booleanValue(rightValue));
-                case INTEGER:
-                    return evaluateInteger(Numbers.toInteger(leftValue), Numbers.toInteger(rightValue));
-                case LONG:
-                    return evaluateLong(Numbers.toLong(leftValue), Numbers.toLong(rightValue));
-                case FLOAT:
-                    return evaluateFloat(Numbers.toFloat(leftValue), Numbers.toFloat(rightValue));
-                case DOUBLE:
-                    return evaluateDouble(Numbers.toDouble(leftValue), Numbers.toDouble(rightValue));
-                case STRING:
-                    return evaluateString(Strings.toString(leftValue), Strings.toString(rightValue));
-                case DATE:
-                    return evaluateDate(Dates.toInstant(leftValue), Dates.toInstant(rightValue));
-            }
+            if (leftValue != null || rightValue != null)
+                switch (leftPrimType) {
+                    case BOOLEAN:
+                        return evaluateBoolean(Booleans.booleanValue(leftValue), Booleans.booleanValue(rightValue));
+                    case INTEGER:
+                        return evaluateInteger(Numbers.toInteger(leftValue), Numbers.toInteger(rightValue));
+                    case LONG:
+                        return evaluateLong(Numbers.toLong(leftValue), Numbers.toLong(rightValue));
+                    case FLOAT:
+                        return evaluateFloat(Numbers.toFloat(leftValue), Numbers.toFloat(rightValue));
+                    case DOUBLE:
+                        return evaluateDouble(Numbers.toDouble(leftValue), Numbers.toDouble(rightValue));
+                    case STRING:
+                        return evaluateString(Strings.toString(leftValue), Strings.toString(rightValue));
+                    case DATE:
+                        return evaluateDate(Dates.toInstant(leftValue), Dates.toInstant(rightValue));
+                }
         }
         return evaluateObject(leftValue, rightValue);
     }
 
     protected Boolean evaluateBoolean(Boolean a, Boolean b) {
-        return a == null | b == null ? null : evaluateBoolean(a.booleanValue(), b.booleanValue());
+        return a == null || b == null ? null : evaluateBoolean(a.booleanValue(), b.booleanValue());
     }
 
     protected Boolean evaluateInteger(Integer a, Integer b) {
-        return a == null | b == null ? null : evaluateInteger(a.intValue(), b.intValue());
+        return a == null || b == null ? null : evaluateInteger(a.intValue(), b.intValue());
     }
 
     protected Boolean evaluateLong(Long a, Long b) {
-        return a == null | b == null ? null : evaluateLong(a.longValue(), b.longValue());
+        return a == null || b == null ? null : evaluateLong(a.longValue(), b.longValue());
     }
 
     protected Boolean evaluateFloat(Float a, Float b) {
-        return a == null | b == null ? null : evaluateFloat(a.floatValue(), b.floatValue());
+        return a == null || b == null ? null : evaluateFloat(a.floatValue(), b.floatValue());
     }
 
     protected Boolean evaluateDouble(Float a, Float b) {
-        return a == null | b == null ? null : evaluateDouble(a.doubleValue(), b.doubleValue());
+        return a == null || b == null ? null : evaluateDouble(a.doubleValue(), b.doubleValue());
     }
 
     protected Boolean evaluateString(String a, String b) { return evaluateObject(a, b);}
