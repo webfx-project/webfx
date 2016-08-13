4e36eb98756102f94d2a44d6d0be9e904a12b34c
Bruno Salmon <dev.salmonb@gmail.com>
2016-08-13 13:20:50 +0200

    Changed OrganizationActivity to display icons for type, country and continent

--- mongoose/mongoose-logic/src/main/java/mongoose/activities/organizations/OrganizationsActivity.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/organizations/OrganizationsActivity.java
@@ -58,8 +58,8 @@ public class OrganizationsActivity extends PresentationActivity<OrganizationsVie
                 // Limit condition
                 .combine(pm.limitProperty(), "{limit: '100'}")
                 .setExpressionColumns("[" +
-                        "{label: 'Name', expression: 'name + ` (` + type.code + `)`'}," +
-                        "{label: 'Country', expression: 'country.(name + ` (` + continent.name + `)`)'}" +
+                        "{label: 'Name', expression: '[icon, name + ` (` + type.code + `)`]'}," +
+                        "'country', 'country.continent'" +
                         "]")
                 .applyDomainModelRowStyle()
                 .displayResultSetInto(pm.organizationsDisplayResultSetProperty())
