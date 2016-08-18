738057f0679593140409df9795c5e490dca74367
Bruno Salmon <dev.salmonb@gmail.com>
2016-08-18 16:37:07 +0200

    Added forEach() and nulls utility methods to Arrays

--- naga-commons/src/main/java/naga/commons/util/Arrays.java
+++ naga-commons/src/main/java/naga/commons/util/Arrays.java
@@ -1,10 +1,12 @@
 package naga.commons.util;
 
 import naga.commons.util.collection.Collections;
+import naga.commons.util.function.Consumer;
 
+import java.util.ArrayList;
 import java.util.List;
 
-/*
+/**
  * @author Bruno Salmon
  */
 
@@ -33,4 +35,47 @@ public class Arrays {
         return array == null ? null : Collections.toString(java.util.Arrays.asList(array).iterator());
     }
 
+    public static <T> void forEach(T[] array, Consumer<T> consumer) {
+        for (T element : array)
+            consumer.accept(element);
+    }
+
+    public static <T> void forEachNonNull(T[] array, Consumer<T> consumer) {
+        for (T element : array)
+            if (element != null)
+                consumer.accept(element);
+    }
+
+    public static <T> List<T> collectNonNulls(T[] array, List<T> list) {
+        forEachNonNull(array, list::add);
+        return list;
+    }
+
+    public static <T> List<T> nonNullsAsList(T[] array) {
+        return collectNonNulls(array, new ArrayList<>(length(array)));
+    }
+
+    public static <T> int indexOfNull(T[] array) {
+        int n = length(array);
+        for (int i = 0; i < n; i++)
+            if (array[i] == null)
+                return i;
+        return -1;
+    }
+
+    public static <T> boolean allNonNulls(T[] array) {
+        return indexOfNull(array) == -1;
+    }
+
+    public static <T> boolean hasNulls(T[] array) {
+        return !allNonNulls(array);
+    }
+
+    public static <T> T getValue(T[] array, int index) {
+        return array == null ? null : array[index];
+    }
+
+    public static <T> String getString(T[] array, int index) {
+        return Strings.toString(getValue(array, index));
+    }
 }
