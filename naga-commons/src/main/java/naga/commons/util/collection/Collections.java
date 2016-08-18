a057903a97ce116896abf739edac34c2c9ba7333
Bruno Salmon <dev.salmonb@gmail.com>
2016-08-18 16:34:58 +0200

    Added forEach() and convert() to Collections

--- naga-commons/src/main/java/naga/commons/util/collection/Collections.java
+++ naga-commons/src/main/java/naga/commons/util/collection/Collections.java
@@ -1,11 +1,30 @@
 package naga.commons.util.collection;
 
+import naga.commons.util.function.Consumer;
+import naga.commons.util.function.Converter;
+
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author Bruno Salmon
  */
 public class Collections {
+
+    public static <T> void forEach(Collection<T> collection, Consumer<T> consumer) {
+        // collection.forEach(consumer); // Not GWT compilable for now
+        for (T element : collection)
+            consumer.accept(element);
+    }
+
+    public static <A, B> List<B> convert(Collection<A> aList, Converter<A, B> aToBConverter) {
+        // return aList.stream().map(aToBConverter::convert).collect(Collectors.toList()); // Not GWT compilable for now
+        List<B> bList = new ArrayList<>(aList.size());
+        forEach(aList, a -> bList.add(aToBConverter.convert(a)));
+        return bList;
+    }
 
     public static String toString(Iterator it) {
         if (!it.hasNext())
@@ -21,5 +40,4 @@ public class Collections {
             sb.append(',').append(' ');
         }
     }
-
 }
