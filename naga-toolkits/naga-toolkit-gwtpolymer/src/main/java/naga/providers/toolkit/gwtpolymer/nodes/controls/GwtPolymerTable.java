878ff4118040095ca9f90ff2c11d63343a517c29
Bruno Salmon <dev.salmonb@gmail.com>
2016-08-18 16:57:53 +0200

    Updated GwtPolymerTable implementation to use the new Cell API

--- naga-toolkits/naga-toolkit-gwtpolymer/src/main/java/naga/providers/toolkit/gwtpolymer/nodes/controls/GwtPolymerTable.java
+++ naga-toolkits/naga-toolkit-gwtpolymer/src/main/java/naga/providers/toolkit/gwtpolymer/nodes/controls/GwtPolymerTable.java
@@ -4,14 +4,11 @@ import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.client.JsArray;
 import com.google.gwt.core.client.JsArrayInteger;
 import com.google.gwt.dom.client.*;
+import com.google.gwt.user.client.ui.UIObject;
 import com.vaadin.polymer.Polymer;
 import com.vaadin.polymer.elemental.Function;
 import com.vaadin.polymer.vaadin.*;
 import com.vaadin.polymer.vaadin.widget.VaadinGrid;
-import naga.commons.type.ArrayType;
-import naga.commons.type.Type;
-import naga.commons.type.Types;
-import naga.commons.util.Arrays;
 import naga.commons.util.Strings;
 import naga.providers.toolkit.gwt.nodes.GwtSelectableDisplayResultSetNode;
 import naga.toolkit.display.DisplayColumn;
@@ -19,6 +16,10 @@ import naga.toolkit.display.DisplayResultSet;
 import naga.toolkit.display.DisplaySelection;
 import naga.toolkit.display.Label;
 import naga.toolkit.properties.markers.SelectionMode;
+import naga.toolkit.cell.renderers.CellRenderer;
+import naga.toolkit.cell.GridFiller;
+import naga.toolkit.cell.ImageTextGridAdapter;
+import naga.toolkit.spi.nodes.GuiNode;
 import naga.toolkit.spi.nodes.controls.Table;
 
 /**
@@ -85,104 +86,50 @@ public class GwtPolymerTable extends GwtSelectableDisplayResultSetNode<VaadinGri
         });
     }
 
-    private int rowStyleColumnIndex = -1;
+    private final GridFiller gridFiller = new GridFiller<Cell>(new ImageTextGridAdapter<Cell, UIObject>() {
+        @Override
+        public void setCellContent(Cell cell, GuiNode<UIObject> content, DisplayColumn displayColumn) {
+            Element img = content.unwrapToNativeNode().getElement();
+            img.setAttribute("style", "margin-left: auto; margin-right: auto;");
+            Element cellElement = cell.getElement().cast();
+            cellElement.removeAllChildren();
+            cellElement.appendChild(img);
+        }
 
-    @Override
-    protected void syncVisualDisplayResult(DisplayResultSet rs) {
-        Polymer.ready(node.getElement(), o -> {
-            //Platform.log("Updating GwtPolymerTable: " + rs.getRowCount() + " rows & " + rs.getColumnCount() + " columns");
-            JsArray gridColumns = node.getColumns();
-            rowStyleColumnIndex = -1;
-            int columnCount = rs.getColumnCount();
-            int rowCount = rs.getRowCount();
-            Header header = node.getHeader();
-            header.setRowCount(1);
-            for (int columnIndex = 0, gridColumnIndex = 0; columnIndex < columnCount; columnIndex++) {
-                DisplayColumn displayColumn = rs.getColumns()[columnIndex];
-                String role = displayColumn.getRole();
-                if ("style".equals(role))
-                    rowStyleColumnIndex = columnIndex;
-                else if (role == null) {
-                    Column gridColumn;
-                    if (gridColumnIndex < gridColumns.length())
-                        gridColumn = gridColumns.get(gridColumnIndex).cast();
-                    else
-                        node.addColumn(gridColumn = JavaScriptObject.createObject().cast(), null);
-                    HeaderAndFooterCell headerCell = header.getCell(0, gridColumnIndex);
-                    Label label = displayColumn.getLabel();
-                    SpanElement headerText = null;
-                    if (label.getText() != null) {
-                        headerText = Document.get().createSpanElement();
-                        headerText.appendChild(Document.get().createTextNode(label.getText()));
-                    }
-                    ImageElement headerIcon = null;
-                    if (label.getIconPath() != null) {
-                        headerIcon = Document.get().createImageElement();
-                        headerIcon.setSrc(label.getIconPath());
-                    }
-                    Node headerContent;
-                    if (headerText == null) {
-                        if (headerIcon != null)
-                            headerIcon.setAttribute("style", "margin-left: auto; margin-right: auto;");
-                        headerContent = headerIcon;
-                    } else if (headerIcon == null) {
-                        headerText.setAttribute("style", "margin-left: auto; margin-right: auto;");
-                        headerContent = headerText;
-                    } else {
-                        headerIcon.setAttribute("style", "vertical-align: middle; margin-right: 5px;");
-                        headerText.setAttribute("style", "vertical-align: middle;");
-                        SpanElement span = Document.get().createSpanElement();
-                        span.setAttribute("style", "width: 100%; text-align: center;");
-                        span.appendChild(headerIcon);
-                        span.appendChild(headerText);
-                        headerContent = span;
-                    }
-                    headerCell.setContent(headerContent);
-                    Double prefWidth = displayColumn.getPrefWidth();
-                    if (prefWidth != null) {
-                        if (label.getText() != null)
-                            prefWidth = prefWidth * 2.75; // factor compared to JavaFx style (temporary hardcoded)
-                        prefWidth = prefWidth + 10; // because of the 5px left and right padding
-                        gridColumn.setMinWidth(prefWidth);
-                        gridColumn.setMaxWidth(prefWidth);
-                        gridColumn.setWidth(prefWidth);
-                    }
-                    final int colIndex = columnIndex;
-                    Type type = displayColumn.getType();
-                    boolean isArray = Types.isArrayType(type);
-                    boolean isImageAndText;
-                    if (isArray) {
-                        Type[] types = ((ArrayType) type).getTypes();
-                        isImageAndText = Arrays.length(types) == 2 && Types.isImageType(types[0]);
-                    } else
-                        isImageAndText = false;
-                    boolean isImage = !isArray && Types.isImageType(type);
-                    gridColumn.setRenderer(oCell -> {
-                        Cell cell = (Cell) oCell;
-                        Row row = cell.getRow().cast();
-                        int rowIndex = (int) row.getIndex();
-                        Object value = rs.getValue(rowIndex, colIndex);
-                        String innerHtml;
-                        if (isImage)
-                            innerHtml = value == null ? null : "<img src='" + Strings.toString(value) + "' style='margin-left: auto; margin-right: auto;'/>";
-                        else if (isImageAndText) {
-                            Object[] array = (Object[]) value;
-                            innerHtml = Arrays.length(array) < 2 ? null : "<span><img src='" + Strings.toString(array[0]) + "' style='vertical-align: middle; margin-right: 5px;'/><span style='vertical-align: middle;'>" + Strings.toString(array[1]) + "</span></span>";
-                        } else if (isArray) {
-                            innerHtml = null; // TODO
-                        } else { // default case (presumably text)
-                            String style = "overflow: hidden; text-overflow: ellipsis; width: 100%;";
-                            if (displayColumn.getTextAlign() != null)
-                                style += " text-align: " + displayColumn.getTextAlign() + ";";
-                            innerHtml = value == null ? null : "<span style='" + style + "'>" + Strings.toString(value) + "</span>";
-                        }
-                        cell.getElement().<Element>cast().setInnerHTML(innerHtml);
-                        return null;
-                    });
-                    gridColumnIndex++;
-                }
-            }
+        @Override
+        public void setCellTextContent(Cell cell, String text, DisplayColumn displayColumn) {
+            String style = "overflow: hidden; text-overflow: ellipsis; width: 100%;";
+            if (displayColumn.getTextAlign() != null)
+                style += " text-align: " + displayColumn.getTextAlign() + ";";
+            String innerHtml = text == null ? null : "<span style='" + style + "'>" + text + "</span>";
+            cell.getElement().<Element>cast().setInnerHTML(innerHtml);
+        }
+
+        @Override
+        public void setCellImageAndTextContent(Cell cell, GuiNode<UIObject> image, String text, DisplayColumn displayColumn) {
+            Document document = Document.get();
+            SpanElement span = document.createSpanElement();
+            Element img = image.unwrapToNativeNode().getElement();
+            img.setAttribute("style", "vertical-align: middle; margin-right: 5px;");
+            span.appendChild(img);
+            SpanElement textElement = document.createSpanElement();
+            textElement.setInnerText(text);
+            textElement.setAttribute("style", "vertical-align: middle;");
+            span.appendChild(textElement);
+            Element cellElement = cell.getElement().cast();
+            cellElement.removeAllChildren();
+            cellElement.appendChild(span);
+        }
+    }) {
+
+        private JsArray gridColumns;
+
+        @Override
+        public void fillGrid(DisplayResultSet rs) {
+            gridColumns = node.getColumns();
+            super.fillGrid(rs);
             // Setting items to an unfilled (but correctly sized) javascript array as data fetching is actually done in the column renderer
+            int rowCount = rs.getRowCount();
             node.setItems(JavaScriptObject.createArray(rowCount));
             node.setSize(rowCount);
             node.setVisibleRows(rowCount); // This makes the grid height fit with the number of rows (no scroll bar)
@@ -194,36 +141,80 @@ public class GwtPolymerTable extends GwtSelectableDisplayResultSetNode<VaadinGri
                 node.addSelectedItemsChangedHandler(event -> syncToolkitDisplaySelection());
                 syncHandlerInstalled = true;
             }
-            //Platform.log("GwtPolymerTable updated");
+        }
+
+        @Override
+        protected void setUpGridColumn(int gridColumnIndex, DisplayColumn displayColumn, CellRenderer cellRenderer, int rsColumnIndex, DisplayResultSet rs) {
+            Column gridColumn;
+            if (gridColumnIndex < gridColumns.length())
+                gridColumn = gridColumns.get(gridColumnIndex).cast();
+            else
+                node.addColumn(gridColumn = JavaScriptObject.createObject().cast(), null);
+            HeaderAndFooterCell headerCell = node.getHeader().getCell(0, gridColumnIndex);
+            Label label = displayColumn.getLabel();
+            SpanElement headerText = null;
+            if (label.getText() != null) {
+                headerText = Document.get().createSpanElement();
+                headerText.appendChild(Document.get().createTextNode(label.getText()));
+            }
+            ImageElement headerIcon = null;
+            if (label.getIconPath() != null) {
+                headerIcon = Document.get().createImageElement();
+                headerIcon.setSrc(label.getIconPath());
+            }
+            Node headerContent;
+            if (headerText == null) {
+                if (headerIcon != null)
+                    headerIcon.setAttribute("style", "margin-left: auto; margin-right: auto;");
+                headerContent = headerIcon;
+            } else if (headerIcon == null) {
+                headerText.setAttribute("style", "margin-left: auto; margin-right: auto;");
+                headerContent = headerText;
+            } else {
+                headerIcon.setAttribute("style", "vertical-align: middle; margin-right: 5px;");
+                headerText.setAttribute("style", "vertical-align: middle;");
+                SpanElement span = Document.get().createSpanElement();
+                span.setAttribute("style", "width: 100%; text-align: center;");
+                span.appendChild(headerIcon);
+                span.appendChild(headerText);
+                headerContent = span;
+            }
+            headerCell.setContent(headerContent);
+            Double prefWidth = displayColumn.getPrefWidth();
+            if (prefWidth != null) {
+                if (label.getText() != null)
+                    prefWidth = prefWidth * 2.75; // factor compared to JavaFx style (temporary hardcoded)
+                prefWidth = prefWidth + 10; // because of the 5px left and right padding
+                gridColumn.setMinWidth(prefWidth);
+                gridColumn.setMaxWidth(prefWidth);
+                gridColumn.setWidth(prefWidth);
+            }
+            gridColumn.setRenderer(oCell -> {
+                Cell cell = (Cell) oCell;
+                Row row = cell.getRow().cast();
+                int rowIndex = (int) row.getIndex();
+                Object value = rs.getValue(rowIndex, rsColumnIndex);
+                fillCell(cell, value,cellRenderer, displayColumn);
+                return null;
+            });
+        }
+    };
+
+    @Override
+    protected void syncVisualDisplayResult(DisplayResultSet rs) {
+        Polymer.ready(node.getElement(), o -> {
+            gridFiller.fillGrid(rs);
             return null;
         });
     }
 
     private Function createRowClassGenerator() {
         return oRow -> {
-            DisplayResultSet rs = getDisplayResultSet();
-            if (rs == null)
-                return null;
             Row row = (Row) oRow;
             int rowIndex = (int) row.getIndex();
-            int columnIndex = rowStyleColumnIndex;
-            if (rowIndex < 0 || rowIndex >= rs.getRowCount() || columnIndex < 0 || columnIndex >= rs.getColumnCount())
-                return null;
-            Object value = rs.getValue(rowIndex, rowStyleColumnIndex);
-            if (!(value instanceof Object[]))
-                return null;
-            Object[] styleClassesArray = (Object[]) value;
-            StringBuilder sb = new StringBuilder();
-            for (Object styleClass : styleClassesArray) {
-                if (styleClass != null) {
-                    if (sb.length() > 0)
-                        sb.append(' ');
-                    sb.append(styleClass);
-                }
-            }
-            String styleClasses = sb.toString().trim();
+            String styleClasses = gridFiller.getRowStyle(rowIndex);
             // Note: returning an empty string makes the vaadin grid run into an infinite loop!
-            return styleClasses.isEmpty() ? null : styleClasses; //  Must return null instead of an empty string
+            return Strings.isEmpty(styleClasses) ? null : styleClasses; //  Must return null instead of an empty string
         };
     }
 }
