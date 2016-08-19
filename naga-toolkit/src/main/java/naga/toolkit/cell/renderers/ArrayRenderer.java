67282c378e9bab44da583f116e77e53782fc6117
Bruno Salmon <dev.salmonb@gmail.com>
2016-08-19 23:23:08 +0200

    Exposed a public static method in ArrayRenderer to work with GridPane

--- naga-toolkit/src/main/java/naga/toolkit/cell/renderers/ArrayRenderer.java
+++ naga-toolkit/src/main/java/naga/toolkit/cell/renderers/ArrayRenderer.java
@@ -23,15 +23,17 @@ public class ArrayRenderer<N> implements ValueRenderer<N> {
 
     @Override
     public GuiNode<N> renderCellValue(Object value) {
+        return renderCellValue(value, valueRenderers, collator);
+    }
+
+    public static <N> GuiNode<N> renderCellValue(Object value, ValueRenderer[] valueRenderers, NodeCollator<N> collator) {
         GuiNode<N>[] nodes = null;
         if (value instanceof Object[]) {
             Object[] array = (Object[]) value;
-            int n = Arrays.length(array);
-            if (n == Arrays.length(valueRenderers)) {
-                nodes = new GuiNode[n];
-                for (int i = 0; i < n; i++)
-                    nodes[i] = valueRenderers[i].renderCellValue(array[i]);
-            }
+            int n = Math.min(Arrays.length(array), Arrays.length(valueRenderers));
+            nodes = new GuiNode[n];
+            for (int i = 0; i < n; i++)
+                nodes[i] = valueRenderers[i].renderCellValue(array[i]);
         }
         return collator.collateNodes(nodes);
     }
