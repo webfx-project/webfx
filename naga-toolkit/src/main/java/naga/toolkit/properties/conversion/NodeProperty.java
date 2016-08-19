b7748354f6d261af1ddb50ea0da0b0c5d3f7518f
Bruno Salmon <dev.salmonb@gmail.com>
2016-08-19 23:25:10 +0200

    Updated FxVPage to embed the center node in a ScrollPane when necessary

--- naga-toolkit/src/main/java/naga/toolkit/properties/conversion/NodeProperty.java
+++ naga-toolkit/src/main/java/naga/toolkit/properties/conversion/NodeProperty.java
@@ -1,6 +1,7 @@
 package naga.toolkit.properties.conversion;
 
 import javafx.beans.property.Property;
+import naga.commons.util.function.Converter;
 import naga.toolkit.spi.Toolkit;
 import naga.toolkit.spi.nodes.GuiNode;
 
@@ -10,6 +11,10 @@ import naga.toolkit.spi.nodes.GuiNode;
 public class NodeProperty<N> extends ConvertedProperty<GuiNode<N>, N> {
 
     public NodeProperty(Property<N> property) {
-        super(property, node -> Toolkit.unwrapToNativeNode(node), fxNode -> Toolkit.get().wrapNativeNode(fxNode));
+        this(property, node -> node);
+    }
+
+    public NodeProperty(Property<N> property, Converter<N, N> unwrapConverter) {
+        super(property, node -> unwrapConverter.convert(Toolkit.unwrapToNativeNode(node)), Toolkit.get()::wrapNativeNode);
     }
 }
--- naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/nodes/layouts/FxVPage.java
+++ naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/nodes/layouts/FxVPage.java
@@ -2,12 +2,14 @@ package naga.providers.toolkit.javafx.nodes.layouts;
 
 import javafx.beans.property.Property;
 import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
 import naga.providers.toolkit.javafx.nodes.FxNode;
-import naga.toolkit.spi.nodes.layouts.VPage;
-import naga.toolkit.spi.nodes.GuiNode;
 import naga.toolkit.properties.conversion.NodeProperty;
+import naga.toolkit.spi.nodes.GuiNode;
+import naga.toolkit.spi.nodes.layouts.VPage;
 
 /**
  * @author Bruno Salmon
@@ -38,8 +40,22 @@ public class FxVPage extends FxNode<BorderPane> implements VPage<BorderPane, Nod
     private NodeProperty<Node> centerProperty;
     @Override
     public Property<GuiNode<Node>> centerProperty() {
-        if (centerProperty == null) centerProperty = new NodeProperty<>(node.centerProperty());
+        if (centerProperty == null) centerProperty = new NodeProperty<>(node.centerProperty(), FxVPage::embedInScrollPaneWhenNecessary);
         return centerProperty;
+    }
+
+    private static Node embedInScrollPaneWhenNecessary(Node node) {
+        if (node == null || node instanceof TableView || node instanceof BorderPane)
+            return node;
+        Object nodeScrollPane = node.getProperties().get("scrollPane");
+        if (nodeScrollPane instanceof ScrollPane)
+            return (Node) nodeScrollPane;
+        ScrollPane scrollPane = new ScrollPane(node);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setFitToHeight(true);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        node.getProperties().put("scrollPane", scrollPane);
+        return scrollPane;
     }
 
     private NodeProperty<Node> footerProperty;
