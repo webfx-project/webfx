718092062320bddf119af5ad36e9d6cc42151a1c
Bruno Salmon <dev.salmonb@gmail.com>
2016-08-22 19:36:27 +0200

    Made OrganizationActivity the default page

--- mongoose/mongoose-logic/src/main/java/mongoose/client/backend/MongooseBackendApplication.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/client/backend/MongooseBackendApplication.java
@@ -9,7 +9,7 @@ public class MongooseBackendApplication extends MongooseApplication {
 
     @Override
     public void onStart() {
-        uiRouter.setDefaultInitialHistoryPath("/event/120/bookings");
+        uiRouter.setDefaultInitialHistoryPath("/organizations");
         super.onStart();
     }
 
