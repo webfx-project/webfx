d00368176f0d203f3f9729d1a1d60650e37d2e03
Bruno Salmon <dev.salmonb@gmail.com>
2016-08-31 09:31:45 +0200

    Added placeholder in Swing search box

--- naga-toolkits/naga-toolkit-swing/src/main/java/naga/providers/toolkit/swing/nodes/controls/SwingSearchBox.java
+++ naga-toolkits/naga-toolkit-swing/src/main/java/naga/providers/toolkit/swing/nodes/controls/SwingSearchBox.java
@@ -3,6 +3,8 @@ package naga.providers.toolkit.swing.nodes.controls;
 import javafx.beans.property.Property;
 import javafx.beans.property.SimpleObjectProperty;
 import naga.providers.toolkit.swing.nodes.SwingNode;
+import naga.providers.toolkit.swing.util.JPlaceholderTextField;
+import naga.providers.toolkit.swing.util.StyleUtil;
 import naga.toolkit.spi.nodes.controls.SearchBox;
 
 import javax.swing.*;
@@ -15,11 +17,12 @@ import javax.swing.event.DocumentListener;
 public class SwingSearchBox extends SwingNode<JTextField> implements SearchBox<JTextField> {
 
     public SwingSearchBox() {
-        this(new JTextField());
+        this(new JPlaceholderTextField());
     }
 
     public SwingSearchBox(JTextField search) {
         super(search);
+        search.setFont(StyleUtil.getFont(false, false));
         search.getDocument().addDocumentListener(new DocumentListener() {
             public void changedUpdate(DocumentEvent e) { updateProperty(); }
             public void removeUpdate(DocumentEvent e) { updateProperty(); }
@@ -28,7 +31,8 @@ public class SwingSearchBox extends SwingNode<JTextField> implements SearchBox<J
                 textProperty.setValue(search.getText());
             }
         });
-        //placeholderProperty.addListener((observable, oldValue, newValue) -> node.setPlaceholder(newValue));
+        if (search instanceof JPlaceholderTextField)
+            placeholderProperty.addListener((observable, oldValue, newValue) -> ((JPlaceholderTextField) node).setPlaceholder(newValue));
     }
 
     private final Property<String> textProperty = new SimpleObjectProperty<>();
