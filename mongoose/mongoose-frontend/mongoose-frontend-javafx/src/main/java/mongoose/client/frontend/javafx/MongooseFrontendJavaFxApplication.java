090acfb6579607ec1f4b054656d3f0040cb98f0a
Bruno Salmon <dev.salmonb@gmail.com>
2016-09-03 21:15:33 +0200

    Reactivated the mongoose-frontend module and updated javafx and gwt sub-modules

--- mongoose/mongoose-frontend/mongoose-frontend-javafx/src/main/java/mongoose/client/frontend/javafx/MongooseFrontendJavaFxApplication.java
+++ mongoose/mongoose-frontend/mongoose-frontend-javafx/src/main/java/mongoose/client/frontend/javafx/MongooseFrontendJavaFxApplication.java
@@ -1,8 +1,18 @@
 package mongoose.client.frontend.javafx;
 
-import javafx.scene.text.Font;
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import mongoose.client.frontend.MongooseFrontendApplication;
+import naga.framework.activity.client.UiApplicationContext;
+import naga.framework.ui.rx.RxScheduler;
+import naga.framework.ui.rx.RxUi;
+import naga.platform.bus.call.PendingBusCall;
 import naga.providers.toolkit.javafx.JavaFxToolkit;
+import naga.providers.toolkit.javafx.util.FxImageStore;
+import naga.toolkit.spi.Toolkit;
+import naga.toolkit.spi.nodes.GuiNode;
+import rx.Observable;
 
 /**
  * @author Bruno Salmon
@@ -13,24 +23,38 @@ public class MongooseFrontendJavaFxApplication {
         installJavaFxHooks();
         // Once hooks are set, we can start the application
         MongooseFrontendApplication.main(args);
+        Observable.combineLatest(
+                RxUi.observe(UiApplicationContext.getUiApplicationContext().windowBoundProperty()),
+                RxUi.observe(PendingBusCall.pendingCallsCountProperty()),
+                (windowBound, pendingCallsCount) -> !windowBound || pendingCallsCount > 0)
+                .observeOn(RxScheduler.UI_SCHEDULER)
+                .subscribe(MongooseFrontendJavaFxApplication::setLoadingSpinnerVisible);
     }
 
-    public static void installJavaFxHooks() {
-        // Setting JavaFx start hook to load mongoose.application.backend.javafx.fonts
-        JavaFxToolkit.setStartHook(() -> {
-            try {
-                double fontSize = 11;
-                Class applicationClass = MongooseFrontendJavaFxApplication.class;
-                Font.loadFont(applicationClass.getResource("/mongoose/client/backend/javafx/fonts/OpenSans-Regular.ttf").toExternalForm(), fontSize);
-                Font.loadFont(applicationClass.getResource("/mongoose/client/backend/javafx/fonts/OpenSans-Bold.ttf").toExternalForm(), fontSize);
-                Font.loadFont(applicationClass.getResource("/mongoose/client/backend/javafx/fonts/OpenSans-Italic.ttf").toExternalForm(), fontSize);
-                Font.loadFont(applicationClass.getResource("/mongoose/client/backend/javafx/fonts/OpenSans-BoldItalic.ttf").toExternalForm(), fontSize);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
+    private static void installJavaFxHooks() {
         // Setting JavaFx scene hook to apply the mongoose css file
-        JavaFxToolkit.setSceneHook(scene -> scene.getStylesheets().addAll("mongoose/client/backend/javafx/css/mongoose.css"));
+        JavaFxToolkit.setSceneHook(scene -> scene.getStylesheets().addAll("mongoose/client/java/css/mongoose.css"));
     }
 
+    private static ImageView spinner;
+
+    private static void setLoadingSpinnerVisible(boolean visible) {
+        GuiNode guiNode = Toolkit.get().getApplicationWindow().getNode();
+        if (guiNode != null) {
+            Node node = (Node) guiNode.unwrapToNativeNode();
+            if (node instanceof Pane) {
+                Pane rootPane = (Pane) node;
+                if (!visible) {
+                    rootPane.getChildren().remove(spinner);
+                } else if (!rootPane.getChildren().contains(spinner)) {
+                    if (spinner == null)
+                        spinner = FxImageStore.createIconImageView("mongoose/client/java/images/spinner.gif");
+                    spinner.setManaged(false);
+                    spinner.setX(rootPane.getWidth()  / 2 - spinner.prefWidth(-1)  / 2);
+                    spinner.setY(rootPane.getHeight() / 2 - spinner.prefHeight(-1) / 2);
+                    rootPane.getChildren().add(spinner);
+                }
+            }
+        }
+    }
 }
