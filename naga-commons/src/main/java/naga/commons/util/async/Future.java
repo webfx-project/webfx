58678f6c15600f493a7885ae53881c05dfb7409f
Bruno Salmon <dev.salmonb@gmail.com>
2016-09-30 18:31:38 +0200

    Added compose() and map() methods to Future interface

--- naga-commons/src/main/java/naga/commons/util/async/Future.java
+++ naga-commons/src/main/java/naga/commons/util/async/Future.java
@@ -1,6 +1,8 @@
 package naga.commons.util.async;
 
+import naga.commons.util.function.Callable;
 import naga.commons.util.function.Consumer;
+import naga.commons.util.function.Function;
 
 /**
  * @author Bruno Salmon
@@ -137,4 +139,138 @@ public interface Future<T> extends AsyncResult<T> {
     void fail(Throwable throwable);
 
     void fail(String failureMessage);
+
+    /**
+     * Compose this future with a provided {@code next} future.<p>
+     *
+     * When this (the one on which {@code compose} is called) future succeeds, the {@code handler} will be called with
+     * the completed value, this handler should complete the next future.<p>
+     *
+     * If the {@code handler} throws an exception, the returned future will be failed with this exception.<p>
+     *
+     * When this future fails, the failure will be propagated to the {@code next} future and the {@code handler}
+     * will not be called.
+     *
+     * @param handler the handler
+     * @param next the next future
+     * @return the next future, used for chaining
+     */
+    default <U> Future<U> compose(Handler<T> handler, Future<U> next) {
+        setHandler(ar -> {
+            if (ar.succeeded()) {
+                try {
+                    handler.handle(ar.result());
+                } catch (Throwable err) {
+                    if (next.isComplete())
+                        throw err;
+                    next.fail(err);
+                }
+            } else
+                next.fail(ar.cause());
+        });
+        return next;
+    }
+
+    /**
+     * Compose this future with a {@code mapper} function.<p>
+     *
+     * When this future (the one on which {@code compose} is called) succeeds, the {@code mapper} will be called with
+     * the completed value and this mapper returns another future object. This returned future completion will complete
+     * the future returned by this method call.<p>
+     *
+     * If the {@code mapper} throws an exception, the returned future will be failed with this exception.<p>
+     *
+     * When this future fails, the failure will be propagated to the returned future and the {@code mapper}
+     * will not be called.
+     *
+     * @param mapper the mapper function
+     * @return the composed future
+     */
+    default <U> Future<U> compose(Function<T, Future<U>> mapper) {
+        Future<U> ret = Future.future();
+        setHandler(ar -> {
+            if (ar.succeeded()) {
+                Future<U> apply;
+                try {
+                    apply = mapper.apply(ar.result());
+                } catch (Throwable e) {
+                    ret.fail(e);
+                    return;
+                }
+                apply.setHandler(ret.completer());
+            } else
+                ret.fail(ar.cause());
+        });
+        return ret;
+    }
+
+    /**
+     * Apply a {@code mapper} function on this future.<p>
+     *
+     * When this future succeeds, the {@code mapper} will be called with the completed value and this mapper
+     * returns a value. This value will complete the future returned by this method call.<p>
+     *
+     * If the {@code mapper} throws an exception, the returned future will be failed with this exception.<p>
+     *
+     * When this future fails, the failure will be propagated to the returned future and the {@code mapper}
+     * will not be called.
+     *
+     * @param mapper the mapper function
+     * @return the mapped future
+     */
+    default <U> Future<U> map(Function<T, U> mapper) {
+        Future<U> ret = Future.future();
+        setHandler(ar -> {
+            if (ar.succeeded()) {
+                U mapped;
+                try {
+                    mapped = mapper.apply(ar.result());
+                } catch (Throwable e) {
+                    ret.fail(e);
+                    return;
+                }
+                ret.complete(mapped);
+            } else
+                ret.fail(ar.cause());
+        });
+        return ret;
+    }
+
+    default <U> Future<U> map(Callable<U> mapper) {
+        return map(arg -> mapper.call());
+    }
+
+    /**
+     * Map the result of a future to a specific {@code value}.<p>
+     *
+     * When this future succeeds, this {@code value} will complete the future returned by this method call.<p>
+     *
+     * When this future fails, the failure will be propagated to the returned future.
+     *
+     * @param value the value that eventually completes the mapped future
+     * @return the mapped future
+     */
+    default <V> Future<V> map(V value) {
+        Future<V> ret = Future.future();
+        setHandler(ar -> {
+            if (ar.succeeded())
+                ret.complete(value);
+            else
+                ret.fail(ar.cause());
+        });
+        return ret;
+    }
+
+    /**
+     * @return an handler completing this future
+     */
+    default Handler<AsyncResult<T>> completer() {
+        return ar -> {
+            if (ar.succeeded())
+                complete(ar.result());
+            else
+                fail(ar.cause());
+        };
+    }
+
 }
