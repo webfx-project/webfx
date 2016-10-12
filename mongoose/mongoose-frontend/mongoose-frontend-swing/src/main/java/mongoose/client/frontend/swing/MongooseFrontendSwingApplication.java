93ac15c926f1a57ca4b95da176212b864ebfff77
Bruno Salmon <dev.salmonb@gmail.com>
2016-10-12 09:46:05 +0200

    Added Swing version of Mongoose frontend application (for faster application start when testing in IDE)

---
+++ mongoose/mongoose-frontend/mongoose-frontend-swing/src/main/java/mongoose/client/frontend/swing/MongooseFrontendSwingApplication.java
@@ -0,0 +1,13 @@
+package mongoose.client.frontend.swing;
+
+import mongoose.activities.frontend.application.MongooseFrontendApplication;
+
+/**
+ * @author Bruno Salmon
+ */
+public class MongooseFrontendSwingApplication {
+
+    public static void main(String[] args) {
+        MongooseFrontendApplication.main(args);
+    }
+}
