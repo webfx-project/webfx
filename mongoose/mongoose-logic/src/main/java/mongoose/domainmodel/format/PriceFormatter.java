06b168d22e22fe8ea55f3f2deb252151d6060022
Bruno Salmon <dev.salmonb@gmail.com>
2016-10-13 18:56:40 +0200

    Fixed PriceFormatter to show "0" and not "0.00" when show00cents is false

--- mongoose/mongoose-logic/src/main/java/mongoose/domainmodel/format/PriceFormatter.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/domainmodel/format/PriceFormatter.java
@@ -30,7 +30,7 @@ public class PriceFormatter implements Formatter {
             return value;
         String cents = value.toString();
         switch (cents.length()) {
-            case 1 : return "0.0" + cents;
+            case 1 : return show00cents || !cents.equals("0") ? "0.0" + cents : "0";
             case 2 : return "0." + cents;
             default: return cents.substring(0, cents.length() - 2) + (!show00cents && cents.endsWith("00") ? "" : "." + cents.substring(cents.length() - 2));
         }
