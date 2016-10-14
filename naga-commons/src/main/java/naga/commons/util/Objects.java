d08b33267dc4cd64ca8e3a5401a4ba532662606f
Bruno Salmon <dev.salmonb@gmail.com>
2016-10-14 10:03:06 +0200

    Added allNulls() and anyNotNull() methods in Objects

--- naga-commons/src/main/java/naga/commons/util/Objects.java
+++ naga-commons/src/main/java/naga/commons/util/Objects.java
@@ -5,14 +5,19 @@ package naga.commons.util;
  */
 public class Objects {
 
-    private Objects() {
-    }
-
     public static <T> T coalesce(T... ts) {
         for (T t : ts)
             if (t != null)
                 return t;
         return null;
+    }
+
+    public static <T> boolean allNulls(T... ts) {
+        return coalesce(ts) == null;
+    }
+
+    public static <T> boolean anyNotNull(T... ts) {
+        return !allNulls(ts);
     }
 
     public static boolean areEquals(Object o1, Object o2) {
