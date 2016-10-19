621c5ce5c477067562df01ac0454e8bc068d1cf7
Bruno Salmon <dev.salmonb@gmail.com>
2016-10-19 14:52:07 +0200

    Updated Html (Svg) drawing implementation

--- naga-toolkits/naga-toolkit-html/src/main/java/naga/providers/toolkit/html/drawing/SvgDrawingNode.java
+++ naga-toolkits/naga-toolkit-html/src/main/java/naga/providers/toolkit/html/drawing/SvgDrawingNode.java
@@ -1,12 +1,13 @@
 package naga.providers.toolkit.html.drawing;
 
 import elemental2.Element;
-import javafx.collections.ListChangeListener;
+import elemental2.Node;
 import naga.commons.util.collection.Collections;
 import naga.providers.toolkit.html.HtmlUtil;
 import naga.providers.toolkit.html.drawing.view.SvgShapeView;
 import naga.providers.toolkit.html.nodes.HtmlParent;
 import naga.toolkit.drawing.shapes.Shape;
+import naga.toolkit.drawing.shapes.ShapeParent;
 import naga.toolkit.drawing.spi.Drawing;
 import naga.toolkit.drawing.spi.DrawingMixin;
 import naga.toolkit.drawing.spi.DrawingNode;
@@ -17,7 +18,18 @@ import naga.toolkit.drawing.spi.impl.DrawingImpl;
  */
 public class SvgDrawingNode extends HtmlParent</*SVGElement*/ Element> implements DrawingNode<Element>, DrawingMixin {
 
-    private final DrawingImpl drawing = new DrawingImpl(SvgShapeViewFactory.SINGLETON);
+    private final DrawingImpl drawing = new DrawingImpl(SvgShapeViewFactory.SINGLETON) {
+        @Override
+        protected void syncChildrenShapesWithVisual(ShapeParent shapeParent) {
+            Node parent = shapeParent == this ? node : getSvgShapeElement((Shape) shapeParent);
+            HtmlUtil.setChildren(parent, Collections.convert(shapeParent.getChildrenShapes(), this::getSvgShapeElement));
+        }
+
+        private Element getSvgShapeElement(Shape shape) {
+            return ((SvgShapeView) getOrCreateAndBindShapeView(shape)).getSvgShapeElement();
+        }
+
+    };
 
     public SvgDrawingNode() {
         this(SvgUtil.createSvgElement());
@@ -25,13 +37,6 @@ public class SvgDrawingNode extends HtmlParent</*SVGElement*/ Element> implement
 
     public SvgDrawingNode(Element svg) {
         super(svg);
-        drawing.getChildrenShapes().addListener(new ListChangeListener<Shape>() {
-            @Override
-            public void onChanged(Change<? extends Shape> c) {
-                drawing.draw();
-                HtmlUtil.setChildren(svg, Collections.convert(drawing.getChildrenShapes(), shape -> ((SvgShapeView) drawing.getOrCreateShapeView(shape)).getSvgShapeElement()));
-            }
-        });
         //svg.setAttribute("style", "width: 600; height: 250; border: 1px solid red");
     }
 
--- naga-toolkits/naga-toolkit-html/src/main/java/naga/providers/toolkit/html/drawing/view/SvgRectangleView.java
+++ naga-toolkits/naga-toolkit-html/src/main/java/naga/providers/toolkit/html/drawing/view/SvgRectangleView.java
@@ -1,42 +1,31 @@
 package naga.providers.toolkit.html.drawing.view;
 
 import elemental2.Element;
-import javafx.beans.value.ChangeListener;
 import naga.providers.toolkit.html.drawing.SvgUtil;
 import naga.toolkit.drawing.shapes.Rectangle;
+import naga.toolkit.drawing.spi.DrawingNotifier;
 import naga.toolkit.drawing.spi.view.RectangleView;
+import naga.toolkit.drawing.spi.view.implbase.RectangleViewImplBase;
 
 /**
  * @author Bruno Salmon
  */
-public class SvgRectangleView implements RectangleView, SvgShapeView {
+public class SvgRectangleView extends RectangleViewImplBase implements RectangleView, SvgShapeView {
 
-    private Rectangle rectangle;
-    private Element svgRectangle;
+    private final Element svgRectangle = SvgUtil.createSvgRectangle();
 
     @Override
-    public void bind(Rectangle rectangle) {
-        this.rectangle = rectangle;
-        svgRectangle = SvgUtil.createSvgRectangle();
-        syncVisual();
-        ChangeListener<Double> changeListener = (observable, oldValue, newValue) -> syncVisual();
-        rectangle.xProperty().addListener(changeListener);
-        rectangle.yProperty().addListener(changeListener);
-        rectangle.widthProperty().addListener(changeListener);
-        rectangle.heightProperty().addListener(changeListener);
+    public void bind(Rectangle rectangle, DrawingNotifier drawingNotifier) {
+        super.bind(rectangle, drawingNotifier);
+        runNowAndOnPropertyChange(this::syncVisual, rectangle.xProperty(), rectangle.yProperty(), rectangle.widthProperty(), rectangle.heightProperty());
     }
 
     private void syncVisual() {
-        svgRectangle.setAttribute("x", rectangle.getX());
-        svgRectangle.setAttribute("y", rectangle.getY());
-        svgRectangle.setAttribute("width", rectangle.getWidth());
-        svgRectangle.setAttribute("height", rectangle.getHeight());
+        svgRectangle.setAttribute("x", shape.getX());
+        svgRectangle.setAttribute("y", shape.getY());
+        svgRectangle.setAttribute("width", shape.getWidth());
+        svgRectangle.setAttribute("height", shape.getHeight());
         svgRectangle.setAttribute("fill", "red");
-    }
-
-    @Override
-    public void unbind() {
-        svgRectangle = null;
     }
 
     @Override
