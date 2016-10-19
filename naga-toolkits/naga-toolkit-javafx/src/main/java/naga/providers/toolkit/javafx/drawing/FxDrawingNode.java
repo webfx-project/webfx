6c77141f7d9011cd9eb56fa0b3269f59e3b28a51
Bruno Salmon <dev.salmonb@gmail.com>
2016-10-19 14:51:50 +0200

    Updated Fx drawing implementation

--- naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/drawing/FxDrawingNode.java
+++ naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/drawing/FxDrawingNode.java
@@ -1,22 +1,45 @@
 package naga.providers.toolkit.javafx.drawing;
 
-import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import naga.providers.toolkit.javafx.drawing.view.FxShapeView;
 import naga.providers.toolkit.javafx.nodes.FxNode;
 import naga.toolkit.drawing.shapes.Shape;
+import naga.toolkit.drawing.shapes.ShapeParent;
 import naga.toolkit.drawing.spi.Drawing;
 import naga.toolkit.drawing.spi.DrawingMixin;
 import naga.toolkit.drawing.spi.DrawingNode;
 import naga.toolkit.drawing.spi.impl.DrawingImpl;
 import naga.toolkit.util.ObservableLists;
 
+import java.lang.reflect.Method;
+
 /**
  * @author Bruno Salmon
  */
 public class FxDrawingNode extends FxNode<Group> implements DrawingNode<Group>, DrawingMixin {
 
-    private final DrawingImpl drawing = new DrawingImpl(FxShapeViewFactory.SINGLETON);
+    private final Drawing drawing = new DrawingImpl(FxShapeViewFactory.SINGLETON) {
+        @Override
+        protected void syncChildrenShapesWithVisual(ShapeParent shapeParent) {
+            Parent parent = shapeParent == this ? node : (Parent) getFxShapeNode((Shape) shapeParent);
+            try {
+                Method getChildren = Parent.class.getDeclaredMethod("getChildren");
+                getChildren.setAccessible(true);
+                ObservableList<Node> children = (ObservableList<Node>) getChildren.invoke(parent);
+                ObservableLists.setAllConverted(shapeParent.getChildrenShapes(), this::getFxShapeNode, children);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        private Node getFxShapeNode(Shape shape) {
+            return ((FxShapeView) getOrCreateAndBindShapeView(shape)).getFxShapeNode();
+        }
+    };
+
 
     public FxDrawingNode() {
         this(new Group());
@@ -24,13 +47,6 @@ public class FxDrawingNode extends FxNode<Group> implements DrawingNode<Group>, 
 
     public FxDrawingNode(Group node) {
         super(node);
-        drawing.getChildrenShapes().addListener(new ListChangeListener<Shape>() {
-            @Override
-            public void onChanged(Change<? extends Shape> c) {
-                drawing.draw();
-                ObservableLists.setAllConverted(drawing.getChildrenShapes(), shape -> ((FxShapeView) drawing.getOrCreateShapeView(shape)).getFxShapeNode(), node.getChildren());
-            }
-        });
     }
 
     @Override
--- naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/drawing/view/FxRectangleView.java
+++ naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/drawing/view/FxRectangleView.java
@@ -3,6 +3,7 @@ package naga.providers.toolkit.javafx.drawing.view;
 import javafx.scene.Node;
 import javafx.scene.paint.Color;
 import naga.toolkit.drawing.shapes.Rectangle;
+import naga.toolkit.drawing.spi.DrawingNotifier;
 import naga.toolkit.drawing.spi.view.RectangleView;
 
 /**
@@ -13,7 +14,7 @@ public class FxRectangleView implements FxShapeView, RectangleView {
     javafx.scene.shape.Rectangle fxRectangle;
 
     @Override
-    public void bind(Rectangle rectangle) {
+    public void bind(Rectangle rectangle, DrawingNotifier drawingNotifier) {
         fxRectangle = new javafx.scene.shape.Rectangle(0, 0, Color.RED);
         fxRectangle.xProperty().bind(rectangle.xProperty());
         fxRectangle.yProperty().bind(rectangle.yProperty());
