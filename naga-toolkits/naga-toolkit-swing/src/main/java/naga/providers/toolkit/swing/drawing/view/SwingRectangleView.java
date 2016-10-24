4944812cf38788c7af0fef674fa2bc26f65a21a1
Bruno Salmon <dev.salmonb@gmail.com>
2016-10-24 21:32:59 +0200

    Added LinearGradient support in Swing toolkit

--- naga-toolkits/naga-toolkit-swing/src/main/java/naga/providers/toolkit/swing/drawing/view/SwingRectangleView.java
+++ naga-toolkits/naga-toolkit-swing/src/main/java/naga/providers/toolkit/swing/drawing/view/SwingRectangleView.java
@@ -1,7 +1,9 @@
 package naga.providers.toolkit.swing.drawing.view;
 
 import naga.providers.toolkit.swing.util.SwingPaints;
-import naga.toolkit.drawing.spi.view.RectangleView;
+import naga.toolkit.drawing.paint.LinearGradient;
+import naga.toolkit.drawing.shapes.Rectangle;
+import naga.toolkit.drawing.spi.DrawingNotifier;
 import naga.toolkit.drawing.spi.view.implbase.RectangleViewImplBase;
 
 import java.awt.*;
@@ -9,11 +11,29 @@ import java.awt.*;
 /**
  * @author Bruno Salmon
  */
-public class SwingRectangleView extends RectangleViewImplBase implements SwingShapeView, RectangleView {
+public class SwingRectangleView extends RectangleViewImplBase implements SwingShapeView {
+
+    private Paint paint;
+    private boolean proportionalGradient;
+
+    @Override
+    public void bind(Rectangle shape, DrawingNotifier drawingNotifier) {
+        super.bind(shape, drawingNotifier);
+        updatePaint();
+        shape.fillProperty().addListener((observable, oldValue, newValue) -> updatePaint());
+    }
+
+    private void updatePaint() {
+        naga.toolkit.drawing.paint.Paint fill = shape.getFill();
+        proportionalGradient = fill instanceof LinearGradient && ((LinearGradient) fill).isProportional();
+        paint = SwingPaints.toSwingPaint(fill);
+    }
 
     @Override
     public void paintShape(Graphics g) {
-        g.setColor((Color) SwingPaints.toSwingPaint(shape.getFill()));
+        if (proportionalGradient)
+            paint = SwingPaints.toSwingLinearGradient((LinearGradient) shape.getFill(), shape.getWidth().floatValue(), shape.getHeight().floatValue());
+        ((Graphics2D) g).setPaint(paint);
         g.fillRect(shape.getX().intValue(), shape.getY().intValue(), shape.getWidth().intValue(), shape.getHeight().intValue());
     }
 }
