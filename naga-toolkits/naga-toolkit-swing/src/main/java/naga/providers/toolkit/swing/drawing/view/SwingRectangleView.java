91bd73e7190fc8d753f7deaa6f07eb63f2b9eb13
Bruno Salmon <dev.salmonb@gmail.com>
2016-10-25 13:10:25 +0200

    Added strokeProperty support in Swing toolkit

--- naga-toolkits/naga-toolkit-swing/src/main/java/naga/providers/toolkit/swing/drawing/view/SwingRectangleView.java
+++ naga-toolkits/naga-toolkit-swing/src/main/java/naga/providers/toolkit/swing/drawing/view/SwingRectangleView.java
@@ -13,27 +13,48 @@ import java.awt.*;
  */
 public class SwingRectangleView extends RectangleViewImplBase implements SwingShapeView {
 
-    private Paint paint;
-    private boolean proportionalGradient;
+    private Paint swingFill;
+    private boolean fillIsProportionalGradient;
+    private Stroke swingStroke;
+    private Paint swingStrokePaint;
+    private boolean strokePaintIsProportionalGradient;
 
     @Override
     public void bind(Rectangle shape, DrawingNotifier drawingNotifier) {
         super.bind(shape, drawingNotifier);
-        updatePaint();
-        shape.fillProperty().addListener((observable, oldValue, newValue) -> updatePaint());
+        shape.fillProperty().addListener((observable, oldValue, newValue) -> updateSwingFill());
+        updateSwingFill();
+        shape.strokeProperty().addListener((observable, oldValue, newValue) -> updateSwingStroke());
+        updateSwingStroke();
     }
 
-    private void updatePaint() {
+    private void updateSwingFill() {
         naga.toolkit.drawing.paint.Paint fill = shape.getFill();
-        proportionalGradient = fill instanceof LinearGradient && ((LinearGradient) fill).isProportional();
-        paint = SwingPaints.toSwingPaint(fill);
+        fillIsProportionalGradient = fill instanceof LinearGradient && ((LinearGradient) fill).isProportional();
+        swingFill = fillIsProportionalGradient ? null : SwingPaints.toSwingPaint(fill);
+    }
+
+    private void updateSwingStroke() {
+        naga.toolkit.drawing.paint.Paint stroke = shape.getStroke();
+        swingStroke = new BasicStroke();
+        strokePaintIsProportionalGradient = stroke instanceof LinearGradient && ((LinearGradient) stroke).isProportional();
+        swingStrokePaint = strokePaintIsProportionalGradient ? null : SwingPaints.toSwingPaint(stroke);
     }
 
     @Override
     public void paintShape(Graphics g) {
-        if (proportionalGradient)
-            paint = SwingPaints.toSwingLinearGradient((LinearGradient) shape.getFill(), shape.getWidth().floatValue(), shape.getHeight().floatValue());
-        ((Graphics2D) g).setPaint(paint);
-        g.fillRect(shape.getX().intValue(), shape.getY().intValue(), shape.getWidth().intValue(), shape.getHeight().intValue());
+        java.awt.Rectangle swingRectangle = new java.awt.Rectangle(shape.getX().intValue(), shape.getY().intValue(), shape.getWidth().intValue(), shape.getHeight().intValue());
+        Graphics2D g2 = (Graphics2D) g;
+        if (fillIsProportionalGradient)
+            swingFill = SwingPaints.toSwingLinearGradient((LinearGradient) shape.getFill(), shape.getWidth().floatValue(), shape.getHeight().floatValue());
+        g2.setPaint(swingFill);
+        g2.fill(swingRectangle);
+        if (swingStroke != null) {
+            g2.setStroke(swingStroke);
+            if (strokePaintIsProportionalGradient)
+                swingStrokePaint = SwingPaints.toSwingLinearGradient((LinearGradient) shape.getStroke(), shape.getWidth().floatValue(), shape.getHeight().floatValue());
+            g2.setPaint(swingStrokePaint);
+            g2.draw(swingRectangle);
+        }
     }
 }
