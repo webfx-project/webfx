9687a7091b509a74a9fe727ecd96515259f005a1
Bruno Salmon <dev.salmonb@gmail.com>
2016-10-25 15:27:35 +0200

    Added strokeDashOffsetProperty support in JavaFx toolkit

--- naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/drawing/view/FxRectangleView.java
+++ naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/drawing/view/FxRectangleView.java
@@ -29,6 +29,7 @@ public class FxRectangleView implements FxShapeView, RectangleView {
         fxRectangle.strokeLineCapProperty().bind(new ConvertedProperty<>(rectangle.strokeLineCapProperty(), FxStrokes::toFxStrokeLineCap));
         fxRectangle.strokeLineJoinProperty().bind(new ConvertedProperty<>(rectangle.strokeLineJoinProperty(), FxStrokes::toFxStrokeLineJoin));
         fxRectangle.strokeMiterLimitProperty().bind(rectangle.strokeMiterLimitProperty());
+        fxRectangle.strokeDashOffsetProperty().bind(rectangle.strokeDashOffsetProperty());
     }
 
     @Override
