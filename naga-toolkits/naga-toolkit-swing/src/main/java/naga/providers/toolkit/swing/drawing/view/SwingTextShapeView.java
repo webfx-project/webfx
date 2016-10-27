0dd8bf94968a5355ce0d6220be482a39f877f600
Bruno Salmon <dev.salmonb@gmail.com>
2016-10-27 20:09:23 +0200

    Added textOriginProperty support in Swing toolkit

--- naga-toolkits/naga-toolkit-swing/src/main/java/naga/providers/toolkit/swing/drawing/view/SwingTextShapeView.java
+++ naga-toolkits/naga-toolkit-swing/src/main/java/naga/providers/toolkit/swing/drawing/view/SwingTextShapeView.java
@@ -2,6 +2,7 @@ package naga.providers.toolkit.swing.drawing.view;
 
 import naga.providers.toolkit.swing.util.SwingFonts;
 import naga.toolkit.drawing.shapes.TextShape;
+import naga.toolkit.drawing.shapes.VPos;
 import naga.toolkit.drawing.spi.DrawingNotifier;
 import naga.toolkit.drawing.spi.view.implbase.TextShapeViewImplBase;
 
@@ -13,8 +14,12 @@ import java.awt.*;
 public class SwingTextShapeView extends TextShapeViewImplBase implements SwingShapeView<TextShape> {
 
     private final SwingShapeBinderPainter swingShapeBinderPainter = new SwingShapeBinderPainter((g) ->
-        SwingFonts.toSwingFont(shape.getFont()).createGlyphVector(g.getFontRenderContext(), shape.getText()).getOutline()
+        getShapeSwingFont().createGlyphVector(g.getFontRenderContext(), shape.getText()).getOutline()
     );
+
+    private Font getShapeSwingFont() {
+        return SwingFonts.toSwingFont(shape.getFont());
+    }
 
     @Override
     public void bind(TextShape shape, DrawingNotifier drawingNotifier) {
@@ -24,7 +29,22 @@ public class SwingTextShapeView extends TextShapeViewImplBase implements SwingSh
 
     @Override
     public void paintShape(Graphics2D g) {
-        g.translate(shape.getX(), shape.getY());
+        g.translate(shape.getX(), shape.getY() + vPosToBaselineOffset(shape.getTextOrigin(), g));
         swingShapeBinderPainter.applyCommonShapePropertiesToGraphicsAndPaintShape(shape, g);
+    }
+
+    private double vPosToBaselineOffset(VPos vpos, Graphics2D g) {
+        if (vpos != null && vpos != VPos.BASELINE) {
+            FontMetrics fontMetrics = g.getFontMetrics(getShapeSwingFont());
+            switch (vpos) {
+                case TOP:
+                    return fontMetrics.getAscent();
+                case CENTER:
+                    return fontMetrics.getAscent() - fontMetrics.getHeight() / 2;
+                case BOTTOM:
+                    return -fontMetrics.getDescent();
+            }
+        }
+        return 0;
     }
 }
