2cb0bcf7f7ec9be7830ce0502660e4800c83aec3
Bruno Salmon <dev.salmonb@gmail.com>
2016-10-27 14:58:00 +0200

    Added frontProperty support in Html toolkit (Svg)

--- naga-toolkits/naga-toolkit-html/src/main/java/naga/providers/toolkit/html/drawing/view/SvgTextShapeView.java
+++ naga-toolkits/naga-toolkit-html/src/main/java/naga/providers/toolkit/html/drawing/view/SvgTextShapeView.java
@@ -1,8 +1,11 @@
 package naga.providers.toolkit.html.drawing.view;
 
 import elemental2.Element;
+import naga.platform.spi.Platform;
 import naga.providers.toolkit.html.drawing.SvgDrawingNode;
 import naga.providers.toolkit.html.drawing.SvgUtil;
+import naga.toolkit.drawing.shapes.Font;
+import naga.toolkit.drawing.shapes.FontPosture;
 import naga.toolkit.drawing.spi.view.implbase.TextShapeViewImplBase;
 
 /**
@@ -18,8 +21,11 @@ public class SvgTextShapeView extends TextShapeViewImplBase implements SvgShapeV
         svgElement.textContent = shape.getText();
         svgElement.setAttribute("x", shape.getX());
         svgElement.setAttribute("y", shape.getY());
-        svgElement.setAttribute("font-family", "Serif");
-        svgElement.setAttribute("font-size", 100);
+        Font font = shape.getFont();
+        svgElement.setAttribute("font-family", font.getFamily());
+        svgElement.setAttribute("font-style", font.getPosture() == FontPosture.ITALIC ? "italic" : null);
+        svgElement.setAttribute("font-weight", font.getWeight() == null ? 0 : font.getWeight().getWeight());
+        svgElement.setAttribute("font-size", font.getSize());
     }
 
     @Override
