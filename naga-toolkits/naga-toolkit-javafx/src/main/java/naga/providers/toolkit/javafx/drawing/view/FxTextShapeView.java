d77697394d397668fe69dddd8722f15a9fb2f469
Bruno Salmon <dev.salmonb@gmail.com>
2016-10-27 20:05:47 +0200

    Added textOriginProperty support in JavaFx toolkit

--- naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/drawing/view/FxTextShapeView.java
+++ naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/drawing/view/FxTextShapeView.java
@@ -3,6 +3,7 @@ package naga.providers.toolkit.javafx.drawing.view;
 import javafx.scene.text.Text;
 import naga.providers.toolkit.javafx.util.FxFonts;
 import naga.toolkit.drawing.shapes.TextShape;
+import naga.toolkit.drawing.shapes.VPos;
 import naga.toolkit.drawing.spi.DrawingNotifier;
 import naga.toolkit.drawing.spi.view.TextShapeView;
 import naga.toolkit.properties.conversion.ConvertedProperty;
@@ -18,7 +19,19 @@ public class FxTextShapeView extends FxShapeViewImpl<TextShape, Text> implements
         fxShape.xProperty().bind(shape.xProperty());
         fxShape.yProperty().bind(shape.yProperty());
         fxShape.textProperty().bind(shape.textProperty());
+        fxShape.textOriginProperty().bind(new ConvertedProperty<>(shape.textOriginProperty(), FxTextShapeView::toFxVpos));
         fxShape.fontProperty().bind(new ConvertedProperty<>(shape.fontProperty(), FxFonts::toFxFont));
+    }
+
+    private static javafx.geometry.VPos toFxVpos(VPos vpos) {
+        if (vpos != null)
+            switch (vpos) {
+                case TOP: return javafx.geometry.VPos.TOP;
+                case CENTER: return javafx.geometry.VPos.CENTER;
+                case BASELINE: return javafx.geometry.VPos.BASELINE;
+                case BOTTOM: return javafx.geometry.VPos.BOTTOM;
+            }
+        return null;
     }
 
 }
