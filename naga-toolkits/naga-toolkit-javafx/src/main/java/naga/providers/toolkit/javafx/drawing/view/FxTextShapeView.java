dc5f6ea6a0f3c22a9f5d1fe8d23752a139b5803e
Bruno Salmon <dev.salmonb@gmail.com>
2016-10-31 12:34:09 +0100

    Added textAlignmentProperty and wrappingWidthProperty support in JavaFx toolkit

--- naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/drawing/view/FxTextShapeView.java
+++ naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/drawing/view/FxTextShapeView.java
@@ -2,6 +2,7 @@ package naga.providers.toolkit.javafx.drawing.view;
 
 import javafx.scene.text.Text;
 import naga.providers.toolkit.javafx.util.FxFonts;
+import naga.toolkit.drawing.shapes.TextAlignment;
 import naga.toolkit.drawing.shapes.TextShape;
 import naga.toolkit.drawing.shapes.VPos;
 import naga.toolkit.drawing.spi.DrawingNotifier;
@@ -20,6 +21,8 @@ public class FxTextShapeView extends FxShapeViewImpl<TextShape, Text> implements
         fxShape.yProperty().bind(shape.yProperty());
         fxShape.textProperty().bind(shape.textProperty());
         fxShape.textOriginProperty().bind(new ConvertedProperty<>(shape.textOriginProperty(), FxTextShapeView::toFxVpos));
+        fxShape.textAlignmentProperty().bind(new ConvertedProperty<>(shape.textAlignmentProperty(), FxTextShapeView::toFxTextAlignment));
+        fxShape.wrappingWidthProperty().bind(shape.wrappingWidthProperty());
         fxShape.fontProperty().bind(new ConvertedProperty<>(shape.fontProperty(), FxFonts::toFxFont));
     }
 
@@ -30,6 +33,17 @@ public class FxTextShapeView extends FxShapeViewImpl<TextShape, Text> implements
                 case CENTER: return javafx.geometry.VPos.CENTER;
                 case BASELINE: return javafx.geometry.VPos.BASELINE;
                 case BOTTOM: return javafx.geometry.VPos.BOTTOM;
+            }
+        return null;
+    }
+
+    private static javafx.scene.text.TextAlignment toFxTextAlignment(TextAlignment textAlignment) {
+        if (textAlignment != null)
+            switch (textAlignment) {
+                case LEFT: return javafx.scene.text.TextAlignment.LEFT;
+                case CENTER: return javafx.scene.text.TextAlignment.CENTER;
+                case RIGHT: return javafx.scene.text.TextAlignment.RIGHT;
+                case JUSTIFY: return javafx.scene.text.TextAlignment.JUSTIFY;
             }
         return null;
     }
