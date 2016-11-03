1c87efa9323e880ce69c4face254e3e149ed9f13
Bruno Salmon <dev.salmonb@gmail.com>
2016-11-03 23:05:22 +0100

    Implemented Scale transform in Swing toolkit

--- naga-toolkits/naga-toolkit-swing/src/main/java/naga/providers/toolkit/swing/util/SwingTransforms.java
+++ naga-toolkits/naga-toolkit-swing/src/main/java/naga/providers/toolkit/swing/util/SwingTransforms.java
@@ -2,6 +2,7 @@ package naga.providers.toolkit.swing.util;
 
 import naga.commons.util.collection.Collections;
 import naga.toolkit.transform.Rotate;
+import naga.toolkit.transform.Scale;
 import naga.toolkit.transform.Transform;
 import naga.toolkit.transform.Translate;
 
@@ -16,7 +17,12 @@ public class SwingTransforms {
     public static AffineTransform toSwingTransform(List<Transform> transforms) {
         if (Collections.isEmpty(transforms))
             return null;
-        return toSwingTransform(transforms.get(0));
+        return transforms.stream().map(SwingTransforms::toSwingTransform).reduce(SwingTransforms::concatenate).get();
+    }
+
+    private static AffineTransform concatenate(AffineTransform t1, AffineTransform t2) {
+        t1.concatenate(t2);
+        return t1;
     }
 
     private static AffineTransform toSwingTransform(Transform transform) {
@@ -24,6 +30,8 @@ public class SwingTransforms {
             return toSwingTranslate((Translate) transform);
         if (transform instanceof Rotate)
             return toSwingRotate((Rotate) transform);
+        if (transform instanceof Scale)
+            return toSwingScale((Scale) transform);
         return null;
     }
 
@@ -34,4 +42,9 @@ public class SwingTransforms {
     private static AffineTransform toSwingRotate(Rotate rotate) {
         return AffineTransform.getRotateInstance(rotate.getAngle() * Math.PI / 180, rotate.getPivotX(), rotate.getPivotY());
     }
+
+    private static AffineTransform toSwingScale(Scale scale) {
+        return AffineTransform.getScaleInstance(scale.getX(), scale.getY());
+    }
+
 }
