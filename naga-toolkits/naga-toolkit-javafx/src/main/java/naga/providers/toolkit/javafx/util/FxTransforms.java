a6c4f8a87b40bdb63de8d4099e26ef56ceaeef9d
Bruno Salmon <dev.salmonb@gmail.com>
2016-11-03 22:05:31 +0100

    Implemented Translate transform in JavaFx toolkit

--- naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/util/FxTransforms.java
+++ naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/util/FxTransforms.java
@@ -2,6 +2,7 @@ package naga.providers.toolkit.javafx.util;
 
 import naga.toolkit.transform.Rotate;
 import naga.toolkit.transform.Transform;
+import naga.toolkit.transform.Translate;
 
 /**
  * @author Bruno Salmon
@@ -9,12 +10,21 @@ import naga.toolkit.transform.Transform;
 public class FxTransforms {
 
     public static javafx.scene.transform.Transform toFxTransform(Transform transform) {
+        if (transform instanceof Translate)
+            return toFxTranslate((Translate) transform);
         if (transform instanceof Rotate)
             return toFxRotate((Rotate) transform);
         return null;
     }
 
-    public static javafx.scene.transform.Rotate toFxRotate(Rotate rotate) {
+    private static javafx.scene.transform.Translate toFxTranslate(Translate translate) {
+        javafx.scene.transform.Translate fxTranslate = new javafx.scene.transform.Translate();
+        fxTranslate.xProperty().bind(translate.xProperty());
+        fxTranslate.yProperty().bind(translate.yProperty());
+        return fxTranslate;
+    }
+
+    private static javafx.scene.transform.Rotate toFxRotate(Rotate rotate) {
         javafx.scene.transform.Rotate fxRotate = new javafx.scene.transform.Rotate();
         fxRotate.angleProperty().bind(rotate.angleProperty());
         fxRotate.pivotXProperty().bind(rotate.pivotXProperty());
