207212eb54ee5b7a173c36abbbf23a851bf7be85
Bruno Salmon <dev.salmonb@gmail.com>
2016-11-04 22:28:43 +0100

    Implemented HasWidthProperty in SwingDrawingNode

--- naga-toolkits/naga-toolkit-swing/src/main/java/naga/providers/toolkit/swing/drawing/SwingDrawingNode.java
+++ naga-toolkits/naga-toolkit-swing/src/main/java/naga/providers/toolkit/swing/drawing/SwingDrawingNode.java
@@ -1,5 +1,7 @@
 package naga.providers.toolkit.swing.drawing;
 
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleObjectProperty;
 import naga.providers.toolkit.swing.nodes.SwingNode;
 import naga.toolkit.drawing.spi.Drawing;
 import naga.toolkit.drawing.spi.DrawingMixin;
@@ -27,14 +29,24 @@ public class SwingDrawingNode extends SwingNode<SwingDrawingNode.DrawingPanel> i
         return node.drawing;
     }
 
+    @Override
+    public Property<Double> widthProperty() {
+        return node.widthProperty;
+    }
+
     static class DrawingPanel extends JPanel {
 
         private SwingDrawing drawing;
+        private int lastWidth;
+        private final Property<Double> widthProperty = new SimpleObjectProperty<>(0d);
 
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
             drawing.paintCanvas((Graphics2D) g);
+            int width = getWidth();
+            if (width != lastWidth)
+                widthProperty.setValue((double) (lastWidth = width));
         }
     }
 }
