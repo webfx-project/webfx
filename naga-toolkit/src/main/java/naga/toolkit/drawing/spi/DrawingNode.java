a1e654496b41c9567c9e30e3e5d141a2f2e40fff
Bruno Salmon <dev.salmonb@gmail.com>
2016-11-04 22:24:34 +0100

    Added HasWidthProperty to DrawingNode (to be managed by the implementation so it always reflects the actual width displayed on the screen)

--- naga-toolkit/src/main/java/naga/toolkit/drawing/spi/DrawingNode.java
+++ naga-toolkit/src/main/java/naga/toolkit/drawing/spi/DrawingNode.java
@@ -1,9 +1,12 @@
 package naga.toolkit.drawing.spi;
 
+import naga.toolkit.properties.markers.HasWidthProperty;
 import naga.toolkit.spi.nodes.GuiNode;
 
 /**
  * @author Bruno Salmon
  */
-public interface DrawingNode<N> extends Drawing, GuiNode<N> {
+public interface DrawingNode<N> extends Drawing, GuiNode<N>,
+        HasWidthProperty // to be managed by the implementation so it always reflects the actual width displayed on the screen
+{
 }
