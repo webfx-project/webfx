f46f12fb1d68f4ffed3261fe4dccf60a0bccb937
Bruno Salmon <dev.salmonb@gmail.com>
2016-11-07 10:36:19 +0100

    Added HasHeightProperty to DrawingNode

--- naga-toolkit/src/main/java/naga/toolkit/drawing/spi/DrawingNode.java
+++ naga-toolkit/src/main/java/naga/toolkit/drawing/spi/DrawingNode.java
@@ -1,5 +1,6 @@
 package naga.toolkit.drawing.spi;
 
+import naga.toolkit.properties.markers.HasHeightProperty;
 import naga.toolkit.properties.markers.HasWidthProperty;
 import naga.toolkit.spi.nodes.GuiNode;
 
@@ -7,6 +8,7 @@ import naga.toolkit.spi.nodes.GuiNode;
  * @author Bruno Salmon
  */
 public interface DrawingNode<N> extends Drawing, GuiNode<N>,
-        HasWidthProperty // to be managed by the implementation so it always reflects the actual width displayed on the screen
+        HasWidthProperty, // <- visual (implementation is responsible to bind it to visual width)
+        HasHeightProperty // -> visual (implementation is responsible to bind the visual height to it)
 {
 }
