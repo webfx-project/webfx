46bd2c356ad5490b9b997062c8d017f6d886e58d
Bruno Salmon <dev.salmonb@gmail.com>
2016-11-08 10:07:07 +0100

    Added Point2D and GeometryUtil classes

---
+++ naga-toolkit/src/main/java/naga/toolkit/drawing/shapes/Point2D.java
@@ -0,0 +1,50 @@
+package naga.toolkit.drawing.shapes;
+
+/**
+ * A 2D geometric point that usually represents the x, y coordinates.
+ * It can also represent a relative magnitude vector's x, y magnitudes.
+ */
+public class Point2D {
+
+    /**
+     * The x coordinate.
+     */
+    private double x;
+
+    /**
+     * The y coordinate.
+     */
+    private double y;
+
+    public Point2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * The x coordinate.
+     * @return the x coordinate
+     */
+    public final double getX() {
+        return x;
+    }
+
+    /**
+     * The y coordinate.
+     * @return the y coordinate
+     */
+    public final double getY() {
+        return y;
+    }
+
+    /**
+     * Returns a string representation of this {@code Point2D}.
+     * This method is intended to be used only for informational purposes.
+     * The content and format of the returned string might vary between
+     * implementations.
+     * The returned string might be empty but cannot be {@code null}.
+     */
+    @Override public String toString() {
+        return "Point2D [x = " + getX() + ", y = " + getY() + "]";
+    }
+}
