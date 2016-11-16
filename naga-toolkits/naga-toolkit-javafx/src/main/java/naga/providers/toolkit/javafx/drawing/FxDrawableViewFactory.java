22556a5c5a062e195d9e385f57b3d6ae3fac8762
Bruno Salmon <dev.salmonb@gmail.com>
2016-11-16 21:28:48 +0100

    Implemented EmbedDrawableView for JavaFx toolkit

--- naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/drawing/FxDrawableViewFactory.java
+++ naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/drawing/FxDrawableViewFactory.java
@@ -1,8 +1,10 @@
 package naga.providers.toolkit.javafx.drawing;
 
+import naga.providers.toolkit.javafx.drawing.view.FxEmbedDrawableView;
 import naga.providers.toolkit.javafx.drawing.view.FxGroupView;
 import naga.providers.toolkit.javafx.drawing.view.FxRectangleView;
 import naga.providers.toolkit.javafx.drawing.view.FxTextShapeView;
+import naga.toolkit.drawing.shapes.impl.EmbedDrawableImpl;
 import naga.toolkit.drawing.shapes.impl.GroupImpl;
 import naga.toolkit.drawing.shapes.impl.RectangleImpl;
 import naga.toolkit.drawing.shapes.impl.TextShapeImpl;
@@ -19,5 +21,6 @@ class FxDrawableViewFactory extends DrawableViewFactoryImpl {
         registerDrawableViewFactory(RectangleImpl.class, FxRectangleView::new);
         registerDrawableViewFactory(TextShapeImpl.class, FxTextShapeView::new);
         registerDrawableViewFactory(GroupImpl.class, FxGroupView::new);
+        registerDrawableViewFactory(EmbedDrawableImpl.class, FxEmbedDrawableView::new);
     }
 }
