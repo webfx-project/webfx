306751f8586fd4936abf95a92afd09067ed5bf4d
Bruno Salmon <dev.salmonb@gmail.com>
2016-11-18 19:07:10 +0100

    Implemented clipProperty in JavaFx toolkit

--- naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/drawing/view/FxDrawableViewImpl.java
+++ naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/drawing/view/FxDrawableViewImpl.java
@@ -7,10 +7,13 @@ import javafx.scene.Node;
 import naga.providers.toolkit.javafx.events.FxMouseEvent;
 import naga.providers.toolkit.javafx.util.FxTransforms;
 import naga.toolkit.drawing.shapes.Drawable;
+import naga.toolkit.drawing.spi.impl.DrawingImpl;
+import naga.toolkit.drawing.spi.view.DrawableView;
 import naga.toolkit.properties.conversion.ConvertedProperty;
 import naga.toolkit.spi.events.MouseEvent;
 import naga.toolkit.spi.events.UiEventHandler;
 import naga.toolkit.util.ObservableLists;
+import naga.toolkit.util.Properties;
 
 /**
  * @author Bruno Salmon
@@ -24,6 +27,8 @@ abstract class FxDrawableViewImpl<D extends Drawable, N extends Node> implements
         ObservableLists.bindConverted(fxDrawableNode.getTransforms(), drawable.getTransforms(), FxTransforms::toFxTransform);
         fxDrawableNode.visibleProperty().bind(drawable.visibleProperty());
         fxDrawableNode.opacityProperty().bind(drawable.opacityProperty());
+        DrawingImpl drawing = DrawingImpl.getThreadLocalDrawing();
+        Properties.runNowAndOnPropertiesChange((clipProperty) -> fxDrawableNode.setClip(getDrawableFxNode(drawable.getClip(), drawing)), drawable.clipProperty());
         fxDrawableNode.layoutXProperty().bind(drawable.layoutXProperty());
         fxDrawableNode.layoutYProperty().bind(drawable.layoutYProperty());
         fxDrawableNode.onMouseClickedProperty().bind(new ConvertedProperty<>(drawable.onMouseClickedProperty(), FxDrawableViewImpl::toFxMouseEventHandler));
@@ -50,6 +55,15 @@ abstract class FxDrawableViewImpl<D extends Drawable, N extends Node> implements
 
     private static EventHandler<? super javafx.scene.input.MouseEvent> toFxMouseEventHandler(UiEventHandler<? super MouseEvent> mouseEventHandler) {
         return mouseEventHandler == null ? null : (EventHandler<javafx.scene.input.MouseEvent>) event -> mouseEventHandler.handle(new FxMouseEvent(event));
+    }
+
+    private static Node getDrawableFxNode(Drawable drawable, DrawingImpl drawing) {
+        if (drawable != null) {
+            DrawableView drawableView = drawing.getOrCreateAndBindDrawableView(drawable);
+            if (drawableView instanceof FxDrawableView)
+                return ((FxDrawableView) drawableView).getFxDrawableNode();
+        }
+        return null;
     }
 
 }
