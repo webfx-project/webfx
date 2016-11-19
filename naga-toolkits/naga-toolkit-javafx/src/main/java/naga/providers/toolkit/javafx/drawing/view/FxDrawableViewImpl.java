6f9258e96df52e7ce8578d6f30cf106c082a1c46
Bruno Salmon <dev.salmonb@gmail.com>
2016-11-19 11:50:44 +0100

    Implemented blendModeProperty in JavaFx toolkit

--- naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/drawing/view/FxDrawableViewImpl.java
+++ naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/drawing/view/FxDrawableViewImpl.java
@@ -6,6 +6,7 @@ import javafx.event.EventHandler;
 import javafx.scene.Node;
 import naga.providers.toolkit.javafx.events.FxMouseEvent;
 import naga.providers.toolkit.javafx.util.FxTransforms;
+import naga.toolkit.drawing.shapes.BlendMode;
 import naga.toolkit.drawing.shapes.Drawable;
 import naga.toolkit.drawing.spi.impl.DrawingImpl;
 import naga.toolkit.drawing.spi.view.DrawableView;
@@ -29,6 +30,7 @@ abstract class FxDrawableViewImpl<D extends Drawable, N extends Node> implements
         fxDrawableNode.opacityProperty().bind(drawable.opacityProperty());
         DrawingImpl drawing = DrawingImpl.getThreadLocalDrawing();
         Properties.runNowAndOnPropertiesChange((clipProperty) -> fxDrawableNode.setClip(getDrawableFxNode(drawable.getClip(), drawing)), drawable.clipProperty());
+        Properties.runNowAndOnPropertiesChange((blendMode) -> fxDrawableNode.setBlendMode(toFxBlendMode(drawable.getBlendMode())), drawable.blendModeProperty());
         fxDrawableNode.layoutXProperty().bind(drawable.layoutXProperty());
         fxDrawableNode.layoutYProperty().bind(drawable.layoutYProperty());
         fxDrawableNode.onMouseClickedProperty().bind(new ConvertedProperty<>(drawable.onMouseClickedProperty(), FxDrawableViewImpl::toFxMouseEventHandler));
@@ -55,6 +57,30 @@ abstract class FxDrawableViewImpl<D extends Drawable, N extends Node> implements
 
     private static EventHandler<? super javafx.scene.input.MouseEvent> toFxMouseEventHandler(UiEventHandler<? super MouseEvent> mouseEventHandler) {
         return mouseEventHandler == null ? null : (EventHandler<javafx.scene.input.MouseEvent>) event -> mouseEventHandler.handle(new FxMouseEvent(event));
+    }
+
+    private static javafx.scene.effect.BlendMode toFxBlendMode(BlendMode blendMode) {
+        if (blendMode != null)
+            switch (blendMode) {
+                case SRC_OVER: return javafx.scene.effect.BlendMode.SRC_OVER;
+                case SRC_ATOP: return javafx.scene.effect.BlendMode.SRC_ATOP;
+                case ADD: return javafx.scene.effect.BlendMode.ADD;
+                case MULTIPLY: return javafx.scene.effect.BlendMode.MULTIPLY;
+                case SCREEN: return javafx.scene.effect.BlendMode.SCREEN;
+                case OVERLAY: return javafx.scene.effect.BlendMode.OVERLAY;
+                case DARKEN: return javafx.scene.effect.BlendMode.DARKEN;
+                case LIGHTEN: return javafx.scene.effect.BlendMode.LIGHTEN;
+                case COLOR_DODGE: return javafx.scene.effect.BlendMode.COLOR_DODGE;
+                case COLOR_BURN: return javafx.scene.effect.BlendMode.COLOR_BURN;
+                case HARD_LIGHT: return javafx.scene.effect.BlendMode.HARD_LIGHT;
+                case SOFT_LIGHT: return javafx.scene.effect.BlendMode.SOFT_LIGHT;
+                case DIFFERENCE: return javafx.scene.effect.BlendMode.DIFFERENCE;
+                case EXCLUSION: return javafx.scene.effect.BlendMode.EXCLUSION;
+                case RED: return javafx.scene.effect.BlendMode.RED;
+                case GREEN: return javafx.scene.effect.BlendMode.GREEN;
+                case BLUE: return javafx.scene.effect.BlendMode.BLUE;
+            }
+        return null;
     }
 
     private static Node getDrawableFxNode(Drawable drawable, DrawingImpl drawing) {
