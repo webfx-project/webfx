f1bbf8b6283fc061357b5732741db89843225219
Bruno Salmon <dev.salmonb@gmail.com>
2016-11-19 11:51:00 +0100

    Implemented blendModeProperty in Swing toolkit

--- naga-toolkits/naga-toolkit-swing/src/main/java/naga/providers/toolkit/swing/drawing/view/SwingDrawableView.java
+++ naga-toolkits/naga-toolkit-swing/src/main/java/naga/providers/toolkit/swing/drawing/view/SwingDrawableView.java
@@ -1,6 +1,8 @@
 package naga.providers.toolkit.swing.drawing.view;
 
+import naga.providers.toolkit.swing.util.SwingBlendModes;
 import naga.providers.toolkit.swing.util.SwingTransforms;
+import naga.toolkit.drawing.shapes.BlendMode;
 import naga.toolkit.drawing.shapes.Drawable;
 import naga.toolkit.drawing.shapes.Point2D;
 import naga.toolkit.drawing.spi.DrawingRequester;
@@ -29,7 +31,7 @@ public abstract class SwingDrawableView
         implements CanvasDrawableView<D, Graphics2D> {
 
     private AffineTransform swingTransform;
-    private AlphaComposite swingAlphaComposite;
+    private Composite swingComposite;
     private DrawingImpl drawing;
     private SwingShapeView swingClipView;
     private Shape swingClip;
@@ -51,13 +53,15 @@ public abstract class SwingDrawableView
             tx.concatenate(swingTransform);
             g.setTransform(tx);
         }
-        if (swingAlphaComposite != null) {
+        if (swingComposite != null) {
+/*
             Composite composite = g.getComposite();
             if (composite instanceof AlphaComposite) {
                 AlphaComposite alphaComposite = (AlphaComposite) composite;
-                g.setComposite(alphaComposite.derive(swingAlphaComposite.getAlpha() * alphaComposite.getAlpha()));
+                g.setComposite(alphaComposite.derive(swingComposite.getAlpha() * alphaComposite.getAlpha()));
             } else
-                g.setComposite(swingAlphaComposite);
+*/
+                g.setComposite(swingComposite);
         }
         if (swingClipView != null) {
             if (swingClip == null)
@@ -76,7 +80,17 @@ public abstract class SwingDrawableView
 
     @Override
     public void updateOpacity(Double opacity) {
-        swingAlphaComposite = opacity != null && opacity != 1d ? AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity.floatValue()) : null;
+        updateComposite();
+    }
+
+    @Override
+    public void updateBlendMode(BlendMode blendMode) {
+        updateComposite();
+    }
+
+    private void updateComposite() {
+        D drawable = getDrawable();
+        swingComposite = SwingBlendModes.toComposite(drawable.getBlendMode(), drawable.getOpacity());
     }
 
     @Override
