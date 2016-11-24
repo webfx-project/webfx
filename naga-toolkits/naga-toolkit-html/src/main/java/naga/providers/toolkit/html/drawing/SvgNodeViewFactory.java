4d647c996ca10289f2162a5621bf62a233ddf2df
Bruno Salmon <dev.salmonb@gmail.com>
2016-11-24 17:31:21 +0100

    Replaced SvgVBoxView/SvgHBoxView with SvgRegionView in Html toolkit (and used it also as view for the BorderPane)

--- naga-toolkits/naga-toolkit-html/src/main/java/naga/providers/toolkit/html/drawing/SvgNodeViewFactory.java
+++ naga-toolkits/naga-toolkit-html/src/main/java/naga/providers/toolkit/html/drawing/SvgNodeViewFactory.java
@@ -1,11 +1,13 @@
 package naga.providers.toolkit.html.drawing;
 
 import naga.providers.toolkit.html.drawing.view.*;
+import naga.toolkit.drawing.layout.impl.BorderPaneImpl;
 import naga.toolkit.drawing.layout.impl.HBoxImpl;
 import naga.toolkit.drawing.layout.impl.VBoxImpl;
 import naga.toolkit.drawing.scene.impl.EmbedGuiNodeImpl;
 import naga.toolkit.drawing.scene.impl.GroupImpl;
-import naga.toolkit.drawing.shape.impl.*;
+import naga.toolkit.drawing.shape.impl.CircleImpl;
+import naga.toolkit.drawing.shape.impl.RectangleImpl;
 import naga.toolkit.drawing.spi.impl.NodeViewFactoryImpl;
 import naga.toolkit.drawing.text.impl.TextShapeImpl;
 
@@ -20,9 +22,10 @@ class SvgNodeViewFactory extends NodeViewFactoryImpl {
         registerNodeViewFactory(RectangleImpl.class, SvgRectangleView::new);
         registerNodeViewFactory(CircleImpl.class, SvgCircleView::new);
         registerNodeViewFactory(TextShapeImpl.class, SvgTextShapeView::new);
-        registerNodeViewFactory(GroupImpl.class, SvgGroupView::new);
         registerNodeViewFactory(EmbedGuiNodeImpl.class, SvgEmbedGuiNodeView::new);
-        registerNodeViewFactory(VBoxImpl.class, SvgVBoxView::new);
-        registerNodeViewFactory(HBoxImpl.class, SvgHBoxView::new);
+        registerNodeViewFactory(GroupImpl.class, SvgGroupView::new);
+        registerNodeViewFactory(VBoxImpl.class, SvgRegionView::new);
+        registerNodeViewFactory(HBoxImpl.class, SvgRegionView::new);
+        registerNodeViewFactory(BorderPaneImpl.class, SvgRegionView::new);
     }
 }
---
+++ naga-toolkits/naga-toolkit-html/src/main/java/naga/providers/toolkit/html/drawing/view/SvgRegionView.java
@@ -0,0 +1,18 @@
+package naga.providers.toolkit.html.drawing.view;
+
+import naga.providers.toolkit.html.util.SvgUtil;
+import naga.toolkit.drawing.layout.Region;
+import naga.toolkit.drawing.spi.view.base.RegionViewBase;
+import naga.toolkit.drawing.spi.view.base.RegionViewMixin;
+
+/**
+ * @author Bruno Salmon
+ */
+public class SvgRegionView
+        extends SvgNodeView<Region, RegionViewBase, RegionViewMixin>
+        implements RegionViewMixin {
+
+    public SvgRegionView() {
+        super(new RegionViewBase(), SvgUtil.createSvgGroup());
+    }
+}
