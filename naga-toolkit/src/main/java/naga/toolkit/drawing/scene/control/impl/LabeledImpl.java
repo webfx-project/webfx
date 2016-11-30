1bcb0693deecb01f731e4b76f33e294944aeda0b
Bruno Salmon <dev.salmonb@gmail.com>
2016-11-30 18:20:11 +0100

    Fixed LabeledImpl: text and image properties changes were not reflected in the layout

--- naga-toolkit/src/main/java/naga/toolkit/drawing/scene/control/impl/LabeledImpl.java
+++ naga-toolkit/src/main/java/naga/toolkit/drawing/scene/control/impl/LabeledImpl.java
@@ -2,8 +2,10 @@ package naga.toolkit.drawing.scene.control.impl;
 
 import javafx.beans.property.Property;
 import javafx.beans.property.SimpleObjectProperty;
+import naga.toolkit.drawing.scene.Parent;
 import naga.toolkit.drawing.scene.control.Labeled;
 import naga.toolkit.spi.nodes.controls.Image;
+import naga.toolkit.util.Properties;
 
 /**
  * @author Bruno Salmon
@@ -22,4 +24,12 @@ public class LabeledImpl extends ControlImpl implements Labeled {
         return imageProperty;
     }
 
+    {
+        // Requesting a new layout pass on text and image properties change
+        Properties.runOnPropertiesChange(property -> {
+            Parent parent = getParent();
+            if (parent != null)
+                parent.requestLayout();
+        }, textProperty, imageProperty);
+    }
 }
