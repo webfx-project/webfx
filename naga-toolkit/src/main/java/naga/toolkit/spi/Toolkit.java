0d73521d2c1ddc3f54d2162d5fa8214334c387b3
Bruno Salmon <dev.salmonb@gmail.com>
2016-11-30 18:10:57 +0100

    Added createDrawingNode(Node rootNode) in Toolkit

--- naga-toolkit/src/main/java/naga/toolkit/spi/Toolkit.java
+++ naga-toolkit/src/main/java/naga/toolkit/spi/Toolkit.java
@@ -8,6 +8,7 @@ import naga.commons.util.function.Converter;
 import naga.commons.util.function.Factory;
 import naga.commons.util.serviceloader.ServiceLoaderHelper;
 import naga.toolkit.display.DisplayResultSet;
+import naga.toolkit.drawing.scene.Node;
 import naga.toolkit.drawing.spi.DrawingNode;
 import naga.toolkit.properties.conversion.ConvertedObservableList;
 import naga.toolkit.properties.markers.HasTextProperty;
@@ -270,5 +271,11 @@ public abstract class Toolkit {
 
     public DrawingNode createDrawingNode() {
         return createNode(DrawingNode.class);
+    }
+
+    public DrawingNode createDrawingNode(Node rootNode) {
+        DrawingNode drawingNode = createDrawingNode();
+        drawingNode.setRootNode(rootNode);
+        return drawingNode;
     }
 }
