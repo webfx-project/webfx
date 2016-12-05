e890be1e82d4f6f1396a5f53738f42fbdc135f28
Bruno Salmon <dev.salmonb@gmail.com>
2016-12-05 16:31:25 +0100

    Implemented min/pref/max width/height in FxRegionView

--- naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/fx/view/FxRegionView.java
+++ naga-toolkits/naga-toolkit-javafx/src/main/java/naga/providers/toolkit/javafx/fx/view/FxRegionView.java
@@ -1,14 +1,42 @@
 package naga.providers.toolkit.javafx.fx.view;
 
+import javafx.beans.property.Property;
 import naga.toolkit.fx.scene.layout.Region;
+import naga.toolkit.fx.spi.view.RegionView;
+
+import java.lang.reflect.Method;
 
 /**
  * @author Bruno Salmon
  */
-public class FxRegionView<N extends Region> extends FxNodeViewImpl<N, javafx.scene.layout.Region> {
+abstract class FxRegionView<N extends Region, FxN extends javafx.scene.layout.Region> extends FxNodeViewImpl<N, FxN> implements RegionView<N> {
 
     @Override
-    javafx.scene.layout.Region createFxNode(N node) {
-        return new javafx.scene.layout.Region();
+    void setAndBindNodeProperties(N buttonBase, FxN fxButtonBase) {
+        super.setAndBindNodeProperties(buttonBase, fxButtonBase);
+        fxButtonBase.minWidthProperty().bind(buttonBase.minWidthProperty());
+        fxButtonBase.maxWidthProperty().bind(buttonBase.maxWidthProperty());
+        fxButtonBase.minHeightProperty().bind(buttonBase.minHeightProperty());
+        fxButtonBase.maxHeightProperty().bind(buttonBase.maxHeightProperty());
+        fxButtonBase.prefWidthProperty().bind(buttonBase.prefWidthProperty());
+        fxButtonBase.prefHeightProperty().bind(buttonBase.prefHeightProperty());
+        bindRegionSetterToProperty("setWidth", buttonBase.widthProperty());
+        bindRegionSetterToProperty("setHeight", buttonBase.heightProperty());
+    }
+
+    private void bindRegionSetterToProperty(String setterName, Property<Double> property) {
+        try {
+            Method regionSetter = javafx.scene.layout.Region.class.getDeclaredMethod(setterName, double.class);
+            regionSetter.setAccessible(true);
+            property.addListener((observable, oldValue, newWidth) -> {
+                try {
+                    regionSetter.invoke(getFxNode(), newWidth);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
