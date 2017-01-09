ab89837c3802c4c2b1cc20ff11645187efaaf4b5
Bruno Salmon <dev.salmonb@gmail.com>
2017-01-09 11:14:11 +0100

    Added createDefaultRegionViewer() to NodeViewFactoryImpl (to reduce layout factory registering)

--- naga-fx/src/main/java/naga/fx/spi/viewer/base/NodeViewerFactoryImpl.java
+++ naga-fx/src/main/java/naga/fx/spi/viewer/base/NodeViewerFactoryImpl.java
@@ -2,6 +2,7 @@ package naga.fx.spi.viewer.base;
 
 import naga.commons.util.function.Factory;
 import naga.fx.scene.Node;
+import naga.fx.scene.layout.Region;
 import naga.fx.spi.viewer.NodeViewer;
 import naga.fx.spi.viewer.NodeViewerFactory;
 
@@ -11,20 +12,24 @@ import java.util.Map;
 /**
  * @author Bruno Salmon
  */
-public class NodeViewerFactoryImpl implements NodeViewerFactory {
+public abstract class NodeViewerFactoryImpl implements NodeViewerFactory {
 
     private Map<Class<? extends Node>, Factory<? extends NodeViewer>> nodeViewerFactories = new HashMap<>();
 
-    public <N extends Node, V extends NodeViewer<? super N>> void registerNodeViewerFactory(Class<N> nodeClass, Factory<V> factory) {
+    protected <N extends Node, V extends NodeViewer<? super N>> void registerNodeViewerFactory(Class<N> nodeClass, Factory<V> factory) {
         nodeViewerFactories.put(nodeClass, factory);
     }
 
     @Override
-    public <N extends Node, V extends NodeViewer<N>> V createNodeViewer(N nodeInterface) {
-        Factory<? extends NodeViewer> factory = nodeViewerFactories.get(nodeInterface.getClass());
+    public <N extends Node, V extends NodeViewer<N>> V createNodeViewer(N node) {
+        Factory<? extends NodeViewer> factory = nodeViewerFactories.get(node.getClass());
         if (factory != null)
             return (V) factory.create();
-        System.out.println("WARNING: No NodeViewer factory registered for " + nodeInterface.getClass() + " in " + getClass());
+        if (node instanceof Region)
+            return (V) createDefaultRegionViewer((Region) node);
+        System.out.println("WARNING: No NodeViewer factory registered for " + ((Node) node).getClass() + " in " + getClass());
         return null;
     }
+
+    protected abstract NodeViewer<Region> createDefaultRegionViewer(Region node);
 }
