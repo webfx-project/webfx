f69f941e622d902319a6c372711c4864624c9f3d
Bruno Salmon <dev.salmonb@gmail.com>
2017-01-09 11:03:48 +0100

    Added setSizeChangedCallback() to LayoutMeasurable (ex: when the peer skin has changed)

--- naga-fx/src/main/java/naga/fx/scene/LayoutMeasurable.java
+++ naga-fx/src/main/java/naga/fx/scene/LayoutMeasurable.java
@@ -21,4 +21,5 @@ public interface LayoutMeasurable {
 
     double prefHeight(double width);
 
+    default void setSizeChangedCallback(Runnable sizeChangedCallback) {} // Ex: when the peer skin has changed
 }
