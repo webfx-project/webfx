0e205e9999bff0120a813598211973593651cc37
Bruno Salmon <dev.salmonb@gmail.com>
2017-01-10 17:41:03 +0100

    Added modal background when necessary to GwtSecondaryStagePeer

--- naga-fx-providers/naga-fx-gwt/src/main/java/naga/fx/spi/gwt/GwtSecondaryStagePeer.java
+++ naga-fx-providers/naga-fx-gwt/src/main/java/naga/fx/spi/gwt/GwtSecondaryStagePeer.java
@@ -1,14 +1,15 @@
 package naga.fx.spi.gwt;
 
 import elemental2.CSSStyleDeclaration;
+import elemental2.HTMLBodyElement;
 import elemental2.HTMLElement;
-import naga.fx.spi.peer.StagePeer;
 import naga.fx.spi.gwt.html.HtmlScenePeer;
 import naga.fx.spi.gwt.util.HtmlUtil;
+import naga.fx.spi.peer.StagePeer;
+import naga.fx.stage.Modality;
 import naga.fx.stage.Stage;
 import naga.fx.stage.Window;
 import naga.fx.sun.tk.TKStageListener;
-import naga.platform.spi.Platform;
 
 import static elemental2.Global.window;
 
@@ -19,16 +20,15 @@ class GwtSecondaryStagePeer implements StagePeer {
 
     private final Stage stage;
     private TKStageListener listener;
-    private final HTMLElement container = HtmlUtil.createDivElement();
-    private final CSSStyleDeclaration style = container.style;
+    private HTMLElement modalBackgroundDiv;
+    private final HTMLElement stageDiv = HtmlUtil.createDivElement();
+    private final CSSStyleDeclaration stageDivStyle = stageDiv.style;
 
     GwtSecondaryStagePeer(Stage stage) {
         this.stage = stage;
-        style.position = "absolute";
-        style.overflow = "hidden";
-        style.border = "orange 3px solid";
-        style.visibility = "hidden";
-        window.document.body.appendChild(container);
+        stageDivStyle.position = "absolute";
+        stageDivStyle.overflow = "hidden";
+        stageDivStyle.border = "orange 3px solid";
     }
 
     @Override
@@ -38,25 +38,25 @@ class GwtSecondaryStagePeer implements StagePeer {
 
     @Override
     public void setBounds(float x, float y, boolean xSet, boolean ySet, float w, float h, float cw, float ch, float xGravity, float yGravity) {
-        Platform.log("x = " + x + ", y = " + y + ", w = " + w + ", h = " + h + ", cw = " + cw + ", ch = " + ch);
+        //Platform.log("x = " + x + ", y = " + y + ", w = " + w + ", h = " + h + ", cw = " + cw + ", ch = " + ch);
         if (xSet)
-            style.left = x + "px";
+            stageDivStyle.left = x + "px";
         if (ySet)
-            style.top = y + "px";
+            stageDivStyle.top = y + "px";
         if (w < 0 && cw > 0)
             w = cw + 6;
         if (h < 0 && ch > 0)
             h = ch + 6;
         if (w > 0)
-            style.width = w + "px";
+            stageDivStyle.width = w + "px";
         if (h > 0)
-            style.height = h + "px";
+            stageDivStyle.height = h + "px";
         changedWindowSize();
     }
 
     private void changedWindowSize() {
-        float clientWidth = (float) container.clientWidth;
-        float clientHeight = (float) container.clientHeight;
+        float clientWidth = (float) stageDiv.clientWidth;
+        float clientHeight = (float) stageDiv.clientHeight;
         if (listener != null)
             listener.changedSize(clientWidth, clientHeight);
         ((HtmlScenePeer) stage.getScene().impl_getPeer()).changedWindowSize(clientWidth, clientHeight);
@@ -75,10 +75,23 @@ class GwtSecondaryStagePeer implements StagePeer {
     @Override
     public void setVisible(boolean visible) {
         if (visible != this.visible) {
-            if (visible)
-                style.visibility = null;
-            else
-                window.document.body.removeChild(container);
+            HTMLBodyElement body = window.document.body;
+            if (visible) {
+                if (stage.getModality() != Modality.NONE) {
+                    modalBackgroundDiv = HtmlUtil.createDivElement();
+                    CSSStyleDeclaration style = modalBackgroundDiv.style;
+                    style.position = "absolute";
+                    style.top = style.bottom = style.left = style.right = "0";
+                    style.background = "black";
+                    style.opacity = 0.5;
+                    body.appendChild(modalBackgroundDiv);
+                }
+                body.appendChild(stageDiv);
+            } else {
+                body.removeChild(stageDiv);
+                if (modalBackgroundDiv != null)
+                    body.removeChild(modalBackgroundDiv);
+            }
             this.visible = visible;
         }
     }
@@ -89,7 +102,7 @@ class GwtSecondaryStagePeer implements StagePeer {
     }
 
     private void setWindowContent(elemental2.Node content) {
-        HtmlUtil.setChild(container, content);
+        HtmlUtil.setChild(stageDiv, content);
     }
 
 }
