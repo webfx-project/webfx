b40d3634f4bde850e6a2cf977ee959c64d282c33
Bruno Salmon <dev.salmonb@gmail.com>
2017-01-18 19:50:41 +0100

    Removed unnecessary BehaviorSubject fields in presentation models

--- mongoose/mongoose-logic/src/main/java/mongoose/activities/backend/container/BackendContainerPresentationModel.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/backend/container/BackendContainerPresentationModel.java
@@ -1,18 +1,10 @@
 package mongoose.activities.backend.container;
 
 import mongoose.activities.shared.container.ContainerPresentationModel;
-import javafx.event.ActionEvent;
-import rx.subjects.BehaviorSubject;
 
 /**
  * @author Bruno Salmon
  */
 class BackendContainerPresentationModel extends ContainerPresentationModel {
-
-    private final BehaviorSubject<ActionEvent> bookingsButtonActionEventObservable = BehaviorSubject.create();
-    BehaviorSubject<ActionEvent> bookingsButtonActionEventObservable() { return bookingsButtonActionEventObservable; }
-
-    private final BehaviorSubject<ActionEvent> lettersButtonActionEventObservable = BehaviorSubject.create();
-    BehaviorSubject<ActionEvent> lettersButtonActionEventObservable() { return lettersButtonActionEventObservable; }
 
 }
--- mongoose/mongoose-logic/src/main/java/mongoose/activities/shared/container/ContainerPresentationModel.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/shared/container/ContainerPresentationModel.java
@@ -1,20 +1,10 @@
 package mongoose.activities.shared.container;
 
 import naga.framework.ui.presentation.PresentationModel;
-import javafx.event.ActionEvent;
-import rx.subjects.BehaviorSubject;
 
 /**
  * @author Bruno Salmon
  */
 public class ContainerPresentationModel implements PresentationModel {
-
-    // Display input
-
-    private final BehaviorSubject<ActionEvent> organizationButtonActionEventObservable = BehaviorSubject.create();
-    BehaviorSubject<ActionEvent> organizationsButtonActionEventObservable() { return organizationButtonActionEventObservable; }
-
-    private final BehaviorSubject<ActionEvent> eventsButtonActionEventObservable = BehaviorSubject.create();
-    BehaviorSubject<ActionEvent> eventsButtonActionEventObservable() { return eventsButtonActionEventObservable; }
 
 }
