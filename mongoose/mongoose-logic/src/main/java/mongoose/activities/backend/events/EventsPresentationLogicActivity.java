8a59cd00a7b66513ca8a80d3d15692f49b972009
Bruno Salmon <dev.salmonb@gmail.com>
2017-01-25 14:58:55 +0100

    Changed Events activity default condition to active

--- mongoose/mongoose-logic/src/main/java/mongoose/activities/backend/events/EventsPresentationLogicActivity.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/backend/events/EventsPresentationLogicActivity.java
@@ -23,13 +23,14 @@ public class EventsPresentationLogicActivity extends DomainPresentationLogicActi
     @Override
     protected void startLogic(EventsPresentationModel pm) {
         // Loading the domain model and setting up the reactive filter
-        createReactiveExpressionFilter("{class: 'Event', alias: 'e', fields2: '(select count(1) from Document where !cancelled and event=e) as bookingsCount', where2: 'live', orderBy: 'startDate desc,id desc'}")
+        createReactiveExpressionFilter("{class: 'Event', alias: 'e', fields2: '(select count(1) from Document where !cancelled and event=e) as bookingsCount', where: 'active', orderBy: 'startDate desc,id desc'}")
                 // Search box condition
                 .combine(pm.searchTextProperty(), s -> s == null ? null : "{where: 'lower(name) like `%" + s.toLowerCase() + "%`'}")
                 .combine(pm.organizationIdProperty(), o -> o == null ? null : "{where: 'organization=" + o + "'}")
                 // Limit condition
-                //.combine(pm.withBookingsProperty(), "{where: '(select count(1) from Document where !cancelled and event=e) > 0'}")
                 .combine(pm.limitProperty(), "{limit: '100'}")
+                // With bookings condition
+                //.combine(pm.withBookingsProperty(), "{where: '(select count(1) from Document where !cancelled and event=e) > 0'}")
                 .setExpressionColumns("[" +
                         //"{label: 'Image', expression: 'image(`images/calendar.svg`)'}," +
                         //"{label: 'Event', expression: 'icon, name + ` ~ ` + dateIntervalFormat(startDate,endDate) + ` (` + bookingsCount + `)`'}" +
