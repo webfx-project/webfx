fe42be1d94ca8cd4fa98a3755ef2860903316cc1
Bruno Salmon <dev.salmonb@gmail.com>
2017-01-25 15:00:31 +0100

    Extracted new DayTimeRangeEditor class from OptionsViewActivity

---
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/shared/bookingform/options/DayTimeRangeEditor.java
@@ -0,0 +1,103 @@
+package mongoose.activities.shared.bookingform.options;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.stage.Window;
+import mongoose.activities.shared.logic.time.DayTimeRange;
+import naga.commons.util.function.BiConsumer;
+import naga.fx.spi.Toolkit;
+
+/**
+ * @author Bruno Salmon
+ */
+public class DayTimeRangeEditor {
+
+    public interface DialogCallback {
+
+        void closeDialog();
+
+        void showException(Throwable e);
+
+    }
+
+    public static void showDayTimeRangeEditorDialog(DayTimeRange dayTimeRange, String headerText, BiConsumer<DayTimeRange, DialogCallback> okConsumer, Window ownerWindow) {
+        String dayTimeRangeText = dayTimeRange.getText();
+        TextInputDialog dialog = new TextInputDialog(dayTimeRangeText);
+        dialog.initOwner(ownerWindow);
+        dialog.setTitle("Editing day time range");
+        dialog.setHeaderText(headerText);
+        dialog.setContentText("Day time range:");
+/*
+                Property<Boolean> okDisabledProperty = dialog.getDialogPane().lookupButton(ButtonType.OK).disabledProperty();
+                Properties.runNowAndOnPropertiesChange(p -> okDisabledProperty.setValue(dialog.getEditor().getText().equals(dialog.getDefaultValue())), dialog.getEditor().textProperty());
+*/
+        dialog.show();
+        dialog.setOnCloseRequest(dialogEvent -> {
+            String newDayTimeRangeText = dialog.getResult();
+            if (newDayTimeRangeText == null /* Cancel button */ || newDayTimeRangeText.equals(dayTimeRangeText) /* Ok button but no change */)
+                return; // No change => we return immediately and this will close the dialog
+            // The user made a change, we don't close the dialog yet, we keep it open until the change is recorded in the database
+            dialogEvent.consume(); // This will prevent the dialog to close
+            okConsumer.accept(DayTimeRange.parse(newDayTimeRangeText), new DialogCallback() {
+                @Override
+                public void closeDialog() {
+                    Toolkit.get().scheduler().runInUiThread(() -> {
+                        dialog.setResult(null); // Resetting result back to null for next pass
+                        dialog.close();
+                    });
+                }
+
+                @Override
+                public void showException(Throwable e) {
+                    Toolkit.get().scheduler().runInUiThread(() -> {
+                        dialog.setResult(null); // Resetting result back to null for next pass
+                        showExceptionAlert(e, dialog.getOwner());
+                    });
+                }
+            });
+        });
+    }
+
+    private static void showExceptionAlert(Throwable e, Window owner) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.initOwner(owner);
+        alert.setTitle("Exception Dialog");
+        alert.setHeaderText("Look, an Exception Dialog");
+        alert.setContentText(e.getMessage());
+
+// Create expandable Exception.
+
+        StringBuilder sb = new StringBuilder();
+        for (StackTraceElement element : e.getStackTrace())
+            sb.append(element).append('\n');
+        String exceptionText = sb.toString();
+
+        Label label = new Label("The exception stacktrace was:");
+
+        Label textArea = new Label(exceptionText);
+/*
+        TextArea textArea = new TextArea(exceptionText);
+        textArea.setEditable(false);
+        textArea.setWrapText(true);
+*/
+
+        textArea.setMaxWidth(Double.MAX_VALUE);
+        textArea.setMaxHeight(Double.MAX_VALUE);
+        GridPane.setVgrow(textArea, Priority.ALWAYS);
+        GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+        GridPane expContent = new GridPane();
+        expContent.setMaxWidth(Double.MAX_VALUE);
+        expContent.add(label, 0, 0);
+        expContent.add(textArea, 0, 1);
+
+// Set expandable Exception into the dialog pane.
+        alert.getDialogPane().setExpandableContent(expContent);
+
+        alert.show();
+    }
+
+}
--- mongoose/mongoose-logic/src/main/java/mongoose/activities/shared/bookingform/options/OptionsViewActivity.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/shared/bookingform/options/OptionsViewActivity.java
@@ -1,26 +1,25 @@
 package mongoose.activities.shared.bookingform.options;
 
 import javafx.scene.Node;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextInputDialog;
-import javafx.scene.layout.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
-import javafx.stage.Window;
 import mongoose.activities.shared.bookingform.shared.BookingProcessViewActivity;
-import mongoose.activities.shared.logic.ui.highlevelcomponents.HighLevelComponents;
 import mongoose.activities.shared.logic.calendar.CalendarTimeline;
-import mongoose.activities.shared.logic.ui.calendargraphic.CalendarCell;
-import mongoose.activities.shared.logic.ui.calendargraphic.CalendarClickEvent;
-import mongoose.activities.shared.logic.ui.calendargraphic.CalendarGraphic;
 import mongoose.activities.shared.logic.price.DocumentPricing;
 import mongoose.activities.shared.logic.time.DateTimeRange;
 import mongoose.activities.shared.logic.time.TimeInterval;
+import mongoose.activities.shared.logic.ui.calendargraphic.CalendarCell;
+import mongoose.activities.shared.logic.ui.calendargraphic.CalendarClickEvent;
+import mongoose.activities.shared.logic.ui.calendargraphic.CalendarGraphic;
+import mongoose.activities.shared.logic.ui.highlevelcomponents.HighLevelComponents;
 import mongoose.activities.shared.logic.work.WorkingDocument;
 import mongoose.activities.shared.logic.work.WorkingDocumentLine;
 import mongoose.domainmodel.format.PriceFormatter;
 import mongoose.entities.Option;
 import naga.framework.orm.entity.UpdateStore;
+import naga.fx.spi.Toolkit;
 import naga.platform.spi.Platform;
 
 import java.util.concurrent.TimeUnit;
@@ -60,11 +59,12 @@ public class OptionsViewActivity extends BookingProcessViewActivity {
 
     private void createAndShowCalendarIfBothLogicAndViewAreReady() {
         WorkingDocument workingDocument = getWorkingDocument();
-        if (workingDocument != null) {
+        if (workingDocument != null)
+            Toolkit.get().scheduler().runInUiThread(() -> {
             workingDocumentCalendarGraphic = createOrUpdateCalendarGraphicFromWorkingDocument(workingDocument, workingDocumentCalendarGraphic);
             workingDocumentCalendarGraphic.setCalendarClickHandler(this::onCalendarClick);
             showCalendarIfBothLogicAndViewAreReady();
-        }
+        });
     }
 
     private void onCalendarClick(CalendarClickEvent event) {
@@ -81,42 +81,28 @@ public class OptionsViewActivity extends BookingProcessViewActivity {
             CalendarTimeline calendarTimeline = event.getCalendarTimeline();
             Option option = getCalendarTimelineOption(calendarTimeline);
             if (option != null) {
-                String dayTimeRange = calendarTimeline.getDayTimeRange().getText();
-                TextInputDialog dialog = new TextInputDialog(dayTimeRange);
-                dialog.initOwner(calendarPanel.getScene().getWindow());
-                dialog.setTitle("Editing option day time range");
-                dialog.setHeaderText("Option: " + calendarTimeline.displayNameProperty().getValue());
-                dialog.setContentText("Day time range:");
-/*
-                Property<Boolean> okDisabledProperty = dialog.getDialogPane().lookupButton(ButtonType.OK).disabledProperty();
-                Properties.runNowAndOnPropertiesChange(p -> okDisabledProperty.setValue(dialog.getEditor().getText().equals(dialog.getDefaultValue())), dialog.getEditor().textProperty());
-*/
-                dialog.show();
-                dialog.setOnCloseRequest(dialogEvent -> {
-                    String newDayTimeRange = dialog.getResult();
-                    if (newDayTimeRange == null /* Cancel button */ || newDayTimeRange.equals(dayTimeRange) /* Ok button but no change */)
-                        return; // No change => we return immediately and this will close the dialog
-                    // The user made a change, we don't close the dialog yet, we keep it open until the change is recorded in the database
-                    dialogEvent.consume(); // This will prevent the dialog to close
-                    // Creating an update store
-                    UpdateStore store = UpdateStore.create(getDataSourceModel());
-                    // Creating an instance of Option entity
-                    Option updatingOption = store.getOrCreateEntity(option.getId());
-                    // Updating the option time range
-                    updatingOption.setTimeRange(dayTimeRange);
-                    // Asking the update record this change in the database
-                    store.executeUpdate().setHandler(asyncResult -> {
-                        dialog.setResult(null); // Resetting result back to null for next pass
-                        if (asyncResult.failed())
-                            showExceptionAlert(asyncResult.cause(), dialog.getOwner());
-                        else {
-                            dialog.close();
-                            // Updating the UI
-                            option.setTimeRange(newDayTimeRange);
-                            createAndShowCalendarIfBothLogicAndViewAreReady();
-                        }
-                    });
-                });
+                DayTimeRangeEditor.showDayTimeRangeEditorDialog(calendarTimeline.getDayTimeRange(),
+                        "Option: " + calendarTimeline.displayNameProperty().getValue(),
+                        (newDayTimeRange, dialogCallback) -> {
+                            // Creating an update store
+                            UpdateStore store = UpdateStore.create(getDataSourceModel());
+                            // Creating an instance of Option entity
+                            Option updatingOption = store.getOrCreateEntity(option.getId());
+                            // Updating the option time range
+                            updatingOption.setTimeRange(newDayTimeRange.getText());
+                            // Asking the update record this change in the database
+                            store.executeUpdate().setHandler(asyncResult -> {
+                                if (asyncResult.failed())
+                                    dialogCallback.showException(asyncResult.cause());
+                                else {
+                                    dialogCallback.closeDialog();
+                                    // Updating the UI
+                                    option.setTimeRange(newDayTimeRange.getText());
+                                    createAndShowCalendarIfBothLogicAndViewAreReady();
+                                }
+                            });
+                        }, calendarPanel.getScene().getWindow()
+                );
             }
         }
     }
@@ -130,44 +116,6 @@ public class OptionsViewActivity extends BookingProcessViewActivity {
         return null;
     }
 
-    private static void showExceptionAlert(Throwable e, Window owner) {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.initOwner(owner);
-        alert.setTitle("Exception Dialog");
-        alert.setHeaderText("Look, an Exception Dialog");
-        alert.setContentText(e.getMessage());
-
-// Create expandable Exception.
-
-        StringBuilder sb = new StringBuilder();
-        for (StackTraceElement element : e.getStackTrace())
-            sb.append(element).append('\n');
-        String exceptionText = sb.toString();
-
-        Label label = new Label("The exception stacktrace was:");
-
-        Label textArea = new Label(exceptionText);
-/*
-        TextArea textArea = new TextArea(exceptionText);
-        textArea.setEditable(false);
-        textArea.setWrapText(true);
-*/
-
-        textArea.setMaxWidth(Double.MAX_VALUE);
-        textArea.setMaxHeight(Double.MAX_VALUE);
-        GridPane.setVgrow(textArea, Priority.ALWAYS);
-        GridPane.setHgrow(textArea, Priority.ALWAYS);
-
-        GridPane expContent = new GridPane();
-        expContent.setMaxWidth(Double.MAX_VALUE);
-        expContent.add(label, 0, 0);
-        expContent.add(textArea, 0, 1);
-
-// Set expandable Exception into the dialog pane.
-        alert.getDialogPane().setExpandableContent(expContent);
-
-        alert.show();
-    }
 
     private void updateArrivalOrDepartureDateTime(CalendarClickEvent event, boolean arrival) {
         CalendarCell cell = event.getCalendarCell();
@@ -176,7 +124,7 @@ public class OptionsViewActivity extends BookingProcessViewActivity {
         TimeInterval currentWorkingDocumentInterval = getWorkingDocument().getDateTimeRange().getInterval().changeTimeUnit(TimeUnit.MINUTES);
         TimeInterval newRequestedDocumentInterval =
                 arrival ? new TimeInterval(clickedDayMinute + dayTimeMinuteInterval.getIncludedStart(), currentWorkingDocumentInterval.getExcludedEnd(), TimeUnit.MINUTES)
-                        :         new TimeInterval(currentWorkingDocumentInterval.getIncludedStart(), clickedDayMinute + cell.getDayTimeMinuteInterval().getExcludedEnd(), TimeUnit.MINUTES);
+                        : new TimeInterval(currentWorkingDocumentInterval.getIncludedStart(), clickedDayMinute + cell.getDayTimeMinuteInterval().getExcludedEnd(), TimeUnit.MINUTES);
         DateTimeRange newWorkingDocumentDateTimeRange = getEvent().computeMaxDateTimeRange().intersect(newRequestedDocumentInterval.toSeries());
         setWorkingDocument(createNewDateTimeRangeWorkingDocument(newWorkingDocumentDateTimeRange));
         createAndShowCalendarIfBothLogicAndViewAreReady();
