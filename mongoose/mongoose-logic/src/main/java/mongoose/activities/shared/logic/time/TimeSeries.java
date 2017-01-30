ee827338d354a8061e0b3fa0cb558cb3edf26339
Bruno Salmon <dev.salmonb@gmail.com>
2017-01-30 18:04:56 +0100

    Fixed TimeSeries.toText(): possible wrong comma

--- mongoose/mongoose-logic/src/main/java/mongoose/activities/shared/logic/time/TimeSeries.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/shared/logic/time/TimeSeries.java
@@ -67,8 +67,9 @@ public final class TimeSeries {
     }
 
     StringBuilder toText(StringBuilder sb) {
+        int index = 0;
         for (TimeInterval interval : array)
-            interval.toText(sb.length() > 0 ? sb.append(", ") : sb);
+            interval.toText(index++ > 0 ? sb.append(", ") : sb);
         return sb;
     }
 
