2a995db0ccf78d3df7dc8a8097ceef2640fb0c75
Bruno Salmon <dev.salmonb@gmail.com>
2017-02-28 22:54:23 +0100

    Made Pair usable for HashMap (implemented equals() and hashCode())

--- naga-commons/src/main/java/naga/commons/util/tuples/Pair.java
+++ naga-commons/src/main/java/naga/commons/util/tuples/Pair.java
@@ -31,4 +31,22 @@ public class Pair<T1, T2> {
     public void set2(T2 o2) {
         this.o2 = o2;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+
+        if (o1 != null ? !o1.equals(pair.o1) : pair.o1 != null) return false;
+        return o2 != null ? o2.equals(pair.o2) : pair.o2 == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = o1 != null ? o1.hashCode() : 0;
+        result = 31 * result + (o2 != null ? o2.hashCode() : 0);
+        return result;
+    }
 }
