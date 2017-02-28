58ff9480c567aa7407abeacfc57e08507fd510d2
Bruno Salmon <dev.salmonb@gmail.com>
2017-02-28 23:08:28 +0100

    Made ViewActivityImpl implement ViewDomainActivityContextMixin

--- naga-framework/src/main/java/naga/framework/activity/view/impl/ViewActivityImpl.java
+++ naga-framework/src/main/java/naga/framework/activity/view/impl/ViewActivityImpl.java
@@ -1,9 +1,11 @@
 package naga.framework.activity.view.impl;
 
+import naga.framework.activity.combinations.viewdomain.ViewDomainActivityContextMixin;
 import naga.framework.activity.combinations.viewdomain.impl.ViewDomainActivityContextFinal;
 
 /**
  * @author Bruno Salmon
  */
-public abstract class ViewActivityImpl extends ViewActivityBase<ViewDomainActivityContextFinal> {
+public abstract class ViewActivityImpl extends ViewActivityBase<ViewDomainActivityContextFinal>
+    implements ViewDomainActivityContextMixin<ViewDomainActivityContextFinal> {
 }
