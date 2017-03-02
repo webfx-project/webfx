8ae0625b09b9b2a191148c2b27b0fba54480c500
Bruno Salmon <dev.salmonb@gmail.com>
2017-03-02 18:23:09 +0100

    Extracted MultiLanguageEditor from EditLetterViewActivity

--- mongoose/mongoose-logic/src/main/java/mongoose/activities/backend/letter/edit/EditLetterViewActivity.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/backend/letter/edit/EditLetterViewActivity.java
@@ -3,45 +3,24 @@ package mongoose.activities.backend.letter.edit;
 import javafx.beans.property.Property;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.Node;
-import javafx.scene.control.*;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import naga.commons.util.Objects;
-import naga.commons.util.tuples.Pair;
+import mongoose.activities.backend.util.MultiLanguageEditor;
 import naga.framework.activity.view.impl.ViewActivityImpl;
-import naga.framework.expression.sqlcompiler.sql.SqlCompiled;
-import naga.framework.orm.entity.Entity;
-import naga.framework.orm.entity.UpdateStore;
-import naga.framework.orm.mapping.QueryResultSetToEntityListGenerator;
-import naga.framework.ui.controls.LayoutUtil;
 import naga.fx.properties.Properties;
-import naga.fx.util.ImageStore;
-import naga.fxdata.control.HtmlTextEditor;
-import naga.platform.services.query.QueryArgument;
-import naga.platform.spi.Platform;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author Bruno Salmon
  */
 public class EditLetterViewActivity extends ViewActivityImpl {
 
-    private final static String[] languages = {"en", "de", "es", "fr", "pt"};
     private final Property<Object> routeLetterIdProperty = new SimpleObjectProperty<>();
-
-    private final Map<Object /*letterId*/, UpdateStore> letterStores = new HashMap<>();
-    private final Map<Pair<Object /*letterId**/, Object /*language*/>, LangLetter> langLetters = new HashMap<>();
 
     @Override
     public void onStart() {
         super.onStart();
-        toggleGroup = new ToggleGroup();
         Properties.runOnPropertiesChange(p -> {
             if (isActive())
                 onLetterChanged();
-        }, routeLetterIdProperty, activeProperty(), toggleGroup.selectedToggleProperty());
+        }, routeLetterIdProperty, activeProperty());
     }
 
     @Override
@@ -49,147 +28,15 @@ public class EditLetterViewActivity extends ViewActivityImpl {
         routeLetterIdProperty.setValue(getParameter("letterId"));
     }
 
-    private BorderPane borderPane;
-    private Map<Object, ToggleButton> languageButtons = new HashMap<>();
-    private ToggleGroup toggleGroup;
+    private MultiLanguageEditor multiLanguageEditor;
     @Override
     public Node buildUi() {
-        borderPane = new BorderPane();
-        for (String language : languages) {
-            ToggleButton languageButton = new ToggleButton(null, ImageStore.createImageView("images/32/system/lang_" + language + ".png"));
-            languageButton.setUserData(language);
-            languageButton.setMinWidth(50d);
-            languageButtons.put(language, languageButton);
-        }
-        toggleGroup.getToggles().setAll(languageButtons.values());
-        toggleGroup.selectToggle(languageButtons.get(getI18n().getLanguage()));
-        LangLetter langLetter = getLangLetter(false);
-        if (langLetter != null)
-            langLetter.displayEditor();
-        return borderPane;
+        multiLanguageEditor = new MultiLanguageEditor(getI18n(), routeLetterIdProperty::getValue, getDataSourceModel(), lang -> lang, lang -> "subject_" + lang, "Letter");
+        return multiLanguageEditor.getUiNode();
     }
 
     private void onLetterChanged() {
-        LangLetter langLetter = getLangLetter(true);
-        langLetter.displayEditor();
-        Object letterId = routeLetterIdProperty.getValue();
-        if (!letterStores.containsKey(letterId)) {
-            SqlCompiled sqlCompiled = getDataSourceModel().getDomainModel().compileSelect("select de,en,es,fr,pt,subject_de,subject_en,subject_es,subject_fr,subject_pt from Letter where id=?");
-            // Then we ask the query service to execute the sql query
-            Platform.getQueryService().executeQuery(new QueryArgument(sqlCompiled.getSql(), new Object[]{letterId}, getDataSourceModel().getId())).setHandler(ar -> {
-                if (ar.succeeded()) {
-                    UpdateStore store = UpdateStore.create(getDataSourceModel());
-                    Entity letterEntity = QueryResultSetToEntityListGenerator.createEntityList(ar.result(), sqlCompiled.getQueryMapping(), store, "letter").get(0);
-                    store.markChangesAsCommitted();
-                    letterStores.put(letterId, store);
-                    langLetter.setLetterEntity(letterEntity);
-                }
-            });
-        } else
-            langLetter.setLetterEntity(letterStores.get(letterId).getEntityList("letter").get(0));
-    }
-
-    private LangLetter getLangLetter(boolean createIfNotExists) {
-        Toggle selectedLanguageButton = toggleGroup.getSelectedToggle();
-        Object language = selectedLanguageButton != null ? selectedLanguageButton.getUserData() : getI18n().getLanguage();
-        Pair<Object, Object> pair = new Pair<>(routeLetterIdProperty.getValue(), language);
-        LangLetter langLetter = langLetters.get(pair);
-        if (langLetter == null && createIfNotExists)
-            langLetters.put(pair, langLetter = new LangLetter(pair.get2()));
-        return langLetter;
-    }
-
-    private class LangLetter {
-        private final TextField subjectTextField = new TextField();
-        private final HtmlTextEditor editor = new HtmlTextEditor();
-        private final Button saveButton = getI18n().translateText(new Button(), "Save");
-        private final Button revertButton = getI18n().translateText(new Button(), "Revert");
-        private final Object subjectField;
-        private final Object bodyField;
-        private UpdateStore letterStore;
-        private Entity letterEntity;
-
-        LangLetter(Object lang) {
-            subjectField = "subject_" + lang;
-            bodyField = lang;
-            Properties.runOnPropertiesChange(p -> syncEntityFromUi(), subjectTextField.textProperty(), editor.textProperty());
-            revertButton.setOnAction(e -> revert());
-            saveButton.setOnAction(e -> save());
-        }
-
-        void syncEntityFromUi() {
-            if (letterEntity != null) {
-                String uiLetterSubject = subjectTextField.getText();
-                String entityLetterSubject = letterEntity.getStringFieldValue(subjectField);
-                if (!Objects.areEquals(uiLetterSubject, entityLetterSubject))
-                    letterEntity.setFieldValue(subjectField, uiLetterSubject);
-                String uiLetterBody = editor.getText();
-                String entityLetterBody = letterEntity.getStringFieldValue(bodyField);
-                if (uiLetterBody != null && !Objects.areEquals(uiLetterBody, entityLetterBody))
-                    letterEntity.setFieldValue(bodyField, uiLetterBody);
-                updateButtonsDisable();
-            }
-        }
-
-        void syncUiFromEntity() {
-            if (letterEntity != null) {
-                String uiLetterSubject = subjectTextField.getText();
-                String entityLetterSubject = letterEntity.getStringFieldValue(subjectField);
-                if (!Objects.areEquals(uiLetterSubject, entityLetterSubject))
-                    subjectTextField.setText(entityLetterSubject);
-                String uiLetterBody = editor.getText();
-                String entityLetterBody = letterEntity.getStringFieldValue(bodyField);
-                if (!Objects.areEquals(uiLetterBody, entityLetterBody))
-                    editor.setText(entityLetterBody);
-                updateButtonsDisable();
-            }
-        }
-
-        void setLetterEntity(Entity letterEntity) {
-            if (this.letterEntity != letterEntity) {
-                this.letterEntity = letterEntity;
-                letterStore = (UpdateStore) letterEntity.getStore();
-                syncUiFromEntity();
-            }
-        }
-
-        void revert() {
-            letterStore.cancelChanges();
-            syncUiFromEntity();
-        }
-
-        void save() {
-            letterStore.executeUpdate().setHandler(ar -> {
-                if (ar.succeeded()) {
-                    updateButtonsDisable();
-                }
-            });
-        }
-
-        void updateButtonsDisable() {
-            boolean disable = !letterStore.hasChanges();
-            saveButton.setDisable(disable);
-            revertButton.setDisable(disable);
-        }
-
-        void displayEditor() {
-            if (borderPane != null && borderPane.getCenter() != editor) {
-                syncUiFromEntity();
-                borderPane.setTop(subjectTextField);
-                borderPane.setCenter(editor);
-                BorderPane buttonsBar = new BorderPane();
-                HBox hBox = new HBox();
-                for (Object language : languages)
-                    hBox.getChildren().add(languageButtons.get(language));
-                buttonsBar.setLeft(hBox);
-                buttonsBar.setCenter(new HBox(LayoutUtil.createHGrowable(), saveButton, revertButton, LayoutUtil.createHGrowable()));
-                borderPane.setBottom(buttonsBar);
-                // The following code is just a temporary workaround to make CKEditor work in html platform (to be removed once fixed)
-                if (letterEntity != null) {
-                    editor.resize(1, 1);
-                    editor.requestLayout();
-                }
-            }
-        }
+        if (multiLanguageEditor != null)
+            multiLanguageEditor.onEntityChanged();
     }
 }
---
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/backend/util/MultiLanguageEditor.java
@@ -0,0 +1,231 @@
+package mongoose.activities.backend.util;
+
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import naga.commons.util.Objects;
+import naga.commons.util.function.Callable;
+import naga.commons.util.function.Function;
+import naga.commons.util.tuples.Pair;
+import naga.framework.expression.sqlcompiler.sql.SqlCompiled;
+import naga.framework.orm.domainmodel.DataSourceModel;
+import naga.framework.orm.entity.Entity;
+import naga.framework.orm.entity.EntityList;
+import naga.framework.orm.entity.UpdateStore;
+import naga.framework.orm.mapping.QueryResultSetToEntityListGenerator;
+import naga.framework.ui.action.ActionRegistry;
+import naga.framework.ui.controls.LayoutUtil;
+import naga.framework.ui.i18n.I18n;
+import naga.fx.properties.Properties;
+import naga.fxdata.control.HtmlTextEditor;
+import naga.platform.services.query.QueryArgument;
+import naga.platform.spi.Platform;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static mongoose.actions.MongooseIcons.getLanguageIcon32;
+import static naga.framework.ui.controls.LayoutUtil.setPrefSizeToInfinite;
+
+/**
+ * @author Bruno Salmon
+ */
+public class MultiLanguageEditor {
+
+    private final static String[] languages = {"en", "de", "es", "fr", "pt"};
+    private final static String entityListId = "entity";
+
+    private final I18n i18n;
+    private final Callable entityIdGetter;
+    private final DataSourceModel dataSourceModel;
+    private final String loadingSelect;
+    private final Function<Object, Object> bodyFieldGetter;
+    private final Function<Object, Object> subjectFieldGetter;
+
+    private final Map<Object, ToggleButton> languageButtons = new HashMap<>();
+    private final ToggleGroup toggleGroup = new ToggleGroup();
+    private final BorderPane borderPane = setPrefSizeToInfinite(new BorderPane());
+
+    private final Map<Object /*entityId*/, UpdateStore> entityStores = new HashMap<>();
+    private final Map<Pair<Object /*entityId**/, Object /*language*/>, MonoLanguageEditor> monoLanguageEditors = new HashMap<>();
+
+    public MultiLanguageEditor(I18n i18n, Entity entity, Function<Object, Object> bodyFieldGetter, Function<Object, Object> subjectFieldGetter) {
+        this(i18n, entity::getId, entity.getStore().getDataSourceModel(), bodyFieldGetter, subjectFieldGetter, null);
+        registerEntityInStore(entity, null);
+        onEntityChanged();
+    }
+
+    public MultiLanguageEditor(I18n i18n, Callable entityIdGetter, DataSourceModel dataSourceModel, Function<Object, Object> bodyFieldGetter, Function<Object, Object> subjectFieldGetter, String domainClassIdOrLoadingSelect) {
+        this.i18n = i18n;
+        this.entityIdGetter = entityIdGetter;
+        this.dataSourceModel = dataSourceModel;
+        this.bodyFieldGetter = bodyFieldGetter;
+        this.subjectFieldGetter = subjectFieldGetter;
+        StringBuilder sb = domainClassIdOrLoadingSelect == null || domainClassIdOrLoadingSelect.startsWith("select ") ? null : new StringBuilder("select ");
+        for (String language : languages) {
+            ToggleButton languageButton = new ToggleButton(null, getLanguageIcon32(language));
+            languageButton.setUserData(language);
+            languageButton.setMinWidth(50d);
+            languageButtons.put(language, languageButton);
+            if (sb != null) {
+                if (!Objects.areEquals(language, languages[0]))
+                    sb.append(',');
+                sb.append(bodyFieldGetter.apply(language));
+                if (subjectFieldGetter != null)
+                    sb.append(',').append(subjectFieldGetter.apply(language));
+            }
+        }
+        this.loadingSelect = sb == null ? domainClassIdOrLoadingSelect : sb.append(" from ").append(domainClassIdOrLoadingSelect).append(" where id=?").toString();
+        toggleGroup.getToggles().setAll(languageButtons.values());
+        Properties.runOnPropertiesChange(p -> onEntityChanged(), toggleGroup.selectedToggleProperty());
+        toggleGroup.selectToggle(languageButtons.get(i18n.getLanguage()));
+    }
+
+    public BorderPane getUiNode() {
+        return borderPane;
+    }
+
+    public void onEntityChanged() {
+        MonoLanguageEditor monoLanguageEditor = getCurrentMonoLanguageEditor();
+        if (monoLanguageEditor == null)
+            return;
+        monoLanguageEditor.displayEditor();
+        Object entityId = entityIdGetter.call();
+        if (entityStores.containsKey(entityId))
+            monoLanguageEditor.setEntity(entityStores.get(entityId).getEntityList(entityListId).get(0));
+        else if (loadingSelect != null) {
+            SqlCompiled sqlCompiled = dataSourceModel.getDomainModel().compileSelect(loadingSelect);
+            // Then we ask the query service to execute the sql query
+            Platform.getQueryService().executeQuery(new QueryArgument(sqlCompiled.getSql(), new Object[]{entityId}, dataSourceModel.getId())).setHandler(ar -> {
+                if (ar.succeeded()) {
+                    UpdateStore store = UpdateStore.create(dataSourceModel);
+                    Entity entity = QueryResultSetToEntityListGenerator.createEntityList(ar.result(), sqlCompiled.getQueryMapping(), store, entityListId).get(0);
+                    registerEntityInStore(entity, store);
+                    monoLanguageEditor.setEntity(entity);
+                }
+            });
+        }
+    }
+
+    private void registerEntityInStore(Entity entity, UpdateStore store) {
+        if (store == null)
+            store = UpdateStore.create(dataSourceModel);
+        if (entity.getStore() != store)
+            entity = store.copyEntity(entity);
+        store.markChangesAsCommitted();
+        EntityList entityList = store.getOrCreateEntityList(entityListId);
+        entityList.clear();
+        entityList.add(entity);
+        entityStores.put(entityIdGetter.call(), store);
+    }
+
+    private MonoLanguageEditor getCurrentMonoLanguageEditor() {
+        Object entityId = entityIdGetter.call();
+        if (entityId == null)
+            return null;
+        Toggle selectedLanguageButton = toggleGroup.getSelectedToggle();
+        Object language = selectedLanguageButton != null ? selectedLanguageButton.getUserData() : i18n.getLanguage();
+        Pair<Object, Object> pair = new Pair<>(entityId, language);
+        MonoLanguageEditor monoLanguageEditor = monoLanguageEditors.get(pair);
+        if (monoLanguageEditor == null)
+            monoLanguageEditors.put(pair, monoLanguageEditor = new MonoLanguageEditor(pair.get2()));
+        return monoLanguageEditor;
+    }
+
+    private class MonoLanguageEditor {
+        private final TextField subjectTextField = new TextField();
+        private final HtmlTextEditor editor = new HtmlTextEditor();
+        private final Button saveButton = ActionRegistry.newSaveAction(this::save).toButton(i18n);
+        private final Button revertButton = ActionRegistry.newRevertAction(this::revert).toButton(i18n);
+        private final Object subjectField;
+        private final Object bodyField;
+        private UpdateStore entityStore;
+        private Entity entity;
+
+        MonoLanguageEditor(Object lang) {
+            subjectField = subjectFieldGetter == null ? null : subjectFieldGetter.apply(lang);
+            bodyField = bodyFieldGetter.apply(lang);
+            Properties.runOnPropertiesChange(p -> syncEntityFromUi(), subjectTextField.textProperty(), editor.textProperty());
+        }
+
+        void syncEntityFromUi() {
+            if (entity != null) {
+                if (subjectField != null) {
+                    String uiSubject = subjectTextField.getText();
+                    String entitySubject = entity.getStringFieldValue(subjectField);
+                    if (!Objects.areEquals(uiSubject, entitySubject))
+                        entity.setFieldValue(subjectField, uiSubject);
+                }
+                String uiBody = editor.getText();
+                String entityBody = entity.getStringFieldValue(bodyField);
+                if (uiBody != null && !Objects.areEquals(uiBody, entityBody))
+                    entity.setFieldValue(bodyField, uiBody);
+                updateButtonsDisable();
+            }
+        }
+
+        void syncUiFromEntity() {
+            if (entity != null) {
+                if (subjectField != null) {
+                    String uiSubject = subjectTextField.getText();
+                    String entitySubject = entity.getStringFieldValue(subjectField);
+                    if (!Objects.areEquals(uiSubject, entitySubject))
+                        subjectTextField.setText(entitySubject);
+                }
+                String uiBody = editor.getText();
+                String entityBody = entity.getStringFieldValue(bodyField);
+                if (!Objects.areEquals(uiBody, entityBody))
+                    editor.setText(entityBody);
+                updateButtonsDisable();
+            }
+        }
+
+        void setEntity(Entity entity) {
+            if (this.entity != entity) {
+                this.entity = entity;
+                entityStore = (UpdateStore) entity.getStore();
+                syncUiFromEntity();
+            }
+        }
+
+        void revert() {
+            entityStore.cancelChanges();
+            syncUiFromEntity();
+        }
+
+        void save() {
+            entityStore.executeUpdate().setHandler(ar -> {
+                if (ar.succeeded()) {
+                    updateButtonsDisable();
+                }
+            });
+        }
+
+        void updateButtonsDisable() {
+            boolean disable = !entityStore.hasChanges();
+            saveButton.setDisable(disable);
+            revertButton.setDisable(disable);
+        }
+
+        void displayEditor() {
+            if (borderPane != null && borderPane.getCenter() != editor) {
+                syncUiFromEntity();
+                if (subjectField != null)
+                    borderPane.setTop(subjectTextField);
+                borderPane.setCenter(editor);
+                BorderPane buttonsBar = new BorderPane();
+                HBox hBox = new HBox();
+                for (Object language : languages)
+                    hBox.getChildren().add(languageButtons.get(language));
+                buttonsBar.setLeft(hBox);
+                buttonsBar.setCenter(new HBox(LayoutUtil.createHGrowable(), saveButton, revertButton, LayoutUtil.createHGrowable()));
+                borderPane.setBottom(buttonsBar);
+                // The following code is just a temporary workaround to make CKEditor work in html platform (to be removed once fixed)
+                if (entity != null) {
+                    editor.resize(1, 1);
+                    editor.requestLayout();
+                }
+            }
+        }
+    }
+}
