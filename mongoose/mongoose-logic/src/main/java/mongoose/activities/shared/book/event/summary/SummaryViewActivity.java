c11483591672f87e120a589af65db54269d38406
Bruno Salmon <dev.salmonb@gmail.com>
2017-05-03 12:34:35 +0200

    Added personal details in summary page

--- mongoose/mongoose-logic/src/main/java/mongoose/activities/shared/book/event/summary/SummaryViewActivity.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/shared/book/event/summary/SummaryViewActivity.java
@@ -1,9 +1,13 @@
 package mongoose.activities.shared.book.event.summary;
 
+import javafx.event.ActionEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import mongoose.activities.shared.book.event.shared.BookingProcessViewActivity;
+import mongoose.activities.shared.book.event.shared.PersonDetailsPanel;
 import mongoose.activities.shared.logic.ui.highlevelcomponents.HighLevelComponents;
+import mongoose.activities.shared.logic.work.WorkingDocument;
+import naga.framework.ui.controls.LayoutUtil;
 import naga.framework.ui.i18n.I18n;
 import naga.platform.json.Json;
 import naga.platform.spi.Platform;
@@ -15,6 +19,8 @@ import java.time.Instant;
  */
 public class SummaryViewActivity extends BookingProcessViewActivity {
 
+    private PersonDetailsPanel personDetailsPanel;
+
     public SummaryViewActivity() {
         super(null);
     }
@@ -23,28 +29,50 @@ public class SummaryViewActivity extends BookingProcessViewActivity {
     protected void createViewNodes() {
         super.createViewNodes();
         I18n i18n = getI18n();
-        BorderPane summaryPanel = HighLevelComponents.createSectionPanel(null, null, "Summary", i18n);
-        VBox panelsVBox = new VBox(summaryPanel);
+        personDetailsPanel = new PersonDetailsPanel(getEvent(), this, borderPane);
+        personDetailsPanel.setEditable(false);
+        BorderPane optionsPanel = HighLevelComponents.createSectionPanel(null, null, "Options", i18n);
+        BorderPane personalPanel = personDetailsPanel.getSectionPanel();
+        BorderPane requestPanel = HighLevelComponents.createSectionPanel(null, null, "SpecialRequest", i18n);
+        BorderPane termsPanel = HighLevelComponents.createSectionPanel(null, null, "TermsAndConditions", i18n);
+        VBox panelsVBox = new VBox(20, optionsPanel, personalPanel, requestPanel, termsPanel);
 
-        borderPane.setCenter(panelsVBox);
+        borderPane.setCenter(LayoutUtil.createVerticalScrollPane(panelsVBox));
+    }
 
-        nextButton.setOnAction(e -> getWorkingDocument().submit().setHandler(ar -> {
+    private void syncUiFromModel() {
+        WorkingDocument workingDocument = getWorkingDocument();
+        if (workingDocument != null)
+            personDetailsPanel.syncUiFromModel(workingDocument.getDocument());
+    }
+
+    private void syncModelFromUi() {
+        WorkingDocument workingDocument = getWorkingDocument();
+        if (workingDocument != null)
+            personDetailsPanel.syncModelFromUi(workingDocument.getDocument());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        syncUiFromModel();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        syncModelFromUi();
+    }
+
+    @Override
+    protected void onNextButtonPressed(ActionEvent event) {
+        getWorkingDocument().submit().setHandler(ar -> {
             if (ar.failed())
                 Platform.log("Error submitting booking", ar.cause());
             else {
                 //Platform.log("Document id = " + getWorkingDocument().getDocument().getId());
                 getHistory().push("/event/" + getEvent().getPrimaryKey() + "/bookings", Json.createObject().set("refresh", Instant.now()));
             }
-        }));
+        });
     }
-
-    @Override
-    public void onResume() {
-        startLogic();
-        super.onResume();
-    }
-
-    private void startLogic() {
-    }
-
 }
