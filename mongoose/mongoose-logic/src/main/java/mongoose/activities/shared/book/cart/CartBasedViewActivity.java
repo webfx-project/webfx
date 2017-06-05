ecc7c50def7f9cee56cc8185f77cbddd49139272
Bruno Salmon <dev.salmonb@gmail.com>
2017-06-05 09:53:46 +0200

    Extracted CartBasedViewActivity super class from CartViewActivity

---
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/shared/book/cart/CartBasedViewActivity.java
@@ -0,0 +1,62 @@
+package mongoose.activities.shared.book.cart;
+
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleObjectProperty;
+import mongoose.services.CartService;
+import naga.framework.activity.view.impl.ViewActivityImpl;
+import naga.fx.properties.Properties;
+
+/**
+ * @author Bruno Salmon
+ */
+public abstract class CartBasedViewActivity extends ViewActivityImpl {
+
+    private final Property<Object> cartUuidProperty = new SimpleObjectProperty<>();
+
+    protected Object getCartUuid() {
+        return cartUuidProperty.getValue();
+    }
+
+    @Override
+    protected void fetchRouteParameters() {
+        cartUuidProperty.setValue(getParameter("cartUuid"));
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        startLogic();
+    }
+
+    protected void startLogic() {
+        Properties.runOnPropertiesChange(p -> onCartUuidChange(), cartUuidProperty);
+        Properties.runOnPropertiesChange(p -> onDictionaryChange(), getI18n().dictionaryProperty());
+    }
+
+    protected void onCartUuidChange() {
+        loadCart();
+    }
+
+    protected void onDictionaryChange() {
+        onCartLoaded();
+    }
+
+    private void loadCart() {
+        cartService().onCartDocuments().setHandler(ar -> {
+            if (ar.succeeded())
+                onCartLoaded();
+        });
+    }
+
+    protected abstract void onCartLoaded();
+
+    protected CartService cartService() {
+        return CartService.getOrCreate(getCartUuid(), getDataSourceModel());
+    }
+
+    @Override
+    protected void refreshDataOnActive() {
+        cartService().unload();
+        loadCart();
+    }
+}
--- mongoose/mongoose-logic/src/main/java/mongoose/activities/shared/book/cart/CartViewActivity.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/shared/book/cart/CartViewActivity.java
@@ -8,28 +8,31 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import mongoose.activities.shared.book.event.shared.BookingOptionsPanel;
+import mongoose.activities.shared.book.event.shared.TranslateFunction;
 import mongoose.activities.shared.logic.ui.highlevelcomponents.HighLevelComponents;
 import mongoose.activities.shared.logic.work.WorkingDocument;
+import mongoose.domainmodel.format.PriceFormatter;
 import mongoose.entities.Document;
+import mongoose.entities.Event;
 import mongoose.services.CartService;
 import mongoose.services.EventService;
 import naga.commons.util.collection.Collections;
-import naga.framework.activity.view.impl.ViewActivityImpl;
 import naga.framework.orm.entity.Entity;
 import naga.framework.ui.controls.LayoutUtil;
 import naga.framework.ui.i18n.I18n;
 import naga.framework.ui.mapping.EntityListToDisplayResultSetGenerator;
-import naga.fx.properties.Properties;
 import naga.fxdata.control.DataGrid;
 import naga.fxdata.displaydata.DisplayResultSet;
 import naga.fxdata.displaydata.DisplaySelection;
 
 import java.util.List;
 
+import static naga.framework.ui.format.FormatterRegistry.registerFormatter;
+
 /**
  * @author Bruno Salmon
  */
-public class CartViewActivity extends ViewActivityImpl {
+public class CartViewActivity extends CartBasedViewActivity {
 
     private final Property<DisplayResultSet> documentDisplayResultSetProperty = new SimpleObjectProperty<>();
     private final Property<DisplayResultSet> paymentDisplayResultSetProperty = new SimpleObjectProperty<>();
@@ -37,11 +40,6 @@ public class CartViewActivity extends ViewActivityImpl {
     private final Property<DisplaySelection> documentDisplaySelectionProperty = new SimpleObjectProperty<>();
 
     private BookingOptionsPanel bookingOptionsPanel;
-
-    private final Property<Object> cartUuidProperty = new SimpleObjectProperty<>();
-
-    private int selectedRow;
-    private Document selectedDocument;
     private WorkingDocument selectedWorkingDocument;
 
     @Override
@@ -49,34 +47,29 @@ public class CartViewActivity extends ViewActivityImpl {
         I18n i18n = getI18n();
         BorderPane bookingsPanel = HighLevelComponents.createSectionPanel(null, null, "YourBookings", i18n);
         DataGrid documentTable = LayoutUtil.setPrefMaxHeightToMin(new DataGrid());
-        bookingsPanel.setCenter(new VBox(documentTable));
+        bookingsPanel.setCenter(documentTable);
         BorderPane optionsPanel = HighLevelComponents.createSectionPanel(null, null, "YourOptions", i18n);
         bookingOptionsPanel = new BookingOptionsPanel(i18n);
         optionsPanel.setCenter(bookingOptionsPanel.getGrid());
         BorderPane paymentsPanel = HighLevelComponents.createSectionPanel(null, null, "YourPayments", i18n);
         DataGrid paymentTable = LayoutUtil.setPrefMaxHeightToMin(new DataGrid());
-        paymentsPanel.setCenter(new VBox(paymentTable));
+        paymentsPanel.setCenter(paymentTable);
 
         Button addBookingButton = i18n.translateText(new Button(), "AddAnotherBooking");
         Button modifyBookingButton = i18n.translateText(new Button(), "Modify");
         Button paymentButton = i18n.translateText(new Button(), "MakePayment");
         HBox buttonBar = new HBox(20, addBookingButton, LayoutUtil.createHGrowable(), modifyBookingButton, LayoutUtil.createHGrowable(), paymentButton);
 
-        addBookingButton.setOnAction(e -> {
-            if (selectedDocument != null) {
-                EventService.getOrCreateFromDocument(selectedDocument).setCurrentCart(selectedDocument.getCart());
-                getHistory().push("/book/event/" + selectedDocument.getEventId().getPrimaryKey() + "/fees");
-            }
-        });
+        addBookingButton.setOnAction(e -> getHistory().push("/book/event/" + getEventId() + "/fees"));
 
         modifyBookingButton.setOnAction(e -> {
-            EventService eventService = EventService.getOrCreateFromDocument(selectedDocument);
+            EventService eventService = cartService().getEventService();
             eventService.setSelectedOptionsPreselection(null);
             eventService.setWorkingDocument(selectedWorkingDocument);
-            getHistory().push("/book/event/" + selectedDocument.getEventId().getPrimaryKey() + "/options");
+            getHistory().push("/book/event/" + getEventId() + "/options");
         });
 
-        paymentButton.setOnAction(e -> getHistory().push("/book/cart/" + cartUuidProperty.getValue() + "/payment"));
+        paymentButton.setOnAction(e -> getHistory().push("/book/cart/" + getCartUuid() + "/payment"));
 
         // Binding the UI with the presentation model for further state changes
         // User inputs: the UI state changes are transferred in the presentation model
@@ -90,79 +83,57 @@ public class CartViewActivity extends ViewActivityImpl {
         return LayoutUtil.createVerticalScrollPane(new VBox(20, bookingsPanel, optionsPanel, paymentsPanel, buttonBar));
     }
 
-    @Override
-    protected void fetchRouteParameters() {
-        cartUuidProperty.setValue(getParameter("cartUuid"));
+    private Event getEvent() {
+        return cartService().getEventService().getEvent();
+    }
+
+    private Object getEventId() {
+        return getEvent().getPrimaryKey();
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        startLogic();
+    protected void startLogic() {
+        super.startLogic();
+        documentDisplaySelectionProperty.addListener((observable, oldValue, selection) -> {
+            selectedWorkingDocument = Collections.get(cartService().getCartWorkingDocuments(), selection.getSelectedRow());
+            syncBookingOptionsPanelIfReady();
+        });
     }
 
     @Override
-    protected void refreshDataOnActive() {
-        cartService().unload();
-        loadAndDisplayCart(false);
-    }
-
-    private CartService cartService() {
-        return CartService.getOrCreate(cartUuidProperty.getValue(), getDataSourceModel());
+    protected void onCartLoaded() {
+        CartService cartService = cartService();
+        if (cartService.getEventService() != null)
+            registerFormatter("priceWithCurrency", new PriceFormatter(getEvent()));
+        displayEntities(cartService.getCartDocuments(), "[" +
+                        "'ref'," +
+                        "'person_firstName'," +
+                        "'person_lastName'," +
+                        "{expression: 'price_net', format: 'priceWithCurrency'}," +
+                        "{expression: 'price_deposit', format: 'priceWithCurrency'}," +
+                        "{expression: 'price_balance', format: 'priceWithCurrency'}" +
+                        "]"
+                , "Document", documentDisplayResultSetProperty);
+        new TranslateFunction(getI18n()).register();
+        displayEntities(cartService.getCartPayments(), "[" +
+                        "{expression: 'date', format: 'dateTime'}," +
+                        "{expression: 'document.ref', label: 'Booking ref'}," +
+                        "{expression: 'translate(method)', label: 'Method'}," +
+                        "{expression: 'amount', format: 'priceWithCurrency'}," +
+                        "{expression: 'pending ? `Pending` : successful ? `Success` : `Failed`', label: 'Status'}" +
+                        "]"
+                , "MoneyTransfer", paymentDisplayResultSetProperty);
+        javafx.application.Platform.runLater(() -> {
+            EventService eventService = cartService.getEventService();
+            Document eventDocument = eventService == null || eventService.getWorkingDocument() == null ? null : eventService.getWorkingDocument().getDocument();
+            int selectedIndex = Math.max(0, Collections.indexOf(cartService.getCartWorkingDocuments(), wd -> Entity.sameId(wd.getDocument(), eventDocument)));
+            documentDisplaySelectionProperty.setValue(DisplaySelection.createSingleRowSelection(selectedIndex));
+        });
     }
 
     private void displayEntities(List<? extends Entity> entities, String columnsDefinition, Object classId, Property<DisplayResultSet> displayResultSetProperty) {
         displayResultSetProperty.setValue(EntityListToDisplayResultSetGenerator.createDisplayResultSet(entities, columnsDefinition
                 , getDataSourceModel().getDomainModel(), classId, getI18n()));
-    }
-
-    public void startLogic() {
-        Properties.runNowAndOnPropertiesChange(p -> loadAndDisplayCart(true), cartUuidProperty);
-        Properties.runNowAndOnPropertiesChange(p -> displayCart(), getI18n().dictionaryProperty());
-        documentDisplaySelectionProperty.addListener((observable, oldValue, selection) -> {
-            int selectedRow = selection.getSelectedRow();
-            CartService cartService = cartService();
-            List<Document> cartDocuments = cartService.getCartDocuments();
-            if (Collections.indexInRange(selectedRow, cartDocuments)) {
-                selectedDocument = cartDocuments.get(this.selectedRow = selectedRow);
-                selectedWorkingDocument = cartService.getCartWorkingDocuments().get(selectedRow);
-                syncBookingOptionsPanelIfReady();
-            }
-        });
-    }
-
-    private void loadAndDisplayCart(boolean resetSelection) {
-        if (resetSelection)
-            selectedRow = 0;
-        cartService().onCartDocuments().setHandler(ar -> {
-            if (ar.succeeded())
-                displayCart();
-        });
-    }
-
-    private void displayCart() {
-        List<Document> cartDocuments = cartService().getCartDocuments();
-        displayEntities(cartDocuments, "[" +
-                        "'ref'," +
-                        "'person_firstName'," +
-                        "'person_lastName'," +
-                        "{expression: 'price_net', format: 'price'}," +
-                        "{expression: 'price_deposit', format: 'price'}," +
-                        "{expression: 'price_balance', format: 'price'}" +
-                        "]"
-                , "Document", documentDisplayResultSetProperty);
-        displayEntities(cartService().getCartPayments(), "[" +
-                        "{expression: 'date', format: 'dateTime'}," +
-                        "{expression: 'document.ref', label: 'Booking ref'}," +
-                        "{expression: 'method.name', label: 'Method'}," +
-                        "{expression: 'amount', format: 'price'}," +
-                        "{expression: 'pending ? `Pending` : successful ? `Success` : `Failed`', label: 'Status'}" +
-                        "]"
-                , "MoneyTransfer", paymentDisplayResultSetProperty);
-        javafx.application.Platform.runLater(() -> {
-            if (Collections.indexInRange(selectedRow, cartDocuments))
-                documentDisplaySelectionProperty.setValue(DisplaySelection.createSingleRowSelection(selectedRow));
-        });
     }
 
     private void syncBookingOptionsPanelIfReady() {
