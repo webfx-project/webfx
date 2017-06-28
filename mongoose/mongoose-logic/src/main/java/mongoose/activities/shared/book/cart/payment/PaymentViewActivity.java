02b4075026c64624d8d30d00e250da45de773400
Bruno Salmon <dev.salmonb@gmail.com>
2017-06-28 16:58:15 +0200

    Implemented generic payment gateway call (http post with no signature - should work for WorldPay)

--- mongoose/mongoose-logic/src/main/java/mongoose/activities/shared/book/cart/payment/PaymentViewActivity.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/shared/book/cart/payment/PaymentViewActivity.java
@@ -3,19 +3,37 @@ package mongoose.activities.shared.book.cart.payment;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import mongoose.activities.shared.book.cart.CartBasedViewActivity;
 import mongoose.activities.shared.logic.ui.highlevelcomponents.HighLevelComponents;
 import mongoose.domainmodel.format.PriceFormatter;
-import mongoose.entities.Document;
-import mongoose.entities.MoneyTransfer;
+import mongoose.entities.*;
+import mongoose.util.Labels;
+import naga.commons.util.Dates;
+import naga.commons.util.Strings;
+import naga.commons.util.async.Batch;
 import naga.commons.util.collection.Collections;
+import naga.framework.expression.sqlcompiler.sql.SqlCompiled;
+import naga.framework.orm.domainmodel.DataSourceModel;
+import naga.framework.orm.domainmodel.DomainModel;
+import naga.framework.orm.entity.EntityList;
+import naga.framework.orm.entity.EntityStore;
 import naga.framework.orm.entity.UpdateStore;
+import naga.framework.orm.mapping.QueryResultSetToEntityListGenerator;
+import naga.framework.ui.controls.DialogUtil;
 import naga.framework.ui.controls.LayoutUtil;
+import naga.fx.spi.Toolkit;
+import naga.fxdata.control.HtmlText;
+import naga.platform.client.bus.WebSocketBus;
+import naga.platform.client.url.location.WindowLocation;
+import naga.platform.services.query.QueryArgument;
 import naga.platform.spi.Platform;
+import naga.platform.spi.client.ClientPlatform;
 
 import java.util.List;
 
@@ -59,8 +77,10 @@ public class PaymentViewActivity extends CartBasedViewActivity {
         if (cartDocuments != null && paymentsVBox != null) {
             notPaidEnoughCount = notPaidFullCount = 0;
             documentPayments = Collections.mapFilter(cartDocuments, DocumentPayment::new, DocumentPayment::hasBalance);
-            paymentsVBox.getChildren().setAll(Collections.map(documentPayments, DocumentPayment::getNode));
-            updateTotal();
+            Toolkit.get().scheduler().runInUiThread(() -> {
+                paymentsVBox.getChildren().setAll(Collections.map(documentPayments, DocumentPayment::getNode));
+                updateTotal();
+            });
         }
     }
 
@@ -74,7 +94,7 @@ public class PaymentViewActivity extends CartBasedViewActivity {
     }
 
     private String formatCurrency(int amount) {
-        return PriceFormatter.formatWithCurrency(amount, cartService().getEventService().getEvent());
+        return PriceFormatter.formatWithCurrency(amount, getEvent());
     }
 
     private static int CENTS = 100;
@@ -185,25 +205,36 @@ public class PaymentViewActivity extends CartBasedViewActivity {
         }
     }
 
+    private MoneyTransfer moneyTransfer;
+    private Document doc;
+    private String paymentRef;
+
     private void submitPayment() {
         List<DocumentPayment> payments = Collections.filter(documentPayments, p -> p.getAmount() > 0);
         if (payments.isEmpty())
             return;
-        UpdateStore updateStore = UpdateStore.createAbove(cartService().getEventService().getEventStore());
-        MoneyTransfer moneyTransfer = updateStore.insertEntity(MoneyTransfer.class);
+        EntityStore loadStore = cartService().getEventService().getEventStore();
+        UpdateStore updateStore = UpdateStore.createAbove(loadStore);
+        moneyTransfer = updateStore.insertEntity(MoneyTransfer.class);
         moneyTransfer.setPending(true);
-        moneyTransfer.setMethod(5); // Online
+        moneyTransfer.setMethod(Method.ONLINE_METHOD_ID);
+        paymentRef = null;
         if (payments.size() == 1) {
             DocumentPayment payment = Collections.first(payments);
-            moneyTransfer.setDocument(payment.document);
+            moneyTransfer.setDocument(doc = payment.document);
             moneyTransfer.setAmount(payment.getAmount());
+            paymentRef = payment.document.getRef().toString();
         } else {
             moneyTransfer.setSpread(true);
+            doc = null;
             for (DocumentPayment payment : payments) {
                 MoneyTransfer childTransfer = updateStore.insertEntity(MoneyTransfer.class);
                 childTransfer.setDocument(payment.document);
                 childTransfer.setAmount(payment.getAmount());
                 childTransfer.setParent(moneyTransfer);
+                if (doc == null)
+                    doc = payment.document;
+                paymentRef = (paymentRef == null ? "" : paymentRef + "-") + payment.document.getRef().toString();;
             }
         }
         updateStore.executeUpdate().setHandler(ar -> {
@@ -211,8 +242,92 @@ public class PaymentViewActivity extends CartBasedViewActivity {
                 Platform.log("Error submitting payment", ar.cause());
             else {
                 cartService().unload();
-                getHistory().goBack();
+                DataSourceModel dataSourceModel = loadStore.getDataSourceModel();
+                Object dataSourceId = dataSourceModel.getId();
+                Object[] paymentIdParameter = {ar.result().getArray()[0].getGeneratedKeys()[0]};
+                DomainModel domainModel = dataSourceModel.getDomainModel();
+                SqlCompiled sqlCompiled1 = domainModel.compileSelect("select <frontend_loadEvent> from GatewayParameter gp where exists(select MoneyTransfer mt where mt=? and (gp.account=mt.toMoneyAccount or gp.account=null and gp.company=mt.toMoneyAccount.gatewayCompany)) order by company");
+                SqlCompiled sqlCompiled2 = domainModel.compileSelect("select <frontend_cart> from MoneyTransfer where id=?");
+                Platform.getQueryService().executeQueryBatch(
+                        new Batch<>(new QueryArgument[]{
+                                new QueryArgument(sqlCompiled1.getSql(), paymentIdParameter, dataSourceId),
+                                new QueryArgument(sqlCompiled2.getSql(), paymentIdParameter, dataSourceId)
+                        })
+                ).setHandler(ar2 -> {
+                    if (ar.failed())
+                        Platform.log("Error submitting payment", ar.cause());
+                    else {
+                        EntityList<GatewayParameter> gatewayParameters = QueryResultSetToEntityListGenerator.createEntityList(ar2.result().getArray()[0], sqlCompiled1.getQueryMapping(), loadStore, "gatewayParameters");
+                        Toolkit.get().scheduler().runInUiThread(() -> {
+                            String innerHtml = generateHtmlForm(gatewayParameters);
+                            Platform.log(innerHtml);
+                            HtmlText htmlText = LayoutUtil.setPrefSizeToInfinite(new HtmlText(innerHtml));
+                            DialogUtil.showModalNodeInGoldLayout(htmlText, (Pane) getNode(), 0.9, 0.8);
+                        });
+                    }
+                });
             }
         });
+    }
+
+    private String paymentUrl;
+
+    private String generateHtmlForm(EntityList<GatewayParameter> gatewayParameters) {
+        StringBuilder sb = new StringBuilder("<html><body><form id='gatewayForm' action='[paymentUrl]' method='POST'>");
+        boolean live = getEvent().getBooleanFieldValue("live");
+        boolean test = !live;
+        for (GatewayParameter gp : gatewayParameters) {
+            if (live && gp.isLive() || test && gp.isTest()) {
+                if ("paymentUrl".equals(gp.getName()))
+                    paymentUrl = gp.getValue();
+                else
+                    sb.append("\n<input type='hidden' name='").append(gp.getName()).append("' value='").append(gp.getValue()).append("'/>");
+            }
+        }
+        return replaceBrackets(sb.append("\n</form><script type='text/javascript'>document.getElementById('gatewayForm').submit();</script></body></html>").toString());
+    }
+
+    private String replaceBrackets(String value) {
+        Event event = getEvent();
+        value = Strings.replaceAllSafe(value, "[paymentUrl]", paymentUrl);
+        value = Strings.replaceAllSafe(value, "[ref]", paymentRef);
+        value = Strings.replaceAllSafe(value, "[bref]", doc.getRef().toString());
+        value = Strings.replaceAllSafe(value, "[amount]", PriceFormatter.INSTANCE.format(moneyTransfer.getAmount(), true).toString());
+        value = Strings.replaceAllSafe(value, "[amount_int]", moneyTransfer.getAmount().toString());
+        value = Strings.replaceAllSafe(value, "[event]", Labels.instantTranslate(event, getI18n()));
+        value = Strings.replaceAllSafe(value, "[eventid]", event.getPrimaryKey().toString());
+        value = Strings.replaceAllSafe(value, "[eventid5]", digits(event.getPrimaryKey().toString(), 5, true));
+        value = Strings.replaceAllSafe(value, "[firstName]", doc.getFirstName());
+        value = Strings.replaceAllSafe(value, "[lastName]", doc.getLastName());
+        value = Strings.replaceAllSafe(value, "[name]", doc.isOrdained() ? doc.getLayName() : doc.getFullName());
+        value = Strings.replaceAllSafe(value, "[street]", doc.getStreet());
+        value = Strings.replaceAllSafe(value, "[city]", doc.getCityName());
+        value = Strings.replaceAllSafe(value, "[admin1]", doc.getAdmin1Name());
+        value = Strings.replaceAllSafe(value, "[admin2]", doc.getAdmin2Name());
+        ////"[state_2c]": firstDocument.person_state == null ? null : firstDocument.person_state.substr(0, 2),
+        value = Strings.replaceAllSafe(value, "[postCode]", doc.getPostCode());
+        value = Strings.replaceAllSafe(value, "[country]", doc.getCountry() != null ? doc.getCountry().getName() : doc.getCountryName());
+        value = Strings.replaceAllSafe(value, "[countryCode]", doc.getCountry() != null ? doc.getCountry().getIsoAlpha2() : null);
+        ////"[postcode_int]": keepDigitsOnly(firstDocument.person_postCode),
+        value = Strings.replaceAllSafe(value, "[phone]", doc.getPhone());
+        ////"[phone_int]": keepDigitsOnly(firstDocument.person_phone),
+        value = Strings.replaceAllSafe(value, "[email]", doc.getEmail());
+        WindowLocation currentLocation = ClientPlatform.get().getCurrentLocation();
+        value = Strings.replaceAllSafe(value, "[frontendUrl]", currentLocation.getOrigin());
+        String cartUrl = Strings.removeSuffix(currentLocation.getHref(), "/payment");
+        value = Strings.replaceAllSafe(value, "[cartUrl]", cartUrl);
+        value = Strings.replaceAllSafe(value, "[session]", ((WebSocketBus) Platform.bus()).getSessionId());
+        value = Strings.replaceAllSafe(value, "[lang]", getLanguage().toString());
+        value = Strings.replaceAllSafe(value, "[paymentId]", moneyTransfer.getPrimaryKey().toString());
+        value = Strings.replaceAllSafe(value, "[paymentId6]", digits(moneyTransfer.getPrimaryKey().toString(), 6, false));
+        value = Strings.replaceAllSafe(value, "[date]", Dates.format(moneyTransfer.getDate(), "YYYYMMDDhhmmss"));
+        return value;
+    }
+
+    private static String digits(String s, int n, boolean right) {
+        s = "" + s;
+        while (s.length() < n)
+            s = right ? s + '0' : '0' + s;
+        return s.substring(s.length() - n, s.length());
     }
 }
