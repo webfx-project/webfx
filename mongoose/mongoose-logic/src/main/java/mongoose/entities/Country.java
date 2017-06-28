e8d849014a65e17fc017ba338457ea355ba38c77
Bruno Salmon <dev.salmonb@gmail.com>
2017-06-28 15:06:17 +0200

    Added iso_alpha2 setter and getter to Country

--- mongoose/mongoose-logic/src/main/java/mongoose/entities/Country.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/entities/Country.java
@@ -8,4 +8,13 @@ import naga.framework.orm.entity.Entity;
  * @author Bruno Salmon
  */
 public interface Country extends Entity, EntityHasName, EntityHasLabel {
+
+    default void setIsoAlpha2(String isoAlpha2) {
+        setFieldValue("iso_alpha2", isoAlpha2);
+    }
+
+    default String getIsoAlpha2() {
+        return getStringFieldValue("iso_alpha2");
+    }
+
 }
