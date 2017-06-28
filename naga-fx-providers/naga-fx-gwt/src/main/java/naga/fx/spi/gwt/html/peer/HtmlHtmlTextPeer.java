5bc6f792965c2f3b638444bff236b8da6c0ae6e8
Bruno Salmon <dev.salmonb@gmail.com>
2017-06-28 15:31:51 +0200

    Fixed HtmlHtmlTextPeer: script tags (when present within the html) were not executed

--- naga-fx-providers/naga-fx-gwt/src/main/java/naga/fx/spi/gwt/html/peer/HtmlHtmlTextPeer.java
+++ naga-fx-providers/naga-fx-gwt/src/main/java/naga/fx/spi/gwt/html/peer/HtmlHtmlTextPeer.java
@@ -1,5 +1,9 @@
 package naga.fx.spi.gwt.html.peer;
 
+import elemental2.dom.Element;
+import elemental2.dom.HTMLScriptElement;
+import elemental2.dom.Node;
+import elemental2.dom.NodeList;
 import naga.commons.util.Strings;
 import naga.fx.spi.gwt.util.HtmlUtil;
 import naga.fxdata.control.HtmlText;
@@ -25,6 +29,25 @@ public class HtmlHtmlTextPeer
     @Override
     public void updateText(String text) {
         getElement().innerHTML = Strings.toSafeString(text);
+        if (text.contains("<script"))
+            executeScripts(getElement());
+    }
+
+    private void executeScripts(Node node) {
+        if (node instanceof Element) {
+            Element element = (Element) node;
+            if ("SCRIPT".equals(element.tagName)) {
+                HTMLScriptElement script = HtmlUtil.createElement("script");
+                script.text = element.innerHTML;
+                for (int i = 0; i < element.attributes.length; i++)
+                    script.setAttribute(element.attributes.getAt(i).name, element.attributes.getAt(i).value);
+                element.parentNode.replaceChild(script, element);
+                return;
+            }
+        }
+        NodeList<Node> children = node.childNodes;
+        for (int i = 0; i < children.length; i++)
+            executeScripts(children.item(i));
     }
 
 }
