fd3192b071c81bd03813d7f896be651991b5f44f
Bruno Salmon <dev.salmonb@gmail.com>
2017-08-24 14:49:49 +0200

    Added UsernamePasswordToken class

---
+++ naga-platform/src/main/java/naga/platform/services/auth/UsernamePasswordToken.java
@@ -0,0 +1,22 @@
+package naga.platform.services.auth;
+
+/**
+ * @author Bruno Salmon
+ */
+public class UsernamePasswordToken {
+    private final String username;
+    private final String password;
+
+    public UsernamePasswordToken(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
