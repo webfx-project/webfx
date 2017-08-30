946760c7057163fdea7fa839166df21d7fd90bda
Bruno Salmon <dev.salmonb@gmail.com>
2017-08-30 13:07:29 +0200

    Added some style attributes to the html radio button peer so it is correctly displayed when within a button

--- naga-fx-providers/naga-fx-gwt/src/main/java/naga/fx/spi/gwt/html/peer/HtmlRadioButtonPeer.java
+++ naga-fx-providers/naga-fx-gwt/src/main/java/naga/fx/spi/gwt/html/peer/HtmlRadioButtonPeer.java
@@ -1,5 +1,7 @@
 package naga.fx.spi.gwt.html.peer;
 
+import elemental2.dom.CSSProperties;
+import elemental2.dom.CSSStyleDeclaration;
 import elemental2.dom.HTMLElement;
 import elemental2.dom.HTMLInputElement;
 import emul.javafx.scene.control.RadioButton;
@@ -29,10 +31,13 @@ public class HtmlRadioButtonPeer
             getNode().setSelected(radioButtonElement.checked);
             return null;
         };
-        HtmlUtil.setStyleAttribute(element, "margin-top", "0");
-        HtmlUtil.setStyleAttribute(element, "margin-bottom", "0");
-        HtmlUtil.setStyleAttribute(radioButtonElement, "vertical-align", "middle");
-        HtmlUtil.setStyleAttribute(radioButtonElement, "margin", " 0 5px 0 0");
+        CSSStyleDeclaration style = element.style;
+        style.left = "0px";
+        style.top = "0px";
+        style.margin = CSSProperties.MarginUnionType.of("0");
+        style.padding = CSSProperties.PaddingUnionType.of("0");
+        radioButtonElement.style.verticalAlign = "middle";
+        radioButtonElement.style.margin = CSSProperties.MarginUnionType.of("0 5px 0 0");
     }
 
     @Override
