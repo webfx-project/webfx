970b57f0999aa4d27c8ded11799a0b38e5ed8dfa
Bruno Salmon <dev.salmonb@gmail.com>
2017-09-04 10:17:21 +0200

    Added birthDate field to Person entity

--- mongoose/mongoose-logic/src/main/java/mongoose/entities/Person.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/entities/Person.java
@@ -3,9 +3,22 @@ package mongoose.entities;
 import mongoose.entities.markers.EntityHasPersonDetails;
 import naga.framework.orm.entity.Entity;
 
+import java.time.LocalDate;
+
 /**
  * @author Bruno Salmon
  */
 public interface Person extends Entity, EntityHasPersonDetails {
+
+    default Object getBirthDateField() { return "birthdate";}
+
+    default void setBirthDate(LocalDate birthDate) {
+        setFieldValue(getBirthDateField(), birthDate);
+    }
+
+    default LocalDate getBirthDate() {
+        return getLocalDateFieldValue(getBirthDateField());
+    }
+
 
 }
