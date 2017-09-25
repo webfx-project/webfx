5ff7b0af67de684b6ef832f6edf8a69b121111ff
Bruno Salmon <dev.salmonb@gmail.com>
2017-09-25 16:05:12 +0200

    Added empty XMLElement class in naga-javalib-javacupruntime because the java cup always add "import java_cup.runtime.XMLElement;" in the generated file

---
+++ naga-javalibs/naga-javalib-javacupruntime/src/main/java/java_cup/runtime/XMLElement.java
@@ -0,0 +1,9 @@
+package java_cup.runtime;
+
+/**
+ * @author Bruno Salmon
+ *
+ * Unused class but declared here because the java cup always add "import java_cup.runtime.XMLElement;" in the generated file
+ */
+public class XMLElement {
+}
