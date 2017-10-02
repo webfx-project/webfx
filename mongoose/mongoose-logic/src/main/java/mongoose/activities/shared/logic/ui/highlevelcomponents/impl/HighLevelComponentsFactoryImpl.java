da8b926c6fd765915204284d4202df9c5a565466
Bruno Salmon <dev.salmonb@gmail.com>
2017-10-02 20:28:30 +0200

    Styled book and soldout buttons with green and red gradients

--- mongoose/mongoose-logic/src/main/java/mongoose/activities/shared/logic/ui/highlevelcomponents/impl/HighLevelComponentsFactoryImpl.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/shared/logic/ui/highlevelcomponents/impl/HighLevelComponentsFactoryImpl.java
@@ -1,11 +1,14 @@
 package mongoose.activities.shared.logic.ui.highlevelcomponents.impl;
 
-import mongoose.activities.shared.logic.ui.highlevelcomponents.HighLevelComponentsFactory;
-import mongoose.activities.shared.logic.ui.highlevelcomponents.SectionPanelStyleOptions;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import mongoose.activities.shared.logic.ui.highlevelcomponents.HighLevelComponentsFactory;
+import mongoose.activities.shared.logic.ui.highlevelcomponents.SectionPanelStyleOptions;
+import naga.framework.ui.controls.BackgroundUtil;
+import naga.framework.ui.controls.BorderUtil;
 
 /**
  * @author Bruno Salmon
@@ -24,11 +27,20 @@ public class HighLevelComponentsFactoryImpl implements HighLevelComponentsFactor
 
     @Override
     public Button createBookButton() {
-        return new Button();
+        return createButton("#7fd504", "#2a8236");
     }
 
     @Override
     public Button createSoldoutButton() {
-        return new Button();
+        return createButton("#e92c04", "#853416");
+    }
+
+    private static Button createButton(String topWebColor, String bottomWebColor) {
+        Button button = new Button();
+        button.setBackground(BackgroundUtil.newVerticalLinearGradientBackground(topWebColor, bottomWebColor, 5));
+        button.setBorder(BorderUtil.newBorder(Color.web(bottomWebColor), 5));
+        button.setTextFill(Color.WHITE);
+        button.setStyle("-fx-text-fill: white;");
+        return button;
     }
 }
