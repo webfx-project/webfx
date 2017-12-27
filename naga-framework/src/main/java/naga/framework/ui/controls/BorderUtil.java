b629937d3db479d1c2cea3a61c0da1c37290e506
Bruno Salmon <dev.salmonb@gmail.com>
2017-12-27 23:31:10 +0100

    Added BorderBuilder utility class

--- naga-framework/src/main/java/naga/framework/ui/controls/BorderUtil.java
+++ naga-framework/src/main/java/naga/framework/ui/controls/BorderUtil.java
@@ -9,39 +9,43 @@ import javafx.scene.paint.Paint;
  */
 public class BorderUtil {
 
+    public static Border transparentBorder() {
+        return newBorder(Color.TRANSPARENT);
+    }
+
     public static Border newBorder(Paint fill) {
-        return newBorder(fill, 0);
+        return newBorderBuilder(fill).build();
+    }
+
+    public static BorderBuilder newBorderBuilder(Paint fill) {
+        return newBorderBuilder(newBorderStrokeBuilder(fill));
+    }
+
+    public static BorderBuilder newBorderBuilder(BorderStrokeBuilder borderStrokeBuilder) {
+        return new BorderBuilder().setStrokeBuilder(borderStrokeBuilder);
+    }
+
+    public static BorderStrokeBuilder newBorderStrokeBuilder(Paint fill) {
+        return new BorderStrokeBuilder().setStroke(fill);
     }
 
     public static Border newBorder(Paint fill, double radius) {
-        return newBorder(fill, radius, BorderStroke.THIN);
+        return newBorderBuilder(fill, radius).build();
+    }
+
+    public static BorderBuilder newBorderBuilder(Paint fill, double radius) {
+        return newBorderBuilder(newBorderStrokeBuilder(fill).setRadius(radius));
     }
 
     public static Border newBorder(Paint fill, double radius, double width) {
-        return newBorder(fill, radius, new BorderWidths(width));
+        return newBorderBuilder(fill, radius, width).build();
     }
 
-    public static Border newBorder(Paint fill, double radius, BorderWidths widths) {
-        return new Border(new BorderStroke(fill, BorderStrokeStyle.SOLID, radius <= 0 ? CornerRadii.EMPTY : new CornerRadii(radius), widths));
-    }
-
-    public static Border newWebColorBorder(String webColor) {
-        return newBorder(Color.web(webColor));
+    public static BorderBuilder newBorderBuilder(Paint fill, double radius, double width) {
+        return newBorderBuilder(newBorderStrokeBuilder(fill).setRadius(radius).setWidth(width));
     }
 
     public static Border newWebColorBorder(String webColor, double radius) {
-        return newBorder(Color.web(webColor), radius);
-    }
-
-    public static Border newWebColorBorder(String webColor, double radius, double width) {
-        return newBorder(Color.web(webColor), radius, width);
-    }
-
-    public static Border newWebColorBorder(String webColor, double radius, BorderWidths widths) {
-        return newBorder(Color.web(webColor), radius, widths);
-    }
-
-    public static Border transparentBorder() {
-        return newBorder(Color.TRANSPARENT);
+        return newBorderBuilder(Color.web(webColor), radius).build();
     }
 }
