ac951030c798c5777c7d9bfa99ccb6f2754afc87
Bruno Salmon <dev.salmonb@gmail.com>
2017-12-27 23:35:13 +0100

    Rewrote control and button factory mixin now based on button builders

--- naga-framework/src/main/java/naga/framework/ui/action/ButtonFactoryMixin.java
+++ naga-framework/src/main/java/naga/framework/ui/action/ButtonFactoryMixin.java
@@ -1,7 +1,14 @@
 package naga.framework.ui.action;
 
 import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import naga.framework.ui.controls.BackgroundUtil;
+import naga.framework.ui.controls.BorderUtil;
+import naga.framework.ui.controls.ButtonBuilder;
 import naga.framework.ui.controls.ControlFactoryMixin;
+import naga.framework.ui.layouts.LayoutUtil;
 
 /**
  * @author Bruno Salmon
@@ -9,23 +16,98 @@ import naga.framework.ui.controls.ControlFactoryMixin;
 public interface ButtonFactoryMixin extends ControlFactoryMixin {
 
     default Button newButton(Object i18nKey, Runnable handler) {
-        return newButton(ActionRegistry.newAction(i18nKey, handler));
+        return newButtonBuilder(i18nKey, handler).build();
     }
 
-    default Button newButton(Action action) {
-        return styleButton(action.toButton(getI18n()));
+    default ButtonBuilder newButtonBuilder(Object i18nKey, Runnable handler) {
+        return newButtonBuilder(ActionRegistry.newAction(i18nKey, handler));
+    }
+
+    default Button newLargeGreenButton(Object i18nKey) {
+        return LayoutUtil.setMaxWidthToInfinite(newLargeGreenButtonBuilder(i18nKey).build());
+    }
+
+    default ButtonBuilder newLargeGreenButtonBuilder(Object i18nKey) {
+        return newGreenButtonBuilder(i18nKey).setHeight(50);
+    }
+
+    default Button newGreenButton(Object i18nKey) {
+        return newGreenButtonBuilder(i18nKey).build();
+    }
+
+    default ButtonBuilder newGreenButtonBuilder(Object i18nKey) {
+        return newColorButtonBuilder(i18nKey, "#B7CA79", "#7D9563");
+    }
+
+    default Button newTransparentButton(Object i18nKey) {
+        return newTransparentButtonBuilder(i18nKey).build();
+    }
+
+    default ButtonBuilder newTransparentButtonBuilder(Object i18nKey) {
+        return transparent(newButtonBuilder(i18nKey));
+    }
+
+    default Button newTransparentButton(Action action) {
+        return newTransparentButtonBuilder(action).build();
+    }
+
+    default ButtonBuilder newTransparentButtonBuilder(Action action) {
+        return transparent(newButtonBuilder(action));
+    }
+
+    default ButtonBuilder newColorButtonBuilder(Object i18nKey, String topColor, String bottomColor) {
+        return colorize(newButtonBuilder(i18nKey), topColor, bottomColor);
+    }
+
+    default ButtonBuilder transparent(ButtonBuilder buttonBuilder) {
+        return colorize(buttonBuilder, null, null);
+    }
+
+    default ButtonBuilder colorize(ButtonBuilder buttonBuilder, String topColor, String bottomColor) {
+        double buttonHeight = 50;
+        double borderWidth = 4;
+        Paint textFill = Color.WHITE, pressedTextFill = textFill;
+        Background background, pressedBackground;
+        if (topColor != null && bottomColor != null) {
+            background = BackgroundUtil.newVerticalLinearGradientBackground(topColor, bottomColor, buttonHeight / 2, borderWidth);
+            pressedBackground = BackgroundUtil.newVerticalLinearGradientBackground(bottomColor, topColor, buttonHeight / 2, borderWidth);
+        } else {
+            background = BackgroundUtil.newBackground(Color.TRANSPARENT, buttonHeight / 2, borderWidth - 1);
+            pressedBackground = BackgroundUtil.newBackground(textFill, buttonHeight / 2, borderWidth - 1);
+            pressedTextFill = Color.BLACK;
+        }
+        return buttonBuilder
+                .setBorder(BorderUtil.newBorder(textFill, buttonHeight / 2, borderWidth))
+                .setBackground(background)
+                .setPressedBackground(pressedBackground)
+                .setTextFill(textFill)
+                .setPressedTextFill(pressedTextFill)
+                //.setHeight(buttonHeight)
+                ;
     }
 
     default Button newOkButton(Runnable handler) {
-        return newButton(ActionRegistry.newOkAction(handler));
+        return newOkButtonBuilder(handler).build();
+    }
+
+    default ButtonBuilder newOkButtonBuilder(Runnable handler) {
+        return newButtonBuilder(ActionRegistry.newOkAction(handler));
     }
 
     default Button newCancelButton(Runnable handler) {
-        return newButton(ActionRegistry.newCancelAction(handler));
+        return newCancelButtonBuilder(handler).build();
+    }
+
+    default ButtonBuilder newCancelButtonBuilder(Runnable handler) {
+        return newButtonBuilder(ActionRegistry.newCancelAction(handler));
     }
 
     default Button newRemoveButton(Runnable handler) {
-        return newButton(ActionRegistry.newRemoveAction(handler));
+        return newRemoveButtonBuilder(handler).build();
+    }
+
+    default ButtonBuilder newRemoveButtonBuilder(Runnable handler) {
+        return newButtonBuilder(ActionRegistry.newRemoveAction(handler));
     }
 
 }
--- naga-framework/src/main/java/naga/framework/ui/controls/ControlFactoryMixin.java
+++ naga-framework/src/main/java/naga/framework/ui/controls/ControlFactoryMixin.java
@@ -1,7 +1,10 @@
 package naga.framework.ui.controls;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
+import naga.framework.ui.action.Action;
 import naga.framework.ui.i18n.I18nMixin;
 
 /**
@@ -10,11 +13,39 @@ import naga.framework.ui.i18n.I18nMixin;
 public interface ControlFactoryMixin extends I18nMixin {
 
     default Button newButton() {
-        return styleButton(new Button());
+        return newButtonBuilder().build();
+    }
+
+    default ButtonBuilder newButtonBuilder() {
+        return new ButtonBuilder().setI18n(getI18n()).setStyleFunction(this::styleButton);
     }
 
     default Button newButton(Object i18nKey) {
-        return translateText(newButton(), i18nKey);
+        return newButtonBuilder(i18nKey).build();
+    }
+
+    default ButtonBuilder newButtonBuilder(Object i18nKey) {
+        return newButtonBuilder().setI18nKey(i18nKey);
+    }
+
+    default Button newButton(Object i18nKey, EventHandler<ActionEvent> onAction) {
+        return newButtonBuilder(i18nKey, onAction).build();
+    }
+
+    default ButtonBuilder newButtonBuilder(Object i18nKey, EventHandler<ActionEvent> onAction) {
+        return newButtonBuilder(i18nKey).setOnAction(onAction);
+    }
+
+    default Button newButton(Action action) {
+        return newButtonBuilder(action).build();
+    }
+
+    default ButtonBuilder newButtonBuilder(Action action) {
+        return newButtonBuilder().setAction(action);
+    }
+
+    default Button styleButton(Button button) {
+        return button;
     }
 
     default CheckBox newCheckBox(Object i18nKey) {
@@ -45,10 +76,6 @@ public interface ControlFactoryMixin extends I18nMixin {
 
     default Text newText(Object i18nKey) {
         return translateText(new Text(), i18nKey);
-    }
-
-    default Button styleButton(Button button) {
-        return button;
     }
 
 }
