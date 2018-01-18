bc20cd742da79f5d1457d7f2e14cb6ccbe3ea010
Bruno Salmon <dev.salmonb@gmail.com>
2018-01-18 14:56:38 +0100

    Made hBoxCollator tolerate nodes = null

--- naga-fx/src/main/java/naga/fxdata/cell/collator/NodeCollatorRegistry.java
+++ naga-fx/src/main/java/naga/fxdata/cell/collator/NodeCollatorRegistry.java
@@ -17,12 +17,14 @@ public class NodeCollatorRegistry {
     private static Map<String, NodeCollator> collators = new HashMap<>();
 
     private static NodeCollator hBoxCollator = nodes -> {
-        HBox hBox = new HBox((double) 5, nodes);
+        HBox hBox = new HBox(5);
+        if (nodes != null)
+            hBox.getChildren().addAll(nodes);
         hBox.setAlignment(Pos.CENTER_LEFT);
         return hBox;
     };
-    private static NodeCollator vBoxCollator = (children) -> new VBox(children);
-    private static NodeCollator flowPaneCollator = (children) -> new FlowPane(children);
+    private static NodeCollator vBoxCollator = VBox::new;
+    private static NodeCollator flowPaneCollator = FlowPane::new;
     private static NodeCollator firstCollator = nodes -> Arrays.getValue(nodes, 0);
 
     static {
