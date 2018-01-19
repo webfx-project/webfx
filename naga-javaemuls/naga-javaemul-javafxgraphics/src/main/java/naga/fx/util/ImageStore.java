d9cb970ba24e8e820d507aece8e0ec5b031a3350
Bruno Salmon <dev.salmonb@gmail.com>
2018-01-19 15:11:35 +0100

    Implemented new ImageStore feature: image view (with passed width and height) can be reset to the image intrinsic size once loaded (automatically used for /16/ and /32/ images)

--- naga-javaemuls/naga-javaemul-javafxgraphics/src/main/java/naga/fx/util/ImageStore.java
+++ naga-javaemuls/naga-javaemul-javafxgraphics/src/main/java/naga/fx/util/ImageStore.java
@@ -1,5 +1,7 @@
 package naga.fx.util;
 
+import emul.javafx.beans.value.ChangeListener;
+import emul.javafx.beans.value.ObservableValue;
 import emul.javafx.scene.image.Image;
 import emul.javafx.scene.image.ImageView;
 
@@ -11,10 +13,70 @@ import java.util.Map;
  */
 public class ImageStore {
 
-    private final static Map<String, Image> imagesCache =  new HashMap<>(); // new WeakHashMap<>();
+    private final static Map<String, Image> imagesCache = new HashMap<>(); // WeakHashMap() is not emulated in GWT
+
+    public static ImageView createImageView(String iconPath) {
+        double wh = 0;
+        boolean resetToIntrinsicSizeOnceLoaded = false;
+        if (iconPath != null) {
+            if (resetToIntrinsicSizeOnceLoaded = iconPath.contains("/16/"))
+                wh = 16;
+            else if (resetToIntrinsicSizeOnceLoaded = iconPath.contains("/32/"))
+                wh = 32;
+        }
+        return createImageView(iconPath, wh, wh, resetToIntrinsicSizeOnceLoaded);
+    }
+
+    public static ImageView createImageView(String iconPath, double w, double h) {
+        return createImageView(iconPath, w, h, false);
+    }
+
+    public static ImageView createImageView(String iconPath, double w, double h, boolean resetToIntrinsicSizeOnceLoaded) {
+        ImageView imageView = new ImageView();
+        if (iconPath != null) {
+            Image image = getOrCreateImage(iconPath, w, h, resetToIntrinsicSizeOnceLoaded);
+            if (image != null) {
+                imageView.setImage(image);
+                if (resetToIntrinsicSizeOnceLoaded) {
+                    if (isImageLoaded(image))
+                        w = h = 0;
+                    else
+                        runOnImageLoaded(image, () -> {
+                            imageView.setFitWidth(0d);
+                            imageView.setFitHeight(0d);
+                        });
+                }
+            }
+        }
+        if (w > 0)
+            imageView.setFitWidth(w);
+        if (h > 0)
+            imageView.setFitHeight(h);
+        return imageView;
+    }
+
+    private static boolean isImageLoaded(Image image) {
+        return image.getWidth() > 0;
+    }
+
+    private static void runOnImageLoaded(Image image, Runnable runnable) {
+        image.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                observable.removeListener(this);
+                runnable.run();
+            }
+        });
+    }
 
     public static Image getOrCreateImage(String url) {
         return getOrCreateImage(url, 0, 0);
+    }
+
+    public static Image getOrCreateImage(String url, double w, double h, boolean resetToIntrinsicSizeOnceLoaded) {
+        if (resetToIntrinsicSizeOnceLoaded) // width & height are not applied to the image in this case
+            w = h = 0; // keeping 0 (natural size) instead
+        return getOrCreateImage(url, w, h);
     }
 
     public static Image getOrCreateImage(String url, double w, double h) {
@@ -38,32 +100,5 @@ public class ImageStore {
         if (image != null && (w != 0 && image.getWidth() != w || h != 0 && image.getHeight() != h))
             image = null;
         return image;
-    }
-
-    public static ImageView createImageView(String iconPath) {
-        double wh = 0;
-        if (iconPath != null) {
-            if (iconPath.contains("/16/"))
-                wh = 16;
-            else if (iconPath.contains("/32/"))
-                wh = 32;
-        }
-        return createImageView(iconPath, wh, wh);
-    }
-
-    public static ImageView createImageView(String iconPath, double w, double h) {
-        ImageView imageView = null;
-        if (iconPath != null) {
-            Image image = getOrCreateImage(iconPath, w, h);
-            if (image != null)
-                imageView = new ImageView(image);
-        }
-        if (imageView == null)
-            imageView = new ImageView();
-        if (w > 0)
-            imageView.setFitWidth(w);
-        if (h > 0)
-            imageView.setFitHeight(h);
-        return imageView;
     }
 }
