944130699a2977b5a1ce7baa0450f96b3875f772
Bruno Salmon <dev.salmonb@gmail.com>
2018-01-22 18:02:20 +0100

    Made MongooseValidationSupport work with any node (not just Control). Also renamed some methods.

--- mongoose/mongoose-logic/src/main/java/mongoose/activities/shared/logic/ui/validation/MongooseValidationSupport.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/shared/logic/ui/validation/MongooseValidationSupport.java
@@ -17,9 +17,10 @@ import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
+import javafx.scene.control.Control;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextInputControl;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
@@ -37,7 +38,6 @@ import org.controlsfx.control.decoration.GraphicDecoration;
 import org.controlsfx.validation.decoration.GraphicValidationDecoration;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -47,8 +47,6 @@ import java.util.List;
 public class MongooseValidationSupport {
 
     private static final String DEFAULT_REQUIRED_MESSAGE = "This field is required";
-    private static final Image ERROR_IMAGE = ImageStore.getOrCreateImage(MongooseIcons.validationErrorIcon16Url, 16, 16);
-    private static final Image REQUIRED_IMAGE = ImageStore.getOrCreateImage(MongooseIcons.validationRequiredIcon16Url);
 
     private final List<Validator> validators = new ArrayList<>();
     private final List<Node> validatorErrorDecorationNodes = new ArrayList<>();
@@ -82,58 +80,57 @@ public class MongooseValidationSupport {
     }
 
     public void addRequiredInput(TextInputControl textInputControl, String errorMessage) {
-        addNotEmptyControlValidation(textInputControl.textProperty(), textInputControl, errorMessage);
+        addRequiredInput(textInputControl.textProperty(), textInputControl, errorMessage);
     }
 
-    public void addNotEmptyControlValidation(ObservableValue valueProperty, Control control) {
-        addNotEmptyControlValidation(valueProperty, control, DEFAULT_REQUIRED_MESSAGE);
+    public void addRequiredInput(ObservableValue valueProperty, Node inputNode) {
+        addRequiredInput(valueProperty, inputNode, DEFAULT_REQUIRED_MESSAGE);
     }
 
-    public void addNotEmptyControlValidation(ObservableValue valueProperty, Control control, String errorMessage) {
-        addControlValidation(Bindings.createBooleanBinding(() -> testNotEmpty(valueProperty.getValue()), valueProperty), control, errorMessage);
+    public void addRequiredInput(ObservableValue valueProperty, Node inputNode, String errorMessage) {
+        addValidationRule(Bindings.createBooleanBinding(() -> testNotEmpty(valueProperty.getValue()), valueProperty), inputNode, errorMessage);
     }
 
     private static boolean testNotEmpty(Object value) {
         return value != null && (!(value instanceof String) || !((String) value).trim().isEmpty());
     }
 
-    public void addControlValidation(ObservableValue<Boolean> validProperty, Control control, String errorMessage) {
+    public void addValidationRule(ObservableValue<Boolean> validProperty, Node node, String errorMessage) {
         ObservableRuleBasedValidator validator = new ObservableRuleBasedValidator();
-        ObservableBooleanValue rule = // ObservableRules.notEmpty(control.textProperty());
+        ObservableBooleanValue rule =
                 Bindings.createBooleanBinding(() ->
-                    !validatingProperty.get() || validProperty.getValue() || !isShowing(control)
+                    !validatingProperty.get() || validProperty.getValue() || !isShowing(node)
                 , validProperty, validatingProperty);
         validator.addRule(rule, ValidationMessage.error(errorMessage));
-        int index = validators.size();
         validators.add(validator);
-        validatorErrorDecorationNodes.add(null);
+        validatorErrorDecorationNodes.add(node);
 
-        ControlsFxVisualizer validationVisualizer = new ControlsFxVisualizer();
-        validationVisualizer.setDecoration(new GraphicValidationDecoration() {
-            @Override
-            protected Node createErrorNode() {
-                Node errorNode = new ImageView(ERROR_IMAGE);
-                validatorErrorDecorationNodes.set(index, errorNode);
-                errorNode.getProperties().put("control", control);
-                return errorNode;
-            }
+        if (node instanceof Control) {
+            Control control = (Control) node;
+            ControlsFxVisualizer validationVisualizer = new ControlsFxVisualizer();
+            validationVisualizer.setDecoration(new GraphicValidationDecoration() {
+                @Override
+                protected Node createErrorNode() {
+                    return ImageStore.createImageView(MongooseIcons.validationErrorIcon16Url);
+                }
 
-            @Override
-            protected Collection<Decoration> createValidationDecorations(org.controlsfx.validation.ValidationMessage message) {
-                boolean isButton = control instanceof Button; // Probably an entity button with already a drop down arrow icon on right
-                boolean isTextInput = !isButton && control instanceof TextInputControl;
-                double xRelativeOffset = isButton || isTextInput ? -1 : 1; // positioning the decoration inside the control for button and text input
-                double xOffset = isButton ?  -20 : 0; // moving the decoration before the drop down arrow
-                return Arrays.asList(new GraphicDecoration(createDecorationNode(message), Pos.CENTER_RIGHT, xOffset, 0, xRelativeOffset, 0));
-            }
+                @Override
+                protected Collection<Decoration> createValidationDecorations(org.controlsfx.validation.ValidationMessage message) {
+                    boolean isButton = node instanceof Button; // Probably an entity button with already a drop down arrow icon on right
+                    boolean isTextInput = !isButton && node instanceof TextInputControl;
+                    double xRelativeOffset = isButton || isTextInput ? -1 : 1; // positioning the decoration inside the control for button and text input
+                    double xOffset = isButton ?  -20 : 0; // moving the decoration before the drop down arrow
+                    return java.util.Collections.singletonList(new GraphicDecoration(createDecorationNode(message), Pos.CENTER_RIGHT, xOffset, 0, xRelativeOffset, 0));
+                }
 
-            @Override
-            protected Collection<Decoration> createRequiredDecorations(Control target) {
-                return Arrays.asList(new GraphicDecoration(new ImageView(REQUIRED_IMAGE), Pos.CENTER_LEFT, -5, 0));
-            }
-        });
-        validationVisualizer.initVisualization(validator.getValidationStatus(), control, true);
-        control.getProperties().put("validationVisualizer", validationVisualizer);
+                @Override
+                protected Collection<Decoration> createRequiredDecorations(Control target) {
+                    return java.util.Collections.singletonList(new GraphicDecoration(ImageStore.createImageView(MongooseIcons.validationRequiredIcon16Url), Pos.CENTER_LEFT, -5, 0));
+                }
+            });
+            validationVisualizer.initVisualization(validator.getValidationStatus(), control, true);
+            node.getProperties().put("validationVisualizer", validationVisualizer);
+        }
     }
 
     private void showValidatorErrorPopOver(Validator validator) {
@@ -154,19 +151,19 @@ public class MongooseValidationSupport {
             label.setTextFill(Color.WHITE);
             label.setBackground(BackgroundUtil.newBackground(Color.RED, 5, 2));
             label.setBorder(BorderUtil.newBorder(Color.WHITE, 5, 2));
-            Rectangle triangle = new Rectangle(10, 10, Color.RED);
-            triangle.getTransforms().add(new Rotate(45, 5, 5));
-            triangle.layoutYProperty().bind(Properties.compute(label.heightProperty(), n -> n.doubleValue() - 7));
-            triangle.setLayoutX(20d);
-            popOverContentNode = new Group(label, triangle);
+            Rectangle diamond = new Rectangle(10, 10, Color.RED);
+            diamond.getTransforms().add(new Rotate(45, 5, 5));
+            diamond.layoutYProperty().bind(Properties.compute(label.heightProperty(), n -> n.doubleValue() - 7));
+            diamond.setLayoutX(20d);
+            popOverContentNode = new Group(label, diamond);
             //popOverContentNode.setOpacity(0.75);
             //popOverContentNode.setEffect(new DropShadow());
             showPopOver(errorDecorationNode);
             // Removing the error pop over when the status is valid again
             validator.getValidationStatus().validProperty().addListener(new ChangeListener<Boolean>() {
                 @Override
-                public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-                    if (newValue) {
+                public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean valid) {
+                    if (valid) {
                         observable.removeListener(this);
                         popOverOwnerNode = null;
                         hidePopOver();
@@ -220,7 +217,7 @@ public class MongooseValidationSupport {
         Platform.runLater(() -> {
             hidePopOver();
             if (isShowing(popOverOwnerNode)) {
-                popOverDecorationTarget = (Node) popOverOwnerNode.getProperties().get("control");
+                popOverDecorationTarget = popOverOwnerNode;
                 popOverDecoration = new GraphicDecoration(popOverContentNode, 0, -1, 0, -1);
                 popOverDecoration.applyDecoration(popOverDecorationTarget);
                 if (popUpOverAutoScroll) {
