d9bb95152572732e8f8e70a912707bbff28c8f38
Bruno Salmon <dev.salmonb@gmail.com>
2018-01-30 17:49:07 +0100

    Made Animations.animateProperty() return the Timeline if created

--- naga-framework/src/main/java/naga/framework/ui/anim/Animations.java
+++ naga-framework/src/main/java/naga/framework/ui/anim/Animations.java
@@ -18,21 +18,25 @@ public class Animations {
     // Better ease out interpolator than the one proposed in JavaFx (ie Interpolator.EASE_OUT)
     public final static Interpolator EASE_OUT_INTERPOLATOR = Interpolator.SPLINE(0, .75, .25, 1);
 
-    public static <T> void animateProperty(WritableValue<T> target, T finalValue) {
-        animateProperty(target, finalValue, true);
+    public static <T> Timeline animateProperty(WritableValue<T> target, T finalValue) {
+        return animateProperty(target, finalValue, true);
     }
 
-    public static <T> void animateProperty(WritableValue<T> target, T finalValue, boolean animate) {
-        animateProperty(target, finalValue, animate ? EASE_OUT_INTERPOLATOR : null);
+    public static <T> Timeline animateProperty(WritableValue<T> target, T finalValue, boolean animate) {
+        return animateProperty(target, finalValue, animate ? EASE_OUT_INTERPOLATOR : null);
     }
 
-    public static <T> void animateProperty(WritableValue<T> target, T finalValue, Interpolator interpolator) {
+    public static <T> Timeline animateProperty(WritableValue<T> target, T finalValue, Interpolator interpolator) {
         if (!Objects.areEquals(target.getValue(), finalValue)) {
             if (interpolator == null)
                 target.setValue(finalValue);
-            else
-                new Timeline(new KeyFrame(Duration.seconds(1), new KeyValue(target, finalValue, interpolator))).play();
+            else {
+                Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), new KeyValue(target, finalValue, interpolator)));
+                timeline.play();
+                return timeline;
+            }
         }
+        return null;
     }
 
     public static void shake(Node node) {
