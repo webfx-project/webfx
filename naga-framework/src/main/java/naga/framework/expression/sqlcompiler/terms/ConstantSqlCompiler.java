f67b2a987ecaabb79bc09c7df0301438a35db5f9
Bruno Salmon <dev.salmonb@gmail.com>
2018-02-05 15:34:00 +0100

    Added LocalDate case in ConstantSqlCompiler and made conversion utility methods public

--- naga-framework/src/main/java/naga/framework/expression/sqlcompiler/terms/ConstantSqlCompiler.java
+++ naga-framework/src/main/java/naga/framework/expression/sqlcompiler/terms/ConstantSqlCompiler.java
@@ -1,7 +1,10 @@
 package naga.framework.expression.sqlcompiler.terms;
 
 import naga.framework.expression.terms.Constant;
+import naga.util.Dates;
 import naga.util.Strings;
+
+import java.time.LocalDate;
 
 /**
  * @author Bruno Salmon
@@ -14,14 +17,28 @@ public class ConstantSqlCompiler extends AbstractTermSqlCompiler<Constant> {
 
     @Override
     public void compileExpressionToSql(Constant e, Options o) {
-        StringBuilder sb = o.build.prepareAppend(o);
         Object constantValue = e.getConstantValue();
-        if (constantValue instanceof String) {
-            sb.append(toSqlQuotedString((String) constantValue));
-            if (o.separator != null) // Quick fix for Postgres when ordering by ISC transport field = (select 'ISC' from ...)
-                sb.append("::text"); // casting to text to avoid the "failed to find conversion function from unknown to text" error
-        } else
-            sb.append(constantValue);
+        StringBuilder sb = o.build.prepareAppend(o).append(toSqlConstant(constantValue));
+        // Quick fix for Postgres when ordering by ISC transport field = (select 'ISC' from ...)
+        // casting to text to avoid the "failed to find conversion function from unknown to text" error
+        if (o.separator != null && constantValue instanceof String)
+            sb.append("::text");
+    }
+
+    public static String toSqlConstant(Object constant) {
+        if (constant instanceof String)
+            return toSqlString((String) constant);
+        if (constant instanceof LocalDate)
+            return toSqlDate((LocalDate) constant);
+        return String.valueOf(constant);
+    }
+
+    public static String toSqlString(String s) {
+        return toSqlQuotedString(s);
+    }
+
+    public static String toSqlDate(LocalDate date) {
+        return Dates.format(date, "'yyyy-MM-dd'");
     }
 
     private static String toSqlQuotedString(String s) {
