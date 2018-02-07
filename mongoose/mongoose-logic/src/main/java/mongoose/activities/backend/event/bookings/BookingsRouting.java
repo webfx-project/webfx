ec12f1ba8aa856d5fbe1b5e875a72942a9e72e5c
Bruno Salmon <dev.salmonb@gmail.com>
2018-02-07 18:06:20 +0100

    Added parseDayParam to BookingsRouting

--- mongoose/mongoose-logic/src/main/java/mongoose/activities/backend/event/bookings/BookingsRouting.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/backend/event/bookings/BookingsRouting.java
@@ -8,13 +8,15 @@ import naga.framework.ui.router.UiRoute;
 import naga.framework.ui.router.UiRouteImpl;
 import naga.platform.client.url.history.History;
 
+import java.time.LocalDate;
+
 /**
  * @author Bruno Salmon
  */
 public class BookingsRouting {
 
     // Would be better but pb retrieving named groups doesn't work with JavaScript RegExp (can't retrieve groups): private final static String ANY_PATH = "/bookings(/organization/:organizationId|/event/:eventId|/day/:day|/arrivals|/departures)*";
-    private final static String ANY_PATH = "/bookings(/organization/:organizationId)?(/event/:eventId)?(/day/:day)?(/arrivals)?(/departures)?";
+    private final static String ANY_PATH = "/bookings(/organization/:organizationId)?(/event/:eventId)?(/day/:day)?(/arrivals)?(/departures)?(/minday/:minday)?(/maxday/:maxday)?";
     private final static String EVENT_PATH = "/bookings/event/:eventId";
 
     public static UiRoute<?> uiRoute() {
@@ -31,4 +33,14 @@ public class BookingsRouting {
         history.push(MongooseRoutingUtil.interpolateEventIdInPath(eventId, EVENT_PATH));
     }
 
+    public static LocalDate parseDayParam(String parameterValue) {
+        if (parameterValue == null)
+            return null;
+        switch (parameterValue) {
+            case "yesterday" : return LocalDate.now().minusDays(1);
+            case "today": return LocalDate.now();
+            case "tomorrow" : return LocalDate.now().plusDays(1);
+            default: return LocalDate.parse(parameterValue); // Expecting an iso date (yyyy-MM-dd)
+        }
+    }
 }
