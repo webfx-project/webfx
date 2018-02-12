55f1975a2ecae37702384a99281ba9b7fd1be666
Bruno Salmon <dev.salmonb@gmail.com>
2018-02-12 11:02:33 +0100

    Rewrote java.util.regex emulation based on XRegExp

--- naga-javaemuls/naga-javaemul-gwt/src/main/java/emul/java/util/regex/Matcher.java
+++ naga-javaemuls/naga-javaemul-gwt/src/main/java/emul/java/util/regex/Matcher.java
@@ -1,6 +1,6 @@
 package emul.java.util.regex;
 
-import com.google.gwt.regexp.shared.MatchResult;
+import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.regexp.shared.RegExp;
 
 /** Emulation of the {@link Matcher} class, uses {@link RegExp} as internal implementation.
@@ -8,25 +8,76 @@ import com.google.gwt.regexp.shared.RegExp;
 
 public class Matcher {
 
-    private final RegExp regExp;
     private final String input;
-    private final MatchResult matchResult;
+    /* Implementation using standard JavaScript RegExp
+     Commented as not all browsers implement named groups (only Chrome 64 for now)
+
+        private final RegExp regExp;
+        private final MatchResult matchResult;
+
+        Matcher (Pattern pattern, CharSequence input) {
+            this.input = String.valueOf(input);
+            this.regExp = pattern.regExp;
+            matchResult = regExp.exec(this.input);
+        }
+
+        public boolean matches() {
+            return regExp.test(input);
+        }
+
+        public int groupCount() {
+            return matchResult.getGroupCount();
+        }
+
+        public String group(int group) {
+            return matchResult.getGroup(group);
+        }
+
+        public String group(String name) {
+            return getMatchResultGroup((JavaScriptObject) (Object) matchResult, name);
+        }
+
+    */
+
+    private static native String getMatchResultGroup(JavaScriptObject mr, String name) /*-{
+        return mr.groups ? mr.groups[name] : null;
+    }-*/;
+
+    private final JavaScriptObject xRegExp;
+    private final JavaScriptObject matchResult;
 
     Matcher (Pattern pattern, CharSequence input) {
-        this.regExp = pattern.regExp;
         this.input = String.valueOf(input);
-        matchResult = regExp.exec(this.input);
+        xRegExp = pattern.xRegExp;
+        matchResult = newMatchResult(xRegExp, this.input);
     }
 
     public boolean matches() {
-        return regExp.test(input);
+        return matchResult != null;
     }
 
     public int groupCount() {
-        return matchResult.getGroupCount();
+        return matchResultLength(matchResult);
     }
 
     public String group(int group) {
-        return matchResult.getGroup(group);
+        return matchValue(matchResult, group);
     }
+
+    public String group(String name) {
+        return getMatchResultGroup(matchResult, name);
+    }
+
+    private static native JavaScriptObject newMatchResult(JavaScriptObject xRegExp, String input) /*-{
+        return $wnd.XRegExp.exec(input, xRegExp);
+    }-*/;
+
+    private static native int matchResultLength(JavaScriptObject mr) /*-{
+        return mr.length;
+    }-*/;
+
+    private static native String matchValue(JavaScriptObject mr, int pos) /*-{
+        //return Object.values(mr)[pos]; // not supported by IE
+        return mr[Object.keys(mr)[pos]];
+    }-*/;
 }
--- naga-javaemuls/naga-javaemul-gwt/src/main/java/emul/java/util/regex/Pattern.java
+++ naga-javaemuls/naga-javaemul-gwt/src/main/java/emul/java/util/regex/Pattern.java
@@ -1,11 +1,15 @@
 package emul.java.util.regex;
 
+import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.regexp.shared.RegExp;
 
 /** Emulation of the {@link Pattern} class, uses {@link RegExp} as internal implementation.
  * @author hneuer */
 
-public class Pattern {
+public final class Pattern {
+
+/* Implementation using standard JavaScript RegExp
+ Commented as not all browsers implement named groups (only Chrome 64 for now)
 
     final RegExp regExp;
 
@@ -13,11 +17,26 @@ public class Pattern {
         this.regExp = RegExp.compile(regExp);
     }
 
-    public static Pattern compile(String regExp) {
-        return new Pattern(regExp);
-    }
+*/
 
     public Matcher matcher(CharSequence input) {
         return new Matcher(this, input);
     }
+
+    public static Pattern compile(String regExp) {
+        return new Pattern(regExp);
+    }
+
+    // Using XRegExp instead
+
+    final JavaScriptObject xRegExp;
+
+    private Pattern (String regExp) {
+        xRegExp = newXRegExp(regExp);
+    }
+
+    private static native JavaScriptObject newXRegExp(String regExp) /*-{
+        return $wnd.XRegExp(regExp);
+    }-*/;
+
 }
