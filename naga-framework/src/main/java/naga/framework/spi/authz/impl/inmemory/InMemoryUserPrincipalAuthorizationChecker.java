9b2f0df8061fca2ea0c6abe76076a43f6c9e1f38
Bruno Salmon <dev.salmonb@gmail.com>
2018-02-19 23:45:46 +0100

    Added setUpInMemoryAsyncLoading() utility method in InMemoryUserPrincipalAuthorizedChecker

--- naga-framework/src/main/java/naga/framework/spi/authz/impl/inmemory/InMemoryUserPrincipalAuthorizationChecker.java
+++ naga-framework/src/main/java/naga/framework/spi/authz/impl/inmemory/InMemoryUserPrincipalAuthorizationChecker.java
@@ -1,7 +1,10 @@
 package naga.framework.spi.authz.impl.inmemory;
 
 import naga.framework.spi.authz.impl.UserPrincipalAuthorizationChecker;
+import naga.util.async.AsyncResult;
 import naga.util.async.Future;
+import naga.util.async.FutureBroadcaster;
+import naga.util.async.Handler;
 
 /**
  * @author Bruno Salmon
@@ -9,7 +12,8 @@ import naga.util.async.Future;
 public class InMemoryUserPrincipalAuthorizationChecker implements UserPrincipalAuthorizationChecker {
 
     private final Object userPrincipal;
-    private final InMemoryAuthorizationRule inMemoryAuthorizationRules;
+    protected final InMemoryAuthorizationRule inMemoryAuthorizationRules;
+    private FutureBroadcaster<?> loadingFutureBroadcaster;
 
     public InMemoryUserPrincipalAuthorizationChecker(Object userPrincipal, InMemoryAuthorizationRule inMemoryAuthorizationRules) {
         this.userPrincipal = userPrincipal;
@@ -23,6 +27,21 @@ public class InMemoryUserPrincipalAuthorizationChecker implements UserPrincipalA
 
     @Override
     public Future<Boolean> isAuthorized(Object operationAuthorizationRequest) {
-        return Future.succeededFuture(inMemoryAuthorizationRules.authorizes(operationAuthorizationRequest));
+        Future<Boolean> future = Future.future();
+        FutureBroadcaster<?> broadcaster = loadingFutureBroadcaster;
+        return broadcaster == null ? complete(operationAuthorizationRequest, future)
+                : broadcaster.newClient().compose(result -> complete(operationAuthorizationRequest, future), future);
+    }
+
+    private Future<Boolean> complete(Object operationAuthorizationRequest, Future<Boolean> future) {
+        future.complete(inMemoryAuthorizationRules.authorizes(operationAuthorizationRequest));
+        return future;
+    }
+
+    protected <T> void setUpInMemoryAsyncLoading(Future<T> loadingFuture, Handler<AsyncResult<T>> loadedHandler) {
+        FutureBroadcaster<T> broadcaster = new FutureBroadcaster<>(loadingFuture);
+        loadingFutureBroadcaster = broadcaster;
+        broadcaster.newClient().setHandler(loadedHandler);
+        broadcaster.newClient().setHandler(ar -> loadingFutureBroadcaster = null);
     }
 }
