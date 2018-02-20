7e7df93c40baccc6511483231ea44faa9247025a
Bruno Salmon <dev.salmonb@gmail.com>
2018-02-20 23:05:33 +0100

    Added export parameter declaration in bookings route (/bookings/export/:activityStateId)

--- mongoose/mongoose-logic/src/main/java/mongoose/activities/backend/event/bookings/BookingsRouting.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/backend/event/bookings/BookingsRouting.java
@@ -14,8 +14,8 @@ import java.time.LocalDate;
  */
 public class BookingsRouting {
 
-    // Would be better but pb retrieving named groups doesn't work with JavaScript RegExp (can't retrieve groups): private final static String ANY_PATH = "/bookings(/organization/:organizationId|/event/:eventId|/day/:day|/arrivals|/departures|/minday/:minDay|/maxday/:maxDay|/filter/:filter|/groupby|:groupBy|/orderby/:orderBy/columns/:columns)*";
-    private final static String ANY_PATH = "/bookings(/organization/:organizationId)?(/event/:eventId)?(/day/:day)?(/arrivals)?(/departures)?(/minday/:minDay)?(/maxday/:maxDay)?(/filter/:filter)?(/groupby/:groupBy)?(/orderby/:orderBy)?(/columns/:columns)?";
+    // Would be better but pb retrieving named groups doesn't work with JavaScript RegExp (can't retrieve groups): private final static String ANY_PATH = "/bookings(/organization/:organizationId|/event/:eventId|/day/:day|/arrivals|/departures|/minday/:minDay|/maxday/:maxDay|/filter/:filter|/groupby|:groupBy|/orderby/:orderBy/columns/:columns|export/:activityStateId)*";
+    private final static String ANY_PATH = "/bookings(/organization/:organizationId)?(/event/:eventId)?(/day/:day)?(/arrivals)?(/departures)?(/minday/:minDay)?(/maxday/:maxDay)?(/filter/:filter)?(/groupby/:groupBy)?(/orderby/:orderBy)?(/columns/:columns)?(/export/:activityStateId)?";
     private final static String EVENT_PATH = "/bookings/event/:eventId";
 
     public static UiRoute<?> uiRoute() {
@@ -39,9 +39,9 @@ public class BookingsRouting {
             return null;
         switch (parameterValue) {
             case "yesterday" : return LocalDate.now().minusDays(1);
-            case "today": return LocalDate.now();
-            case "tomorrow" : return LocalDate.now().plusDays(1);
-            default: return LocalDate.parse(parameterValue); // Expecting an iso date (yyyy-MM-dd)
+            case "today":      return LocalDate.now();
+            case "tomorrow" :  return LocalDate.now().plusDays(1);
+            default:           return LocalDate.parse(parameterValue); // Expecting an iso date (yyyy-MM-dd)
         }
     }
 }
