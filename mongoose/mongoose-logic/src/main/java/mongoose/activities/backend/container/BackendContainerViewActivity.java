0afd212a0ec56f59034992bc986e3351f84dba9f
Bruno Salmon <dev.salmonb@gmail.com>
2018-02-26 22:53:00 +0100

    Changed standard actions with operation actions in the Mongoose backend container

--- mongoose/mongoose-logic/src/main/java/mongoose/activities/backend/container/BackendContainerViewActivity.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/backend/container/BackendContainerViewActivity.java
@@ -1,12 +1,15 @@
 package mongoose.activities.backend.container;
 
-import mongoose.activities.backend.event.bookings.BookingsRouting;
-import mongoose.activities.backend.event.letters.LettersRouting;
-import mongoose.activities.backend.events.EventsRouting;
-import mongoose.activities.backend.monitor.MonitorRooting;
-import mongoose.activities.backend.organizations.OrganizationsRouting;
-import mongoose.activities.backend.tester.TesterRooting;
+import mongoose.activities.backend.event.bookings.BookingsRoutingRequest;
+import mongoose.activities.backend.event.letters.LettersRoutingRequest;
+import mongoose.activities.backend.events.EventsRoutingRequest;
+import mongoose.activities.backend.monitor.MonitorRouting;
+import mongoose.activities.backend.monitor.MonitorRoutingRequest;
+import mongoose.activities.backend.organizations.OrganizationsRoutingRequest;
+import mongoose.activities.backend.tester.TesterRouting;
+import mongoose.activities.backend.tester.TesterRoutingRequest;
 import mongoose.activities.shared.container.SharedContainerViewActivity;
+import naga.framework.operation.OperationActionProducer;
 import naga.framework.ui.action.Action;
 import naga.util.collection.Collections;
 
@@ -15,20 +18,28 @@ import java.util.Collection;
 /**
  * @author Bruno Salmon
  */
-public class BackendContainerViewActivity extends SharedContainerViewActivity {
+public class BackendContainerViewActivity extends SharedContainerViewActivity implements OperationActionProducer {
 
     @Override
     protected Collection<Action> navigationActions() {
         super.navigationActions();
+        getOperationActionRegistry()
+                .registerOperationAction(OrganizationsRoutingRequest.class, newAction("Organizations"))
+                .registerOperationAction(EventsRoutingRequest.class, newAction("Events"))
+                .registerOperationAction(BookingsRoutingRequest.class, newAction("Bookings"))
+                .registerOperationAction(LettersRoutingRequest.class, newAction("Bookings"))
+                .registerOperationAction(MonitorRoutingRequest.class, newAuthAction("Monitor", authorizedProperty(MonitorRouting.authorizationRequest())))
+                .registerOperationAction(TesterRoutingRequest.class, newAuthAction("Tester", authorizedProperty(TesterRouting.authorizationRequest())))
+        ;
         return Collections.listOf(
                   backAction
                 , forwardAction
-                , newAction("Organizations",  () -> OrganizationsRouting.route(getHistory()))
-                , newAction("Events",         () -> EventsRouting.route(getHistory()))
-                , newAction("Bookings",       () -> BookingsRouting.routeUsingEventId(getParameter("eventId"), getHistory()))
-                , newAction("Letters",        () -> LettersRouting.routeUsingEventId(getParameter("eventId"), getHistory()))
-                , newAuthAction("Monitor",    () -> MonitorRooting.route(getHistory()), authorizedProperty(MonitorRooting.authorizationRequest()))
-                , newAuthAction("Tester",     () -> TesterRooting.route(getHistory()), authorizedProperty(TesterRooting.authorizationRequest()))
+                , newAction(() -> new OrganizationsRoutingRequest(getHistory()))
+                , newAction(() -> new EventsRoutingRequest(getHistory()))
+                , newAction(() -> new BookingsRoutingRequest(getParameter("eventId"), getHistory()))
+                , newAction(() -> new LettersRoutingRequest(getParameter("eventId"), getHistory()))
+                , newAction(() -> new MonitorRoutingRequest(getHistory()))
+                , newAction(() -> new TesterRoutingRequest(getHistory()))
                 , englishAction
                 , frenchAction
         );
