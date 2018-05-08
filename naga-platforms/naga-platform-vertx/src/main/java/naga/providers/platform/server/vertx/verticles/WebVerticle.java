663936cc0c4ec149425526a6c19f578c0f50c6e3
Bruno Salmon <dev.salmonb@gmail.com>
2018-05-08 23:29:39 +0200

    Disabled file caching for the SPA root file (to always return the latest version with the latest GWT compilation)

--- naga-platforms/naga-platform-vertx/src/main/java/naga/providers/platform/server/vertx/verticles/WebVerticle.java
+++ naga-platforms/naga-platform-vertx/src/main/java/naga/providers/platform/server/vertx/verticles/WebVerticle.java
@@ -56,9 +56,15 @@ public class WebVerticle extends AbstractVerticle {
                 )
         );
 
-        // GWT perfect caching
+        // GWT perfect caching (xxx.cache.xxx files will never change again)
         router.routeWithRegex(".*\\.cache\\..*").handler(routingContext -> {
             routingContext.response().putHeader("cache-control", "public, max-age=31556926");
+            routingContext.next();
+        });
+
+        // SPA root page shouldn't be cached (to always return the latest version with the latest GWT compilation)
+        router.routeWithRegex(".*/#/.*").handler(routingContext -> {
+            routingContext.response().putHeader("cache-control", "public, max-age=0");
             routingContext.next();
         });
 
