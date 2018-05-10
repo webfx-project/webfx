d13abac3a7d72b2e427a1dffd44b69714fb4670f
Bruno Salmon <dev.salmonb@gmail.com>
2018-05-10 18:14:53 +0200

    Added push client disconnect listener in MongooseVertxRootVerticle

--- mongoose/mongoose-vertx/src/main/java/mongoose/activities/server/vertx/MongooseVertxRootVerticle.java
+++ mongoose/mongoose-vertx/src/main/java/mongoose/activities/server/vertx/MongooseVertxRootVerticle.java
@@ -34,11 +34,31 @@ public class MongooseVertxRootVerticle extends RootVerticle {
 
     private static void registerMongooseLocalDataSource() {
         DataSourceModel dataSourceModel = DomainModelSnapshotLoader.getDataSourceModel();
-        String json = ResourceService.getText("mongoose/datasource/" + dataSourceModel.getId() + "/ConnectionDetails.json").result();
+        dataSourceModel.getDomainModel();
+        Object dataSourceId = dataSourceModel.getId();
+        String json = ResourceService.getText("mongoose/datasource/" + dataSourceId + "/ConnectionDetails.json").result();
         JsonObject jso = json == null ? null : Json.parseObject(json);
         ConnectionDetails connectionDetails = ConnectionDetails.fromJson(jso);
         if (connectionDetails != null)
-            LocalDataSourceRegistry.registerLocalDataSource(dataSourceModel.getId(), connectionDetails);
+            LocalDataSourceRegistry.registerLocalDataSource(dataSourceId, connectionDetails);
+        PushServerService.addPushClientDisconnectListener(pushClientId ->
+            UpdateService.executeUpdate(new UpdateArgument("update session_connection set \"end\"=now() where process_id=?", new Object[]{pushClientId}, dataSourceId))
+                .setHandler(ar -> {
+                    if (ar.failed())
+                        Logger.log(ar.cause());
+                }));
+/* Commented because executing it here is too early (the clients didn't have a chance to reconnect so all ping would fail)
+        QueryService.executeQuery(new QueryArgument("select distinct process_id from session_connection where \"end\" is null", dataSourceId))
+            .setHandler(ar -> {
+                if (ar.failed())
+                    Logger.log(ar.cause());
+                else {
+                    QueryResultSet rs = ar.result();
+                    for (int i = 0, n = rs.getRowCount(); i < n; i++)
+                        PushServerService.pingPushClient(Platform.bus(), rs.getValue(i, 0));
+                }
+            });
+*/
     }
 
 }
