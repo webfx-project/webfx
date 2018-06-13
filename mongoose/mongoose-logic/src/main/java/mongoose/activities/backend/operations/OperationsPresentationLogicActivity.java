1757577b9a929ea8b1f8e67bcc3be5c79cc83481
Bruno Salmon <dev.salmonb@gmail.com>
2018-06-13 22:41:33 +0200

    Added Operation PropertySheet dialog on click

--- mongoose/mongoose-logic/src/main/java/mongoose/activities/backend/operations/OperationsPresentationLogicActivity.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/backend/operations/OperationsPresentationLogicActivity.java
@@ -1,7 +1,11 @@
 package mongoose.activities.backend.operations;
 
+import javafx.scene.layout.Pane;
 import mongoose.activities.bothends.generic.MongooseDomainPresentationLogicActivityBase;
+import naga.framework.activity.combinations.viewapplication.ViewApplicationContext;
+import naga.framework.orm.entity.Entity;
 import naga.framework.ui.filter.ReactiveExpressionFilterFactoryMixin;
+import naga.framework.ui.graphic.controls.sheet.PropertySheet;
 import naga.util.function.Factory;
 
 /**
@@ -10,6 +14,8 @@ import naga.util.function.Factory;
 class OperationsPresentationLogicActivity
         extends MongooseDomainPresentationLogicActivityBase<OperationsPresentationModel>
         implements ReactiveExpressionFilterFactoryMixin {
+
+    private final String expressionColumns = "['name','operationCode','i18nCode','backend','frontend','public']";
 
     OperationsPresentationLogicActivity() {
         this(OperationsPresentationModel::new);
@@ -22,13 +28,19 @@ class OperationsPresentationLogicActivity
     @Override
     protected void startLogic(OperationsPresentationModel pm) {
         // Loading the domain model and setting up the reactive filter
-        createReactiveExpressionFilter("{class: 'Operation', alias: 'o'}")
-                // Search box condition
-                .combineIfNotEmptyTrim(pm.searchTextProperty(), s -> "{where: 'lower(name) like `%" + s.toLowerCase() + "%`'}")
-                // Limit condition
-                .combineIfPositive(pm.limitProperty(), l -> "{limit: '" + l + "'}")
-                .setExpressionColumns("['name','operationCode','i18nCode','backend','frontend','public']")
-                .displayResultInto(pm.genericDisplayResultProperty())
-                .start();
+        createReactiveExpressionFilter("{class: 'Operation', alias: 'o', orderBy: 'name'}")
+            // Search box condition
+            .combineIfNotEmptyTrim(pm.searchTextProperty(), s -> "{where: 'lower(name) like `%" + s.toLowerCase() + "%`'}")
+            // Limit condition
+            .combineIfPositive(pm.limitProperty(), l -> "{limit: '" + l + "'}")
+            .setExpressionColumns(expressionColumns)
+            .displayResultInto(pm.genericDisplayResultProperty())
+            .setSelectedEntityHandler(pm.genericDisplaySelectionProperty(), this::editOperation)
+            .start();
+    }
+
+    private void editOperation(Entity operation) {
+        Pane parent = (Pane) ViewApplicationContext.getViewApplicationContext().getNode();
+        PropertySheet.editEntity(operation, expressionColumns, parent);
     }
 }
