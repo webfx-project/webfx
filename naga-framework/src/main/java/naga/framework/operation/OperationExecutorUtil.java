b5b3eb0cc3e1d1799b759d083bc88cb985a98b18
Bruno Salmon <dev.salmonb@gmail.com>
2018-08-14 17:30:25 +0100

    Removed some unused code and classes from operation package

--- naga-framework/src/main/java/naga/framework/operation/OperationExecutorUtil.java
+++ naga-framework/src/main/java/naga/framework/operation/OperationExecutorUtil.java
@@ -1,9 +1,5 @@
 package naga.framework.operation;
 
-import naga.framework.operation.action.ChainedActionOperationExecutor;
-import naga.framework.operation.action.OperationActionRegistry;
-import naga.framework.operation.authz.AuthorizableOperationExecutor;
-import naga.framework.services.authz.mixin.HasUserPrincipal;
 import naga.util.async.AsyncFunction;
 import naga.util.async.Future;
 
@@ -18,12 +14,6 @@ public class OperationExecutorUtil {
         if (operationExecutor != null)
             return operationExecutor.apply(operationRequest);
         return Future.failedFuture(new IllegalArgumentException("No executor found for operation request " + operationRequest));
-    }
-
-    public static ChainedActionOperationExecutor createAuthorizableOperationActionExecutor(OperationActionRegistry operationActionRegistry, HasUserPrincipal userPrincipalFetcher, OperationExecutorRegistry operationExecutorRegistry) {
-        return new ChainedActionOperationExecutor(operationActionRegistry,
-               new AuthorizableOperationExecutor(userPrincipalFetcher,
-               new OperationDispatcher(operationExecutorRegistry)));
     }
 
 
--- naga-framework/src/main/java/naga/framework/operation/action/OperationAction.java
+++ naga-framework/src/main/java/naga/framework/operation/action/OperationAction.java
@@ -1,7 +1,6 @@
 package naga.framework.operation.action;
 
 import javafx.event.ActionEvent;
-import naga.framework.operation.ChainedOperationExecutor;
 import naga.framework.operation.OperationExecutorUtil;
 import naga.framework.ui.action.impl.WritableAction;
 import naga.util.async.AsyncFunction;
@@ -28,18 +27,8 @@ public class OperationAction<Rq, Rs> extends WritableAction {
         this.operationRequestFactory = operationRequestFactory;
         OperationActionRegistry registry = getOperationActionRegistry();
         if (registry == null)
-            registry = getOperationActionRegistry(topOperationExecutor);
-        if (registry == null)
             registry = OperationActionRegistry.getInstance();
         registry.bindOperationAction(this);
-    }
-
-    public OperationActionRegistry getOperationActionRegistry(AsyncFunction<Rq, Rs> operationExecutor) {
-        if (operationExecutor instanceof ChainedActionOperationExecutor)
-            return ((ChainedActionOperationExecutor) operationExecutor).getRegistry();
-        if (operationExecutor instanceof ChainedOperationExecutor)
-            return getOperationActionRegistry(((ChainedOperationExecutor) operationExecutor).getNextOperationExecutor());
-        return null;
     }
 
     public OperationActionRegistry getOperationActionRegistry() {
