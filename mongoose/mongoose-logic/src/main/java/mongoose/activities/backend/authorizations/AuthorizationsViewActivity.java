867f6fa444e0bf2587366340a0021c78ad3f8d80
Bruno Salmon <dev.salmonb@gmail.com>
2018-08-17 18:29:05 +0100

    Added some fields to display in AuthorizationsViewActivity

--- mongoose/mongoose-logic/src/main/java/mongoose/activities/backend/authorizations/AuthorizationsViewActivity.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/backend/authorizations/AuthorizationsViewActivity.java
@@ -3,11 +3,13 @@ package mongoose.activities.backend.authorizations;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.Node;
+import javafx.scene.layout.Pane;
 import mongoose.services.authn.MongooseUserPrincipal;
 import naga.framework.activity.base.elementals.view.impl.ViewDomainActivityBase;
 import naga.framework.orm.entity.Entities;
 import naga.framework.orm.entity.Entity;
 import naga.framework.ui.filter.ReactiveExpressionFilterFactoryMixin;
+import naga.framework.ui.graphic.controls.sheet.PropertySheet;
 import naga.framework.ui.layouts.FlexBox;
 import naga.fxdata.control.DataGrid;
 
@@ -16,6 +18,9 @@ import naga.fxdata.control.DataGrid;
  */
 class AuthorizationsViewActivity extends ViewDomainActivityBase
         implements ReactiveExpressionFilterFactoryMixin {
+
+    private final String manageeColumns = "[{label: 'Managee', expression: 'active,user.genderIcon,user.firstName,user.lastName'}]";
+    private final String assignmentColumns = "active,operation,rule,activityState";
 
     private DataGrid usersDataGrid = new DataGrid();
     private DataGrid assignmentsDataGrid = new DataGrid();
@@ -33,16 +38,16 @@ class AuthorizationsViewActivity extends ViewDomainActivityBase
     protected void startLogic() {
         createReactiveExpressionFilter("{class: 'AuthorizationManagement'}")
                 .combine(userPrincipalProperty(), principal -> "{where: 'manager = " + (principal instanceof MongooseUserPrincipal ? ((MongooseUserPrincipal) principal).getUserPersonId() : null) + "'}")
-                .setExpressionColumns("[{label: 'Managee', expression: 'user.<ident>'}]")
+                .setExpressionColumns(manageeColumns)
                 .displayResultInto(usersDataGrid.displayResultProperty())
                 .setSelectedEntityHandler(usersDataGrid.displaySelectionProperty(), selectedManagementProperty::setValue)
                 .start();
 
         createReactiveExpressionFilter("{class: 'AuthorizationAssignment'}")
                 .combine(selectedManagementProperty, management -> "{where: 'management = " + Entities.getPrimaryKey(management) + "'}")
-                .setExpressionColumns("active")
+                .setExpressionColumns(assignmentColumns)
                 .displayResultInto(assignmentsDataGrid.displayResultProperty())
-                //.setSelectedEntityHandler(pm.genericDisplaySelectionProperty(), organization -> new RouteToEventsRequest(organization, getHistory()).execute() )
+                .setSelectedEntityHandler(assignmentsDataGrid.displaySelectionProperty(), assignment -> PropertySheet.editEntity(assignment, assignmentColumns, (Pane) getNode()))
                 .start();
     }
 }
