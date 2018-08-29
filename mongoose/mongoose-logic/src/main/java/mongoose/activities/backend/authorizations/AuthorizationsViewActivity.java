a2f54a980a237459061b1bce865cc6ebf0729e69
Bruno Salmon <dev.salmonb@gmail.com>
2018-08-29 17:39:42 +0100

    Made the editing property sheet shown on assignments data grid double-click in authorizations activity

--- mongoose/mongoose-logic/src/main/java/mongoose/activities/backend/authorizations/AuthorizationsViewActivity.java
+++ mongoose/mongoose-logic/src/main/java/mongoose/activities/backend/authorizations/AuthorizationsViewActivity.java
@@ -8,6 +8,7 @@ import mongoose.services.authn.MongooseUserPrincipal;
 import naga.framework.activity.base.elementals.view.impl.ViewDomainActivityBase;
 import naga.framework.orm.entity.Entities;
 import naga.framework.orm.entity.Entity;
+import naga.framework.ui.filter.ReactiveExpressionFilter;
 import naga.framework.ui.filter.ReactiveExpressionFilterFactoryMixin;
 import naga.framework.ui.graphic.controls.sheet.PropertySheet;
 import naga.framework.ui.layouts.FlexBox;
@@ -19,19 +20,23 @@ import naga.fxdata.control.DataGrid;
 final class AuthorizationsViewActivity extends ViewDomainActivityBase
         implements ReactiveExpressionFilterFactoryMixin {
 
-    private final String manageeColumns = "[{label: 'Managee', expression: 'active,user.genderIcon,user.firstName,user.lastName'}]";
-    private final String assignmentColumns = "active,operation,rule,activityState";
+    private final String manageeColumns = "[{label: 'Managee', expression: `active,user.genderIcon,user.firstName,user.lastName`}]";
+    private final String assignmentColumns = "[`active`,`operation`,{expression: `rule`, foreignFields: null, foreignSearchCondition: null, foreignCondition: null},`activityState`]";
 
     private DataGrid usersDataGrid = new DataGrid();
     private DataGrid assignmentsDataGrid = new DataGrid();
 
     private final ObjectProperty<Entity> selectedManagementProperty = new SimpleObjectProperty<>();
 
+    private ReactiveExpressionFilter<Entity> assignmentFilter;
+
     @Override
     public Node buildUi() {
-        FlexBox flexBox = new FlexBox();
-        flexBox.getChildren().setAll(usersDataGrid, assignmentsDataGrid);
-        return flexBox;
+        assignmentsDataGrid.setOnMouseClicked(e -> {
+            if (e.getClickCount() == 2)
+                PropertySheet.editEntity(assignmentFilter.getSelectedEntity(), assignmentColumns, (Pane) getNode());
+        });
+        return new FlexBox(usersDataGrid, assignmentsDataGrid);
     }
 
 
@@ -43,11 +48,12 @@ final class AuthorizationsViewActivity extends ViewDomainActivityBase
                 .setSelectedEntityHandler(usersDataGrid.displaySelectionProperty(), selectedManagementProperty::setValue)
                 .start();
 
-        createReactiveExpressionFilter("{class: 'AuthorizationAssignment', orderBy: 'id'}")
+        assignmentFilter = createReactiveExpressionFilter("{class: 'AuthorizationAssignment', orderBy: 'id'}")
                 .combine(selectedManagementProperty, management -> "{where: 'management = " + Entities.getPrimaryKey(management) + "'}")
                 .setExpressionColumns(assignmentColumns)
                 .displayResultInto(assignmentsDataGrid.displayResultProperty())
-                .setSelectedEntityHandler(assignmentsDataGrid.displaySelectionProperty(), assignment -> PropertySheet.editEntity(assignment, assignmentColumns, (Pane) getNode()))
+                .setDisplaySelectionProperty(assignmentsDataGrid.displaySelectionProperty())
+                //.setSelectedEntityHandler(assignmentsDataGrid.displaySelectionProperty(), assignment -> PropertySheet.editEntity(assignment, assignmentColumns, (Pane) getNode()))
                 .start();
     }
 }
