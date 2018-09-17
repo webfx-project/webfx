b9d14352dee15f648601b6f27e21f64ab0ef570d
Bruno Salmon <dev.salmonb@gmail.com>
2018-09-17 20:15:36 +0100

    Replaced handwritten gwt ServiceLoader with generated ServiceLoader (through ServiceLoaderGenerator tool class)

--- mongoose/mongoose-backend/mongoose-backend-fxkit-gwt/src/main/resources/super/java/util/ServiceLoader.java
+++ mongoose/mongoose-backend/mongoose-backend-fxkit-gwt/src/main/resources/super/java/util/ServiceLoader.java
@@ -1,16 +1,49 @@
-/**
- * ServiceLoader GWT implementation
- *
- * @author Bruno Salmon
- */
-
 package java.util;
 
-import mongooses.web.activities.sharedends.MongooseWebApplicationModuleInitializer;
+import java.util.Iterator;
+import webfx.platforms.core.util.function.Factory;
 
-public abstract class ServiceLoader<S> implements Iterable<S> {
+public class ServiceLoader<S> implements Iterable<S> {
 
     public static <S> ServiceLoader<S> load(Class<S> serviceClass) {
-        return new GwtMongooseBackendServiceLoader().load(serviceClass);
+        if (serviceClass.equals(webfx.platforms.core.services.storage.spi.LocalStorageProvider.class)) return new ServiceLoader<S>(webfx.platform.gwt.services.storage.GwtLocalStorageProviderImpl::new);
+        if (serviceClass.equals(webfx.platforms.core.services.storage.spi.SessionStorageProvider.class)) return new ServiceLoader<S>(webfx.platform.gwt.services.storage.GwtSessionStorageProviderImpl::new);
+        if (serviceClass.equals(webfx.platforms.core.services.windowhistory.spi.WindowHistoryProvider.class)) return new ServiceLoader<S>(webfx.platforms.web.services.windowhistory.WebWindowHistoryProvider::new);
+        if (serviceClass.equals(webfx.platforms.core.services.resource.spi.ResourceServiceProvider.class)) return new ServiceLoader<S>(webfx.platform.gwt.services.resource.GwtResourceServiceProviderImpl::new);
+        if (serviceClass.equals(webfx.fxkits.core.spi.FxKitProvider.class)) return new ServiceLoader<S>(webfx.fxkit.gwt.GwtFxKitProvider::new);
+        if (serviceClass.equals(webfx.platforms.core.services.appcontainer.spi.ApplicationContainerProvider.class)) return new ServiceLoader<S>(webfx.platform.gwt.services.appcontainer.GwtApplicationContainerProvider::new);
+        if (serviceClass.equals(webfx.platforms.core.services.appcontainer.spi.ApplicationModuleInitializer.class)) return new ServiceLoader<S>(mongooses.web.activities.sharedends.MongooseWebApplicationModuleInitializer::new, mongooses.backend.MongooseBackendApplicationModuleInitializer::new, webfx.platforms.core.services.update.UpdateModuleInitializer::new, webfx.fxkits.core.FxKitModuleInitializer::new, webfx.platforms.core.services.buscall.BusCallModuleInitializer::new, webfx.platforms.core.services.query.QueryModuleInitializer::new, webfx.platforms.core.services.querypush.QueryPushModuleInitializer::new);
+        if (serviceClass.equals(webfx.platforms.core.services.shutdown.spi.ShutdownProvider.class)) return new ServiceLoader<S>(webfx.platform.gwt.services.shutdown.GwtShutdownProviderImpl::new);
+        if (serviceClass.equals(webfx.platforms.core.services.log.spi.LoggerProvider.class)) return new ServiceLoader<S>(webfx.platform.gwt.services.log.GwtLoggerProviderImpl::new);
+        if (serviceClass.equals(webfx.platforms.core.util.numbers.spi.NumbersProvider.class)) return new ServiceLoader<S>(webfx.platforms.core.util.numbers.providers.StandardNumbersProviderImpl::new);
+        if (serviceClass.equals(webfx.platforms.core.services.bus.spi.BusServiceProvider.class)) return new ServiceLoader<S>(webfx.platforms.web.services.clientbus.WebClientBusServiceProvider::new, webfx.platforms.core.services.bus.client.ClientBusServiceProviderImpl::new);
+        if (serviceClass.equals(webfx.platforms.core.services.windowlocation.spi.WindowLocationProvider.class)) return new ServiceLoader<S>(webfx.platform.gwt.services.windowlocation.GwtWindowLocationProvider::new);
+        if (serviceClass.equals(webfx.platforms.core.services.json.spi.JsonProvider.class)) return new ServiceLoader<S>(webfx.platform.gwt.services.json.GwtJsonObject::create);
+        if (serviceClass.equals(webfx.platforms.core.services.uischeduler.spi.UiSchedulerProvider.class)) return new ServiceLoader<S>(webfx.platform.gwt.services.scheduler.GwtSchedulerProviderImpl::new);
+        if (serviceClass.equals(webfx.platforms.core.services.scheduler.spi.SchedulerProvider.class)) return new ServiceLoader<S>(webfx.platform.gwt.services.scheduler.GwtSchedulerProviderImpl::new);
+        if (serviceClass.equals(webfx.platforms.web.services.windowhistory.JsWindowHistory.class)) return new ServiceLoader<S>(webfx.platform.gwt.services.windowhistory.GwtJsWindowHistory::new);
+        if (serviceClass.equals(webfx.platforms.core.services.websocket.spi.WebSocketServiceProvider.class)) return new ServiceLoader<S>(webfx.platform.gwt.services.websocket.GwtWebSocketServiceProviderImpl::new);
+        return new ServiceLoader<S>();
+    }
+
+    private final Factory[] factories;
+
+    public ServiceLoader(Factory... factories) {
+        this.factories = factories;
+    }
+
+    public Iterator<S> iterator() {
+        return new Iterator<S>() {
+            int index = 0;
+            @Override
+            public boolean hasNext() {
+                return index < factories.length;
+            }
+
+            @Override
+            public S next() {
+                return (S) factories[index++].create();
+            }
+        };
     }
 }
---
+++ webfx-platform/webfx-platform-gwt/webfx-platform-gwt-super/src/main/java/webfx/platform/gwt/tools/ServiceLoaderGenerator.java
@@ -0,0 +1,178 @@
+package webfx.platform.gwt.tools;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+/**
+ * @author Bruno Salmon
+ */
+public class ServiceLoaderGenerator {
+
+    private static String template = "package java.util;\n" +
+            "\n" +
+            "import java.util.Iterator;\n" +
+            "import webfx.platforms.core.util.function.Factory;\n" +
+            "\n" +
+            "public class ServiceLoader<S> implements Iterable<S> {\n" +
+            "\n" +
+            "    public static <S> ServiceLoader<S> load(Class<S> serviceClass) {\n" +
+            "${generatedCode}" +
+            "        return new ServiceLoader<S>();\n" +
+            "    }\n" +
+            "\n" +
+            "    private final Factory[] factories;\n" +
+            "\n" +
+            "    public ServiceLoader(Factory... factories) {\n" +
+            "        this.factories = factories;\n" +
+            "    }\n" +
+            "\n" +
+            "    public Iterator<S> iterator() {\n" +
+            "        return new Iterator<S>() {\n" +
+            "            int index = 0;\n" +
+            "            @Override\n" +
+            "            public boolean hasNext() {\n" +
+            "                return index < factories.length;\n" +
+            "            }\n" +
+            "\n" +
+            "            @Override\n" +
+            "            public S next() {\n" +
+            "                return (S) factories[index++].create();\n" +
+            "            }\n" +
+            "        };\n" +
+            "    }\n" +
+            "}";
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        for (String spiClassName: loadProviderClassNames("spi")) {
+            List<String> providerClassNames = loadProviderClassNames(spiClassName);
+            if (providerClassNames.isEmpty())
+                System.out.println("WARNING: No provider found for " + spiClassName);
+            else {
+                sb.append("        if (serviceClass.equals(").append(spiClassName).append(".class)) return new ServiceLoader<S>(");
+                for (String providerClassName : providerClassNames) {
+                    if (providerClassName != providerClassNames.get(0))
+                        sb.append(", ");
+                    sb.append(providerClassName).append(providerClassName.equals("webfx.platform.gwt.services.json.GwtJsonObject") ? "::create" : "::new");
+                }
+                sb.append(");\n");
+            }
+        }
+        String serviceLoaderJavaCode = template.replace("${generatedCode}", sb);
+        String serviceLoaderPath = System.getProperty("user.dir") + "/src/main/resources/super/java/util/ServiceLoader.java";
+        System.out.println("Writing " + serviceLoaderPath);
+        try {
+            writeTextFile(serviceLoaderPath, serviceLoaderJavaCode);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<String> loadProviderClassNames(String service) {
+        List<String> providers = new ArrayList<>();
+        try {
+            String prefix = service.equals("spi") ? "META-INF/webfx/" : "META-INF/services/";
+            String fullName = prefix + service;
+            Enumeration<URL> configs = ClassLoader.getSystemResources(fullName);
+            while (configs.hasMoreElements()) {
+                providers.addAll(parse(service, configs.nextElement()));
+            }
+        } catch (IOException x) {
+            fail(service, "Error locating configuration files", x);
+        }
+        return providers;
+    }
+
+    private static List<String> parse(String service, URL u)
+            throws ServiceConfigurationError
+    {
+        InputStream in = null;
+        BufferedReader r = null;
+        ArrayList<String> names = new ArrayList<>();
+        try {
+            in = u.openStream();
+            r = new BufferedReader(new InputStreamReader(in, "utf-8"));
+            int lc = 1;
+            while ((lc = parseLine(service, u, r, lc, names)) >= 0);
+        } catch (IOException x) {
+            fail(service, "Error reading configuration file", x);
+        } finally {
+            try {
+                if (r != null) r.close();
+                if (in != null) in.close();
+            } catch (IOException y) {
+                fail(service, "Error closing configuration file", y);
+            }
+        }
+        return names;
+    }
+
+
+    // Parse a single line from the given configuration file, adding the name
+    // on the line to the names list.
+    //
+    private static int parseLine(String service, URL u, BufferedReader r, int lc,
+                          List<String> names)
+            throws IOException, ServiceConfigurationError
+    {
+        String ln = r.readLine();
+        if (ln == null) {
+            return -1;
+        }
+        int ci = ln.indexOf('#');
+        if (ci >= 0) ln = ln.substring(0, ci);
+        ln = ln.trim();
+        int n = ln.length();
+        if (n != 0) {
+            if ((ln.indexOf(' ') >= 0) || (ln.indexOf('\t') >= 0))
+                fail(service, u, lc, "Illegal configuration-file syntax");
+            int cp = ln.codePointAt(0);
+            if (!Character.isJavaIdentifierStart(cp))
+                fail(service, u, lc, "Illegal provider-class name: " + ln);
+            for (int i = Character.charCount(cp); i < n; i += Character.charCount(cp)) {
+                cp = ln.codePointAt(i);
+                if (!Character.isJavaIdentifierPart(cp) && (cp != '.'))
+                    fail(service, u, lc, "Illegal provider-class name: " + ln);
+            }
+            if (/*!providers.containsKey(ln) &&*/ !names.contains(ln))
+                names.add(ln);
+        }
+        return lc + 1;
+    }
+
+    private static void fail(String service, URL u, int line, String msg)
+            throws ServiceConfigurationError
+    {
+        fail(service, u + ":" + line + ": " + msg);
+    }
+
+    private static void fail(String service, String msg)
+            throws ServiceConfigurationError
+    {
+        throw new ServiceConfigurationError(service + ": " + msg);
+    }
+
+    private static void fail(String service, String msg, Throwable cause)
+            throws ServiceConfigurationError
+    {
+        throw new ServiceConfigurationError(service + ": " + msg,
+                cause);
+    }
+
+    private static Path writeTextFile(String pathName, String content) throws IOException {
+        Path path = Paths.get(pathName);
+        Files.createDirectories(path.getParent()); // Creating all necessary directories
+        //Files.write(path, lzJson.getBytes("UTF-8")); // Writing the file
+        BufferedWriter writer = Files.newBufferedWriter(path, Charset.forName("UTF-8"));
+        writer.write(content);
+        writer.flush();
+        writer.close();
+        return path;
+    }
+
+}
