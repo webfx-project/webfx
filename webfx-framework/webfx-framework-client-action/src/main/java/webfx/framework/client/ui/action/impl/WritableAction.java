1177561031589e674f66c91855b796fcee446272
Bruno Salmon <dev.salmonb@gmail.com>
2018-10-16 18:32:05 +0100

    Added WritableAction constructor with action parameter

--- webfx-framework/webfx-framework-client-action/src/main/java/webfx/framework/client/ui/action/impl/WritableAction.java
+++ webfx-framework/webfx-framework-client-action/src/main/java/webfx/framework/client/ui/action/impl/WritableAction.java
@@ -7,6 +7,8 @@ import javafx.beans.value.ObservableStringValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
+import webfx.framework.client.ui.action.Action;
+import webfx.platform.shared.util.Arrays;
 
 /**
  * An writable action where properties (text, graphic, disabled, visible) can be set later (ie after constructor call)
@@ -15,6 +17,10 @@ import javafx.scene.Node;
  * @author Bruno Salmon
  */
 public class WritableAction extends ReadOnlyAction {
+
+    public WritableAction(Action action, String... writablePropertyNames) {
+        this(createStringProperty(action.textProperty(), "text", writablePropertyNames), createObjectProperty(action.graphicProperty(), "graphic", writablePropertyNames), createBooleanProperty(action.disabledProperty(), "disabled", writablePropertyNames), createBooleanProperty(action.visibleProperty(), "visible", writablePropertyNames), action);
+    }
 
     public WritableAction(EventHandler<ActionEvent> actionHandler) {
         this(new SimpleStringProperty(), new SimpleObjectProperty<>(), new SimpleBooleanProperty(true /* disabled until it is bound */), new SimpleBooleanProperty(false /* invisible until it is bound */), actionHandler);
@@ -54,5 +60,52 @@ public class WritableAction extends ReadOnlyAction {
 
     public void setVisible(boolean visible) {
         writableVisibleProperty().set(visible);
+    }
+
+    private static StringProperty createStringProperty(ObservableStringValue readOnlyProperty, String propertyName, String... writablePropertyNames) {
+        if (readOnlyProperty instanceof StringProperty)
+            return (StringProperty) readOnlyProperty;
+        SimpleStringProperty writableProperty = new SimpleStringProperty() {
+            @Override
+            public void set(String newValue) {
+                unbindPropertyIfWritable(this, propertyName, writablePropertyNames);
+                super.set(newValue);
+            }
+        };
+        writableProperty.bind(readOnlyProperty);
+        return writableProperty;
+    }
+
+    private static <T> ObjectProperty<T> createObjectProperty(ObservableObjectValue readOnlyProperty, String propertyName, String... writablePropertyNames) {
+        if (readOnlyProperty instanceof ObjectProperty)
+            return (ObjectProperty<T>) readOnlyProperty;
+        SimpleObjectProperty<T> writableProperty = new SimpleObjectProperty<T>() {
+            @Override
+            public void set(T newValue) {
+                unbindPropertyIfWritable(this, propertyName, writablePropertyNames);
+                super.set(newValue);
+            }
+        };
+        writableProperty.bind(readOnlyProperty);
+        return writableProperty;
+    }
+
+    private static BooleanProperty createBooleanProperty(ObservableBooleanValue readOnlyProperty, String propertyName, String... writablePropertyNames) {
+        if (readOnlyProperty instanceof BooleanProperty)
+            return (BooleanProperty) readOnlyProperty;
+        SimpleBooleanProperty writableProperty = new SimpleBooleanProperty() {
+            @Override
+            public void set(boolean newValue) {
+                unbindPropertyIfWritable(this, propertyName, writablePropertyNames);
+                super.set(newValue);
+            }
+        };
+        writableProperty.bind(readOnlyProperty);
+        return writableProperty;
+    }
+
+    private static void unbindPropertyIfWritable(Property property, String propertyName, String... writablePropertyNames) {
+        if (Arrays.contains(writablePropertyNames, propertyName) || Arrays.contains(writablePropertyNames, "*"))
+            property.unbind();
     }
 }
