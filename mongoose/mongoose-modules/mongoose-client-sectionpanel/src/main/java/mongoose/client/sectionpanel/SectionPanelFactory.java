7430a03ba73878aa585781cb6f231a81c6f48feb
Bruno Salmon <dev.salmonb@gmail.com>
2018-10-18 20:07:14 +0100

    Cleaned SectionPanelFactory class

--- mongoose/mongoose-modules/mongoose-client-sectionpanel/src/main/java/mongoose/client/sectionpanel/SectionPanelFactory.java
+++ mongoose/mongoose-modules/mongoose-client-sectionpanel/src/main/java/mongoose/client/sectionpanel/SectionPanelFactory.java
@@ -3,10 +3,10 @@ package mongoose.client.sectionpanel;
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import mongoose.client.bookingcalendar.BookingCalendar;
 import mongoose.client.icons.MongooseIcons;
@@ -62,36 +62,25 @@ public final class SectionPanelFactory {
     }
 
     public static BorderPane createSectionPanel(Node... headerNodes) {
-        BorderPane panel = new BorderPane();
-        panel.getStyleClass().add("section-panel");
-        panel.setBorder(BorderUtil.newBorder(Color.grayRgb(0x0d), 5, 1));
-        panel.setBackground(BackgroundUtil.WHITE_BACKGROUND);
-        HBox hBox = (HBox) NodeCollatorRegistry.hBoxCollator().collateNodes(headerNodes);
-        hBox.setBackground(BackgroundUtil.newVerticalLinearGradientBackground("0xF0F0F0", "0xE0E0E0",5));
-        hBox.setMinHeight(40d);
-        hBox.setPadding(new Insets(0, 10, 0 , 10));
-        panel.setTop(hBox);
-        return panel;
+        BorderPane sectionPanel = new BorderPane();
+        sectionPanel.getStyleClass().add("section-panel");
+        sectionPanel.setBorder(BorderUtil.newBorder(Color.grayRgb(0x0d), 5, 1));
+        sectionPanel.setBackground(BackgroundUtil.WHITE_BACKGROUND);
+        return setSectionPanelHeaderNodes(sectionPanel, headerNodes);
     }
 
-    public static Button createBookButton() {
-        return createToBottomGradientButton("#7fd504", "#2a8236");
+    public static BorderPane setSectionPanelHeaderNodes(BorderPane sectionPanel, Node... headerNodes) {
+        return setSectionPanelHeaderNode(sectionPanel, NodeCollatorRegistry.hBoxCollator().collateNodes(headerNodes));
     }
 
-    public static Button createSoldoutButton() {
-        return createToBottomGradientButton("#e92c04", "#853416");
-    }
-
-    public static Button createToBottomGradientButton(String topWebColor, String bottomWebColor) {
-        return createButton(topWebColor + " 0%, " + bottomWebColor + " 100%", bottomWebColor);
-    }
-
-    public static Button createButton(String linearGradient, String borderWebColor) {
-        Button button = new Button();
-        button.setBackground(BackgroundUtil.newLinearGradientBackground(linearGradient, 5));
-        button.setBorder(BorderUtil.newBorder(Color.web(borderWebColor), 5));
-        button.setTextFill(Color.WHITE);
-        button.setStyle("-fx-text-fill: white;");
-        return button;
+    public static BorderPane setSectionPanelHeaderNode(BorderPane sectionPanel, Node headerNode) {
+        if (headerNode instanceof Region) {
+            Region headerRegion = (Region) headerNode;
+            headerRegion.setBackground(BackgroundUtil.newVerticalLinearGradientBackground("0xF0F0F0", "0xE0E0E0",5));
+            headerRegion.setMinHeight(40d);
+            headerRegion.setPadding(new Insets(0, 10, 0 , 10));
+        }
+        sectionPanel.setTop(headerNode);
+        return sectionPanel;
     }
 }
