2dcddcfbd9065fb6a52bb91edc314d5ca22601f9
Bruno Salmon <dev.salmonb@gmail.com>
2019-02-18 11:59:45 +0000

    Added unsized flag to DelegatingSpliterator

--- webfx-tools/webfx-tool-buildtool/src/main/java/webfx/tool/buildtool/util/streamable/impl/DelegatingSpliterator.java
+++ webfx-tools/webfx-tool-buildtool/src/main/java/webfx/tool/buildtool/util/streamable/impl/DelegatingSpliterator.java
@@ -10,9 +10,15 @@ import java.util.function.Consumer;
 abstract class DelegatingSpliterator<T> implements Spliterator<T> {
 
     private Spliterator<T> delegate;
+    private final boolean unsized; // true to force characteristics to unsized (ex: FilteredSpliterator)
 
     DelegatingSpliterator(Spliterator<T> delegate) {
+        this(delegate, false);
+    }
+
+    DelegatingSpliterator(Spliterator<T> delegate, boolean unsized) {
         this.delegate = delegate;
+        this.unsized = unsized;
     }
 
     private void onDelegateFullyTraversed() { // Called when the delegate spliterator is finished (is fully traversed)
@@ -37,21 +43,6 @@ abstract class DelegatingSpliterator<T> implements Spliterator<T> {
     }
 
     @Override
-    public Spliterator<T> trySplit() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public long estimateSize() {
-        return hasDelegateBeenFullyTraversed() ? 0 : delegate.estimateSize();
-    }
-
-    @Override
-    public int characteristics() {
-        return hasDelegateBeenFullyTraversed() ? 0 : delegate.characteristics();
-    }
-
-    @Override
     public void forEachRemaining(Consumer<? super T> action) {
         if (!hasDelegateBeenFullyTraversed()) {
             delegate.forEachRemaining(action);
@@ -60,13 +51,36 @@ abstract class DelegatingSpliterator<T> implements Spliterator<T> {
     }
 
     @Override
-    public long getExactSizeIfKnown() {
-        return hasDelegateBeenFullyTraversed() ? 0 : delegate.getExactSizeIfKnown();
+    public int characteristics() {
+        return hasDelegateBeenFullyTraversed() ? 0 : characteristicsConsideringUnsized(delegate.characteristics());
+    }
+
+    private int characteristicsConsideringUnsized(int characteristics) {
+        return unsized ? characteristics & ~SIZED : characteristics;
     }
 
     @Override
     public boolean hasCharacteristics(int characteristics) {
-        return !hasDelegateBeenFullyTraversed() && delegate.hasCharacteristics(characteristics);
+        return !hasDelegateBeenFullyTraversed() && (!unsized || (characteristics & SIZED) == 0) && delegate.hasCharacteristics(characteristics);
+    }
+
+    @Override
+    public long estimateSize() {
+        return hasDelegateBeenFullyTraversed() ? 0 : sizeConsideringUnsized(delegate.estimateSize());
+    }
+
+    private long sizeConsideringUnsized(long size) {
+        return unsized && size != 0 ? -1 : size;
+    }
+
+    @Override
+    public long getExactSizeIfKnown() {
+        return hasDelegateBeenFullyTraversed() ? 0 : sizeConsideringUnsized(delegate.getExactSizeIfKnown());
+    }
+
+    @Override
+    public Spliterator<T> trySplit() {
+        throw new UnsupportedOperationException();
     }
 
     @Override
