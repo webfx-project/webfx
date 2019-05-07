e0ba3236170b6311dc79807158d5a67055aa2d0c
Bruno Salmon <dev.salmonb@gmail.com>
2019-05-07 14:56:22 +0100

    Fixed SvgPathPeer: was not considering the difference between SVG and JavaFx coordinate systems

--- webfx-fxkit/webfx-fxkit-gwt/src/main/java/webfx/fxkit/gwt/mapper/svg/peer/javafxgraphics/SvgPathPeer.java
+++ webfx-fxkit/webfx-fxkit-gwt/src/main/java/webfx/fxkit/gwt/mapper/svg/peer/javafxgraphics/SvgPathPeer.java
@@ -1,13 +1,18 @@
 package webfx.fxkit.gwt.mapper.svg.peer.javafxgraphics;
 
 import elemental2.dom.Element;
+import elemental2.svg.SVGPathElement;
+import elemental2.svg.SVGRect;
 import javafx.collections.ListChangeListener;
 import javafx.scene.shape.*;
+import javafx.scene.transform.Transform;
+import javafx.scene.transform.Translate;
 import webfx.fxkit.gwt.mapper.util.SvgUtil;
 import webfx.fxkit.mapper.spi.impl.peer.javafxgraphics.PathPeerBase;
 import webfx.fxkit.mapper.spi.impl.peer.javafxgraphics.PathPeerMixin;
 import webfx.platform.shared.util.collection.Collections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -35,6 +40,7 @@ public final class SvgPathPeer
     @Override
     public void updateElements(List<PathElement> elements, ListChangeListener.Change<PathElement> change) {
         setElementAttribute("d", toSvgPathData(elements));
+        updateLocalToParentTransforms(getNode().localToParentTransforms());
     }
 
     private String toSvgPathData(List<PathElement> elements) {
@@ -58,4 +64,17 @@ public final class SvgPathPeer
         } else if (pathElement instanceof ClosePath)
             sb.append('Z');
     }
+
+    @Override
+    public void updateLocalToParentTransforms(List<Transform> localToParentTransforms) {
+        // Before transformation, the top left corner of the container refers to the axis origin (0,0) in SVG whereas it
+        // refers to the top left corner of the path in JavaFx. So to emulate the same behaviour as JavaFx, we need to
+        // add a translation of the path so it appears on the top left corner.
+        List<Transform> svgTransforms = new ArrayList<>(localToParentTransforms.size() + 1);
+        SVGRect bBox = ((SVGPathElement) getElement()).getBBox();
+        svgTransforms.add(new Translate(-bBox.x, -bBox.y));
+        svgTransforms.addAll(localToParentTransforms);
+        super.updateLocalToParentTransforms(svgTransforms);
+    }
+
 }
