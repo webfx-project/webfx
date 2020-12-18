e15605fbd675f1eab404a3180304c9fe224f2006
Bruno Salmon <dev.salmonb@gmail.com>
2020-12-18 00:05:47 +0000

    Added missing Font static factory method

--- webfx-kit/webfx-kit-javafxgraphics-emul/src/main/java/javafx/scene/text/Font.java
+++ webfx-kit/webfx-kit-javafxgraphics-emul/src/main/java/javafx/scene/text/Font.java
@@ -70,6 +70,10 @@ public class Font {
         return new Font(family, weight, posture, size);
     }
 
+    public static Font font(String family, FontPosture posture, double size) {
+        return font(family, null, posture, size);
+    }
+
     public static Font font(String family, FontWeight weight, double size) {
         return font(family, weight, null, size);
     }
