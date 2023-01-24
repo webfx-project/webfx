1ebdcda9b450e6a7c94f6e0028c4af01ff2c1928
Bruno Salmon <dev.salmonb@gmail.com>
2023-01-24 13:37:30 +0000

    Added fill parameter in DeviceSceneUtil

--- webfx-kit/webfx-kit-util-scene/src/main/java/dev/webfx/kit/util/scene/DeviceSceneUtil.java
+++ webfx-kit/webfx-kit-util-scene/src/main/java/dev/webfx/kit/util/scene/DeviceSceneUtil.java
@@ -1,9 +1,11 @@
 package dev.webfx.kit.util.scene;
 
+import dev.webfx.platform.os.OSFamily;
 import dev.webfx.platform.os.OperatingSystem;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Paint;
 import javafx.stage.Screen;
 
 /**
@@ -12,18 +14,41 @@ import javafx.stage.Screen;
 public final class DeviceSceneUtil {
 
     public static Scene newScene(Parent root, double width, double height) {
-        switch (OperatingSystem.getOSFamily()) {
-            case ANDROID:
-            case IOS:
-                return newVisualBoundsScreenScene(root); // Also ok if actually runs in the browser as the scene dimensions will be ignored
-            default:
-                return new Scene(root, width, height);
-        }
+        return newScene(root, getFinalSceneBounds(width, height));
     }
 
-    public static Scene newVisualBoundsScreenScene(Parent root) {
-        Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
-        return new Scene(root, visualBounds.getWidth(), visualBounds.getHeight());
+    public static Scene newScene(Parent root, double width, double height, Paint fill) {
+        Scene scene = newScene(root, width, height);
+        scene.setFill(fill); // Recommended to set a fill on the scene (ex: black) otherwise the Gluon version shows a white vertical line on the right side
+        return scene;
+    }
+
+    public static Scene newScreenVisualBoundsScene(Parent root) {
+        return newScene(root, getScreenVisualBounds());
+    }
+
+    public static Scene newScreenVisualBoundsScene(Parent root, Paint fill) {
+        Scene scene = newScreenVisualBoundsScene(root);
+        scene.setFill(fill); // Recommended to set a fill on the scene (ex: black) otherwise the Gluon version shows a white vertical line on the right side
+        return scene;
+    }
+
+    public static Rectangle2D getFinalSceneBounds(double width, double height) {
+        return isAndroidOrIOS() ? getScreenVisualBounds() // Also ok if actually runs in the browser as the scene dimensions will be ignored
+                :  new Rectangle2D(0, 0, width, height);
+    }
+
+    private static Rectangle2D getScreenVisualBounds() {
+        return Screen.getPrimary().getVisualBounds();
+    }
+
+    private static Scene newScene(Parent root, Rectangle2D finalSceneBounds) {
+        return new Scene(root, finalSceneBounds.getWidth(), finalSceneBounds.getHeight());
+    }
+
+    private static boolean isAndroidOrIOS() {
+        OSFamily osFamily = OperatingSystem.getOSFamily();
+        return osFamily == OSFamily.ANDROID || osFamily == OSFamily.IOS;
     }
 
 }
