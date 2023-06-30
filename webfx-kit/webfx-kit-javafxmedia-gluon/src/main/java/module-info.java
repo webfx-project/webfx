dce1a87d9a1c018a1788db9912b2037954756b4a
Bruno Salmon <dev.salmonb@gmail.com>
2023-06-30 15:34:28 +0100

    Added JavaFX media emulation for Gluon (sound only)

---
+++ webfx-kit/webfx-kit-javafxmedia-gluon/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module javafx.media {
+
+    // Direct dependencies modules
+    requires webfx.platform.audio;
+
+    // Exported packages
+    exports javafx.scene.media;
+
+}
