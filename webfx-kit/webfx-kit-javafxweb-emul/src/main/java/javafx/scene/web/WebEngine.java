a91e9179a1d4097e8ba5f820bbc51cc9c7271c95
Bruno Salmon <dev.salmonb@gmail.com>
2023-09-28 09:41:41 +0100

    Added WebEngine.onError support

--- webfx-kit/webfx-kit-javafxweb-emul/src/main/java/javafx/scene/web/WebEngine.java
+++ webfx-kit/webfx-kit-javafxweb-emul/src/main/java/javafx/scene/web/WebEngine.java
@@ -1,11 +1,31 @@
 package javafx.scene.web;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.event.EventHandler;
+
 /**
  * @author Bruno Salmon
  */
 public class WebEngine {
 
     private final WebView webView;
+
+    private final ObjectProperty<EventHandler<WebErrorEvent>> onError =
+            new SimpleObjectProperty<>(this, "onError");
+
+    public final EventHandler<WebErrorEvent> getOnError() {
+        return onError.get();
+    }
+
+    public final void setOnError(EventHandler<WebErrorEvent> handler) {
+        onError.set(handler);
+    }
+
+    public final ObjectProperty<EventHandler<WebErrorEvent>> onErrorProperty() {
+        return onError;
+    }
+
 
     WebEngine(WebView webView) {
         this.webView = webView;
--- webfx-kit/webfx-kit-javafxweb-peers-gwt/src/main/java/dev/webfx/kit/mapper/peers/javafxweb/spi/gwt/HtmlWebViewPeer.java
+++ webfx-kit/webfx-kit-javafxweb-peers-gwt/src/main/java/dev/webfx/kit/mapper/peers/javafxweb/spi/gwt/HtmlWebViewPeer.java
@@ -3,48 +3,74 @@ package dev.webfx.kit.mapper.peers.javafxweb.spi.gwt;
 import dev.webfx.kit.mapper.peers.javafxgraphics.gwt.html.HtmlNodePeer;
 import dev.webfx.kit.mapper.peers.javafxgraphics.gwt.util.HtmlUtil;
 import elemental2.dom.CSSProperties;
-import elemental2.dom.Element;
-import elemental2.dom.HTMLElement;
 import elemental2.dom.HTMLIFrameElement;
+import javafx.event.EventHandler;
+import javafx.scene.web.WebErrorEvent;
 import javafx.scene.web.WebView;
 
 /**
  * @author Bruno Salmon
  */
 public class HtmlWebViewPeer
-        <E extends Element, N extends WebView, NB extends EmulWebViewPeerBase<N, NB, NM>, NM extends EmulWebViewPeerMixin<N, NB, NM>>
+        <N extends WebView, NB extends EmulWebViewPeerBase<N, NB, NM>, NM extends EmulWebViewPeerMixin<N, NB, NM>>
         extends HtmlNodePeer<N, NB, NM>
-        implements EmulWebViewPeerMixin<N, NB, NM>
-{
+        implements EmulWebViewPeerMixin<N, NB, NM> {
+
+    private final HTMLIFrameElement iFrame;
 
     public HtmlWebViewPeer() {
         this((NB) new EmulWebViewPeerBase(), HtmlUtil.createElement("iframe"));
     }
 
-    public HtmlWebViewPeer(NB base, HTMLElement element) {
-        super(base, element);
+    public HtmlWebViewPeer(NB base, HTMLIFrameElement iFrame) {
+        super(base, iFrame);
+        this.iFrame = iFrame;
+        iFrame.onerror = e -> {
+            reportError();
+            return null;
+        };
+    }
+
+    private void reportError() {
+        EventHandler<WebErrorEvent> onError = getNode().getEngine().getOnError();
+        if (onError != null)
+            onError.handle(new WebErrorEvent(this, WebErrorEvent.ANY, null, null));
     }
 
     @Override
     public void updateWidth(Number width) {
-        getElement().style.width = CSSProperties.WidthUnionType.of(toPx(width.doubleValue()));
+        iFrame.style.width = CSSProperties.WidthUnionType.of(toPx(width.doubleValue()));
     }
 
     @Override
     public void updateHeight(Number height) {
-        getElement().style.height = CSSProperties.HeightUnionType.of(toPx(height.doubleValue()));
+        iFrame.style.height = CSSProperties.HeightUnionType.of(toPx(height.doubleValue()));
     }
 
     @Override
     public void updateUrl(String url) {
-        HTMLIFrameElement iFrame = (HTMLIFrameElement) getElement();
-        if (url != null)
-            iFrame.src = url;
+        if (url != null) {
+            // iFrame.src = url; // <= commented because this doesn't report any network errors despite iFrame.onerror
+            // For a better error reporting, we fetch the url ourselves, and then inject the result into the iFrame
+            iFrame.contentWindow.fetch(url)
+                    .then(response -> {
+                        response.text().then(text -> {
+                            updateLoadContent(text);
+                            return null;
+                        }).catch_(error -> {
+                            reportError();
+                            return null;
+                        });
+                        return null;
+                    }).catch_(error -> {
+                        reportError();
+                        return null;
+                    });
+        }
     }
 
     @Override
     public void updateLoadContent(String content) {
-        HTMLIFrameElement iFrame = (HTMLIFrameElement) getElement();
         if (content != null)
             iFrame.srcdoc = content;
     }
