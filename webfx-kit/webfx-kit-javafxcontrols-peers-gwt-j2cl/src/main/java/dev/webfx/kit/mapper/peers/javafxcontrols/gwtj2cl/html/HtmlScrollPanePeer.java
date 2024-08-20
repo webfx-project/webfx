eb9d4b4f34f82319b381e82190c4e4eb911337b7
Bruno Salmon <dev.salmonb@gmail.com>
2024-08-20 22:46:09 +0100

    Added simple CSS mode for mobiles in HtmlScrollPanePeer (requires passive mode)

--- webfx-kit/webfx-kit-javafxcontrols-peers-gwt-j2cl/src/main/java/dev/webfx/kit/mapper/peers/javafxcontrols/gwtj2cl/html/HtmlScrollPanePeer.java
+++ webfx-kit/webfx-kit-javafxcontrols-peers-gwt-j2cl/src/main/java/dev/webfx/kit/mapper/peers/javafxcontrols/gwtj2cl/html/HtmlScrollPanePeer.java
@@ -4,8 +4,11 @@ import dev.webfx.kit.mapper.peers.javafxcontrols.base.ScrollPanePeerBase;
 import dev.webfx.kit.mapper.peers.javafxcontrols.base.ScrollPanePeerMixin;
 import dev.webfx.kit.mapper.peers.javafxgraphics.SceneRequester;
 import dev.webfx.kit.mapper.peers.javafxgraphics.gwtj2cl.html.HtmlRegionPeer;
+import dev.webfx.kit.mapper.peers.javafxgraphics.gwtj2cl.shared.HtmlSvgNodePeer;
 import dev.webfx.kit.mapper.peers.javafxgraphics.gwtj2cl.util.HtmlUtil;
 import dev.webfx.kit.util.properties.FXProperties;
+import dev.webfx.platform.os.OperatingSystem;
+import dev.webfx.platform.scheduler.Scheduled;
 import dev.webfx.platform.uischeduler.UiScheduler;
 import elemental2.dom.Element;
 import elemental2.dom.HTMLElement;
@@ -13,6 +16,7 @@ import javafx.geometry.BoundingBox;
 import javafx.geometry.Bounds;
 import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.TouchEvent;
 import jsinterop.base.Js;
 import jsinterop.base.JsPropertyMap;
 
@@ -20,10 +24,16 @@ import jsinterop.base.JsPropertyMap;
  * @author Bruno Salmon
  */
 public final class HtmlScrollPanePeer
-        <N extends ScrollPane, NB extends ScrollPanePeerBase<N, NB, NM>, NM extends ScrollPanePeerMixin<N, NB, NM>>
+    <N extends ScrollPane, NB extends ScrollPanePeerBase<N, NB, NM>, NM extends ScrollPanePeerMixin<N, NB, NM>>
 
-        extends HtmlRegionPeer<N, NB, NM>
-        implements ScrollPanePeerMixin<N, NB, NM> {
+    extends HtmlRegionPeer<N, NB, NM>
+    implements ScrollPanePeerMixin<N, NB, NM> {
+
+    private static final boolean USE_CSS = OperatingSystem.isMobile(); // much smoother experience on mobiles but requires passive mode
+    private static final boolean USE_PERFECT_SCROLLBAR = !USE_CSS; // the perfect scrollbars looks better on desktops than in CSS mode
+
+    private Scheduled cssScrollEndDetector;
+    private boolean cssScrollDetected;
 
     public HtmlScrollPanePeer() {
         this((NB) new ScrollPanePeerBase(), HtmlUtil.createElement("fx-scrollpane"));
@@ -36,17 +46,63 @@ public final class HtmlScrollPanePeer
     @Override
     public void bind(N node, SceneRequester sceneRequester) {
         super.bind(node, sceneRequester);
-        // Important: perfect scrollbar expect a standard HTML container (won't work with fx-scrollpane)
-        HTMLElement psContainer = HtmlUtil.setStyle(HtmlUtil.createDivElement(), "position: absolute; width: 100%; height: 100%");
-        setChildrenContainer(psContainer);
-        HtmlUtil.setChildren(getElement(), psContainer);
-        node.setOnChildrenLayout(HtmlScrollPanePeer.this::scheduleUpdate);
+        HTMLElement element = getElement();
+        if (USE_PERFECT_SCROLLBAR) {
+            // Important: perfect scrollbar expect a standard HTML container (won't work with fx-scrollpane)
+            HTMLElement psContainer = HtmlUtil.setStyle(HtmlUtil.createDivElement(), "position: absolute; width: 100%; height: 100%");
+            setChildrenContainer(psContainer);
+            HtmlUtil.setChildren(element, psContainer);
+        } else { // CSS mode which relies on the overflow-x & overflow-y values
+            // Important: the CSS mode will react to touch events only if the Scene touch events thar are passed to
+            // JavaFX originates from an event listener in passive mode, which is not the case by default with webfx,
+            // because the passive mode doesn't let us stop the propagation which is a feature that the application code
+            // requests when calling event.consume(). But it's very likely ok to disable this feature during a touch
+            // scroll to enjoy the smoothness of the scroll in passive mode on mobiles.
+            node.addEventHandler(TouchEvent.ANY, e -> {
+                // The purpose of this handler is to detect any touch event that targets the scroll pane while still in
+                // standard mode (non-passive). By chance, this detection happens just before it is passed to the scene,
+                // and it's the only and last opportunity to activate the passive mode. Without catching this event
+                // and switching to passive mode, the CSS overflow scrollbars wouldn't react to the touch events.
+                if (!HtmlSvgNodePeer.isSceneTouchPassiveMode()) {
+                    HtmlSvgNodePeer.setSceneTouchPassiveMode(true);
+                    // It's also important to detect the end of this touch scroll to go back to the standard mode
+                    rescheduleCssScrollEndDetector();
+                }
+            });
+            // We intercept the JS scroll events to update the ScrollPane position in JavaFX when the html one changes
+            element.addEventListener("scroll", e -> {
+                if (element.scrollTop != scrollTop)
+                    setScrollTop(element.scrollTop);
+                if (element.scrollLeft != scrollLeft)
+                    setScrollLeft(element.scrollLeft);
+                // Also if this happens during a touch scroll, we report the detection of the scroll and reschedule the
+                // css scroll end detector.
+                if (HtmlSvgNodePeer.isSceneTouchPassiveMode()) {
+                    cssScrollDetected = true;
+                    rescheduleCssScrollEndDetector();
+                }
+            });
+        }
+        node.setOnChildrenLayout(HtmlScrollPanePeer.this::scheduleUiUpdate);
         // The following listener is to reestablish the scroll position on scene change. For ex when the user 1) switches
         // to another page through UI routing and then 2) come back, this node is removed from the scene graph at 1) =>
         // scene = null until 2) => scene reestablished, but Perfect scrollbar lost its state when removed from the DOM.
         // This listener will trigger a schedule update at 2) which will restore the perfect scrollbar state (scrollTop
         // & scrollLeft will be reapplied).
-        FXProperties.runOnPropertiesChange(this::scheduleUpdate, node.sceneProperty());
+        FXProperties.runOnPropertiesChange(this::scheduleUiUpdate, node.sceneProperty());
+    }
+
+    private void rescheduleCssScrollEndDetector() {
+        if (cssScrollEndDetector != null)
+            cssScrollEndDetector.cancel();
+        cssScrollEndDetector = UiScheduler.scheduleDelay(200, () -> {
+            if (!cssScrollDetected) {
+                HtmlSvgNodePeer.setSceneTouchPassiveMode(false);
+            } else {
+                cssScrollDetected = false;
+                rescheduleCssScrollEndDetector();
+            }
+        });
     }
 
     private double scrollTop, scrollLeft;
@@ -140,22 +196,28 @@ public final class HtmlScrollPanePeer
                     scrollTop = 0;
             }
         }
-        scheduleUpdate();
+        scheduleUiUpdate();
         syncing = false;
     }
 
     private boolean pending, psInitialized;
-    private void scheduleUpdate() {
+    private void scheduleUiUpdate() {
         if (!pending) {
             pending = true;
             UiScheduler.scheduleDeferred(() -> {
-                Element psContainer = getChildrenContainer();
-                if (!psInitialized) {
-                    N node = getNode();
-                    callPerfectScrollbarInitialize(psContainer, node.getHbarPolicy() == ScrollPane.ScrollBarPolicy.NEVER, node.getvbarPolicy() == ScrollPane.ScrollBarPolicy.NEVER);
-                    psInitialized = true;
+                if (USE_PERFECT_SCROLLBAR) {
+                    Element psContainer = getChildrenContainer();
+                    if (!psInitialized) {
+                        N node = getNode();
+                        callPerfectScrollbarInitialize(psContainer, node.getHbarPolicy() == ScrollPane.ScrollBarPolicy.NEVER, node.getvbarPolicy() == ScrollPane.ScrollBarPolicy.NEVER);
+                        psInitialized = true;
+                    }
+                    callPerfectScrollbarUpdate(psContainer);
+                } else {
+                    HTMLElement element = getElement();
+                    element.scrollTop = scrollTop;
+                    element.scrollLeft = scrollLeft;
                 }
-                callPerfectScrollbarUpdate(psContainer);
                 pending = false;
             });
         }
@@ -166,7 +228,7 @@ public final class HtmlScrollPanePeer
         Js.asPropertyMap(psContainer).set("ps", ps);
         psContainer.addEventListener("ps-scroll-x", e -> setScrollLeft(psContainer.scrollLeft));
         psContainer.addEventListener("ps-scroll-y", e -> setScrollTop(psContainer.scrollTop));
-    };
+    }
 
     private void callPerfectScrollbarUpdate(Element psContainer) {
         psContainer.scrollLeft = scrollLeft;
@@ -187,12 +249,34 @@ public final class HtmlScrollPanePeer
         vSyncUiFromModel();
     }
 
+    private String scrollbarPolicyToOverflow(ScrollPane.ScrollBarPolicy scrollBarPolicy) {
+        if (scrollBarPolicy != null) {
+            switch (scrollBarPolicy) {
+                case ALWAYS:
+                    return "scroll";
+                case AS_NEEDED:
+                    return "auto";
+                case NEVER:
+                    return "hidden";
+            }
+        }
+        return "hidden";
+    }
+
     @Override
     public void updateHbarPolicy(ScrollPane.ScrollBarPolicy hbarPolicy) {
+        if (USE_CSS) {
+            setElementStyleAttribute("overflow-x", scrollbarPolicyToOverflow(hbarPolicy));
+        }
+        // Note: with PerfectScrollbar, This value is considered during initialisation only
     }
 
     @Override
     public void updateVbarPolicy(ScrollPane.ScrollBarPolicy vbarPolicy) {
+        if (USE_CSS) {
+            setElementStyleAttribute("overflow-y", scrollbarPolicyToOverflow(vbarPolicy));
+        }
+        // Note: with PerfectScrollbar, This value is considered during initialisation only
     }
 
     @Override
