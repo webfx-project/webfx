7fa1a9e3e40f76752c2afe6a05d7d565af1d08c3
Bruno Salmon <dev.salmonb@gmail.com>
2025-02-06 12:55:22 +0000

    Updated BooleanPropertyBase from OpenJFX (introduced ValueWrapper)

--- webfx-kit/webfx-kit-javafxbase-emul/src/main/java/javafx/beans/property/BooleanPropertyBase.java
+++ webfx-kit/webfx-kit-javafxbase-emul/src/main/java/javafx/beans/property/BooleanPropertyBase.java
@@ -1,8 +1,34 @@
+/*
+ * Copyright (c) 2011, 2015, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
 package javafx.beans.property;
 
 import com.sun.javafx.binding.ExpressionHelper;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
+import javafx.beans.WeakListener;
 import javafx.beans.binding.BooleanBinding;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableBooleanValue;
@@ -112,7 +138,7 @@ public abstract class BooleanPropertyBase extends BooleanProperty {
     public void set(boolean newValue) {
         if (isBound()) {
             throw new java.lang.RuntimeException((getBean() != null && getName() != null ?
-                    getBean().getClass().getSimpleName() + "." + getName() + " : ": "") + "A bound value cannot be set.");
+                getBean().getClass().getSimpleName() + "." + getName() + " : ": "") + "A bound value cannot be set.");
         }
         if (value != newValue) {
             value = newValue;
@@ -139,17 +165,7 @@ public abstract class BooleanPropertyBase extends BooleanProperty {
         }
 
         final ObservableBooleanValue newObservable = (rawObservable instanceof ObservableBooleanValue) ? (ObservableBooleanValue) rawObservable
-                : new BooleanBinding() {
-            {
-                super.bind(rawObservable);
-            }
-
-            @Override
-            protected boolean computeValue() {
-                final Boolean value = rawObservable.getValue();
-                return (value == null)? false : value;
-            }
-        };
+            : new ValueWrapper(rawObservable);
 
         if (!newObservable.equals(observable)) {
             unbind();
@@ -170,6 +186,9 @@ public abstract class BooleanPropertyBase extends BooleanProperty {
         if (observable != null) {
             value = observable.get();
             observable.removeListener(listener);
+            if (observable instanceof ValueWrapper) {
+                ((ValueWrapper)observable).dispose();
+            }
             observable = null;
         }
     }
@@ -203,7 +222,7 @@ public abstract class BooleanPropertyBase extends BooleanProperty {
         return result.toString();
     }
 
-    private static class Listener implements InvalidationListener {
+    private static class Listener implements InvalidationListener, WeakListener {
 
         private final WeakReference<BooleanPropertyBase> wref;
 
@@ -220,6 +239,30 @@ public abstract class BooleanPropertyBase extends BooleanProperty {
                 ref.markInvalid();
             }
         }
+
+        @Override
+        public boolean wasGarbageCollected() {
+            return wref.get() == null;
+        }
     }
 
+    private class ValueWrapper extends BooleanBinding {
+        private ObservableValue<? extends Boolean> observable;
+
+        public ValueWrapper(ObservableValue<? extends Boolean> observable) {
+            this.observable = observable;
+            bind(observable);
+        }
+
+        @Override
+        protected boolean computeValue() {
+            final Boolean value = observable.getValue();
+            return (value == null) ? false : value;
+        }
+
+        @Override
+        public void dispose() {
+            unbind(observable);
+        }
+    }
 }
