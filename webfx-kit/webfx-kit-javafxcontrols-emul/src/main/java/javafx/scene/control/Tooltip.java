4f05c00fb005bc316830e26e9afbe97acdc4427e
Bruno Salmon <dev.salmonb@gmail.com>
2025-04-30 21:27:37 +0100

    Added Tooltip placeholder class (to prevent compilation error - feature not yet supported)

---
+++ webfx-kit/webfx-kit-javafxcontrols-emul/src/main/java/javafx/scene/control/Tooltip.java
@@ -0,0 +1,30 @@
+package javafx.scene.control;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * Tooltip is not yet supported in WebFX. This class is just here as a placeholder to prevent compilation errors with
+ * JavaFX code using it in a minimal way (just text property for now). Asked by a WebFX user.
+ *
+ * @author Bruno Salmon
+ */
+public class Tooltip extends Control {
+
+    public Tooltip() {
+        this(null);
+    }
+
+    public Tooltip(String text) {
+        super();
+        setText(text);
+    }
+
+    public final StringProperty textProperty() { return text; }
+    public final void setText(String value) {
+        textProperty().setValue(value);
+    }
+    public final String getText() { return text.getValue() == null ? "" : text.getValue(); }
+    private final StringProperty text = new SimpleStringProperty(this, "text", "");
+
+}
